9f5367f32bc3564b1ddcf9398b69d2085c934352
Taylor Otwell <taylorotwell@gmail.com>
2011-06-22 07:26:35 -0700

    Improving view class comments.

--- system/view.php
+++ system/view.php
@@ -95,6 +95,9 @@ class View {
 		// We include the view into the local scope within a
 		// try / catch block to catch any exceptions that may
 		// occur while the view is rendering.
+		//
+		// Otherwise, a white screen of death will be shown
+		// when an exception occurs while rendering the view.
 		// -----------------------------------------------------
 		try
 		{
@@ -111,9 +114,8 @@ class View {
 	/**
 	 * Get the full path to the view.
 	 *
-	 * Views are cascaded, so the application directory views
-	 * will take precedence over the system directory's views
-	 * of the same name.
+	 * Views are cascaded, so the application directory views will take
+	 * precedence over system directory views of the same name.
 	 *
 	 * @return string
 	 */
