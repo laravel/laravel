260f99739474b513b27c47d5b94f7eab1ab243ae
Taylor Otwell <taylorotwell@gmail.com>
2011-06-27 20:51:38 -0500

    added comments to validation lang.

--- application/lang/en/validation.php
+++ application/lang/en/validation.php
@@ -2,26 +2,50 @@
 
 return array(
 
+	/*
+	|--------------------------------------------------------------------------
+	| General Validation Messages
+	|--------------------------------------------------------------------------
+	*/
+
 	"acceptance_of" => "The :attribute must be accepted.",
 	"confirmation_of" => "The :attribute confirmation does not match.",
 	"exclusion_of" => "The :attribute value is invalid.",
 	"format_of" => "The :attribute format is invalid.",
 	"inclusion_of" => "The :attribute value is invalid.",
-	"presence_of" => "The :attribute can't be blank.",
+	"presence_of" => "The :attribute can't be empty.",
 	"uniqueness_of" => "The :attribute has already been taken.",
 	"with_callback" => "The :attribute is invalid.",
 
-	"number_not_valid" => "The :attribute is not a valid number.",
-	"number_not_integer" => "The :attribute is not a valid integer.",
+	/*
+	|--------------------------------------------------------------------------
+	| Numericality_Of Validation Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	"number_not_valid" => "The :attribute must be a valid number.",
+	"number_not_integer" => "The :attribute must be an integer.",
 	"number_wrong_size" => "The :attribute must be :size.",
-	"number_too_big" => "The :attribute must be less than :max.",
+	"number_too_big" => "The :attribute must be no more than :max.",
 	"number_too_small" => "The :attribute must be at least :min.",
 
+	/*
+	|--------------------------------------------------------------------------
+	| Length_Of Validation Messages
+	|--------------------------------------------------------------------------
+	*/
+
 	"string_wrong_size" => "The :attribute must be :size characters.",
-	"string_too_big" => "The :attribute must be less than :max characters.",
+	"string_too_big" => "The :attribute must be no more than :max characters.",
 	"string_too_small" => "The :attribute must be at least :min characters.",
 
-	"file_too_big" => "The :attribute exceeded size limit of :maxkb.",
-	"file_wrong_type" => "The :attribute is an invalid type.",
+	/*
+	|--------------------------------------------------------------------------
+	| Upload_Of Validation Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	"file_wrong_type" => "The :attribute is an invalid file type.",
+	"file_too_big" => "The :attribute exceeds size limit of :maxkb.",
 
 );
