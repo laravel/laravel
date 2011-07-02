1a2c5e511a4a5e53874615c71c5f2a8df24ed1da
John Nye <john@johnnye.net>
2011-07-02 02:09:41 +0100

    Moved the hash class over to bcrypt.
    Moved the class over to bcrypt, as this is a better implementation for storing passwords, it includes a work factor.

--- system/hash.php
+++ system/hash.php
@@ -15,25 +15,50 @@ class Hash {
 	 * @var string
 	 */
 	public $salt;
-
+	/**
+	* The workfactor for making password cracking harder
+	*
+	* @var integer
+	**/
+	
+	const DEFAULT_WORK_FACTOR = 8;
+	
 	/**
 	 * Create a new hash instance.
 	 *
 	 * @param  string  $value
 	 * @param  string  $salt
+	 * @param  integer $workFactor
 	 * @return void
 	 */
-	public function __construct($value, $salt = null)
+	public function __construct($value, $salt = null, $workFactor=0)
 	{
+		// -------------------------------------------------------
+		// Need to check that the work factor is acceptable 
+		// 
+		// Otherwise, we will use the default of 8
+		// -------------------------------------------------------
+		if ($work_factor < 4 || $work_factor > 31) $work_factor = self::DEFAULT_WORK_FACTOR;
+
 		// -------------------------------------------------------
 		// If no salt is given, we'll create a random salt to
 		// use when hashing the password.
 		//
 		// Otherwise, we will use the given salt.
 		// -------------------------------------------------------
-		$this->salt = (is_null($salt)) ? Str::random(16) : $salt;
-
-		$this->value = sha1($value.$this->salt);
+		
+		if(is_null($salt)){
+			$this->salt = '$2a$' . str_pad($work_factor, 2, '0', STR_PAD_LEFT) . '$' .
+			        substr(
+			            strtr(base64_encode(Str::random(16)), '+', '.'), 
+			            0, 22
+			        )
+			    ;
+		}else{
+			$this->salt = $salt;
+		}
+		
+		$this->value = crypt($value, $this->salt);
 	}
 
 	/**
@@ -42,11 +67,12 @@ class Hash {
 	 * @access public
 	 * @param  string  $value
 	 * @param  string  $salt
+	 * @param integer $workFactor
 	 * @return Hash
 	 */
-	public static function make($value, $salt = null)
+	public static function make($value, $salt = null, $workFactor = 0)
 	{
-		return new self($value, $salt);
+		return new self($value, $salt, $workFactor );
 	}
 
 }	
