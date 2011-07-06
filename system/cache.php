a3401d52474ec5fab2426310cf4a4c8b9f59b456
Taylor Otwell <taylorotwell@gmail.com>
2011-07-06 13:40:03 -0700

    Cleaning up Cache class.

--- system/cache.php
+++ system/cache.php
@@ -10,19 +10,13 @@ class Cache {
 	private static $drivers = array();
 
 	/**
-	 * Get the cache driver instance.
+	 * Get a cache driver instance. Cache drivers are singletons.
 	 *
 	 * @param  string  $driver
 	 * @return Cache\Driver
 	 */
 	public static function driver($driver = null)
 	{
-		// --------------------------------------------------
-		// If the cache driver has already been instantiated,
-		// we'll just return that existing instance.
-		//
-		// Otherwise, we'll instantiate a new one.
-		// --------------------------------------------------
 		if ( ! array_key_exists($driver, static::$drivers))
 		{
 			if (is_null($driver))
@@ -41,13 +35,9 @@ class Cache {
 	 */
 	public static function __callStatic($method, $parameters)
 	{
-		// --------------------------------------------------
-		// Passing method calls to the driver instance allows
-		// a better API for the developer.
-		//
-		// For instance, instead of saying Cache::driver()->foo(),
-		// we can now just say Cache::foo().
-		// --------------------------------------------------
+		// Passing method calls to the driver instance provides a better API for the
+		// developer. For instance, instead of saying Cache::driver()->foo(), we can
+		// now just say Cache::foo().
 		return call_user_func_array(array(static::driver(), $method), $parameters);
 	}
 
