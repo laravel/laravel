65ffe0b6103fe93a1a19f471405d6530379e9887
Taylor Otwell <taylorotwell@gmail.com>
2011-07-06 13:45:03 -0700

    Added support for closures as Arr::get default parameter.

--- system/arr.php
+++ system/arr.php
@@ -7,7 +7,7 @@ class Arr {
 	 *
 	 * @param  array   $array
 	 * @param  string  $key
-	 * @param  array   $default
+	 * @param  mixed   $default
 	 * @return mixed
 	 */
 	public static function get($array, $key, $default = null)
@@ -17,7 +17,12 @@ class Arr {
 			return $array;
 		}
 
-		return (array_key_exists($key, $array)) ? $array[$key] : $default;
+		if (array_key_exists($key, $array))
+		{
+			return $array[$key];
+		}
+
+		return is_callable($default) ? call_user_func($default) : $default;
 	}
 
 }
