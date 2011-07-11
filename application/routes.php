9224e62cbef6bdc603914dfe06d7994df8519383
Taylor Otwell <taylorotwell@gmail.com>
2011-07-11 13:03:09 -0700

    More tweaking to routes.php comment.

--- application/routes.php
+++ application/routes.php
@@ -8,9 +8,9 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| Here is the public API of your application. To add functionality to your
-	| application, you add to the array located in this file.
+	| application, you just add to the array located in this file.
 	|
-	| It's a breeze. Just tell Laravel the request URIs it should respond to.
+	| It's a breeze. Simply tell Laravel the request URIs it should respond to.
 	|
 	*/
 
