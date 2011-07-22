ebd8bd55b2a1cf398c5d4829e8a7929c7db98abc
Taylor Otwell <taylorotwell@gmail.com>
2011-07-21 23:54:25 -0500

    added http_only option to cookie::put method.

--- system/cookie.php
+++ system/cookie.php
@@ -28,38 +28,40 @@ class Cookie {
 	/**
 	 * Set a "permanent" cookie. The cookie will last 5 years.
 	 *
-	 * @param  string   $name
-	 * @param  string   $value
-	 * @param  string   $path
-	 * @param  string   $domain
-	 * @param  bool     $secure
+	 * @param  string  $name
+	 * @param  string  $value
+	 * @param  string  $path
+	 * @param  string  $domain
+	 * @param  bool    $secure
+	 * @param  bool    $http_only
 	 * @return bool
 	 */
-	public static function forever($name, $value, $path = '/', $domain = null, $secure = false)
+	public static function forever($name, $value, $path = '/', $domain = null, $secure = false, $http_only = false)
 	{
-		return static::put($name, $value, 2628000, $path, $domain, $secure);
+		return static::put($name, $value, 2628000, $path, $domain, $secure, $http_only);
 	}
 
 	/**
 	 * Set the value of a cookie. If a negative number of minutes is
 	 * specified, the cookie will be deleted.
 	 *
-	 * @param  string   $name
-	 * @param  string   $value
-	 * @param  int      $minutes
-	 * @param  string   $path
-	 * @param  string   $domain
-	 * @param  bool     $secure
+	 * @param  string  $name
+	 * @param  string  $value
+	 * @param  int     $minutes
+	 * @param  string  $path
+	 * @param  string  $domain
+	 * @param  bool    $secure
+	 * @param  bool    $http_only
 	 * @return bool
 	 */
-	public static function put($name, $value, $minutes = 0, $path = '/', $domain = null, $secure = false)
+	public static function put($name, $value, $minutes = 0, $path = '/', $domain = null, $secure = false, $http_only = false)
 	{
 		if ($minutes < 0)
 		{
 			unset($_COOKIE[$name]);
 		}
 
-		return setcookie($name, $value, ($minutes != 0) ? time() + ($minutes * 60) : 0, $path, $domain, $secure);
+		return setcookie($name, $value, ($minutes != 0) ? time() + ($minutes * 60) : 0, $path, $domain, $secure, $http_only);
 	}
 
 	/**
