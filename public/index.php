198e95910d3658a11ab1813caa1f4f494da5fb84
Taylor Otwell <taylorotwell@gmail.com>
2011-07-21 22:10:57 -0500

    incremented version.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * Laravel - A clean and classy framework for PHP web development.
  *
  * @package  Laravel
- * @version  1.2.2
+ * @version  1.3.0
  * @author   Taylor Otwell
  * @license  MIT License
  * @link     http://laravel.com 
