acea14a30693646e5e7902aa2f9c37381a5d03c5
Taylor Otwell <taylorotwell@gmail.com>
2011-07-28 13:42:41 -0500

    Cleaning up configuration comments.

--- application/config/application.php
+++ application/config/application.php
@@ -33,9 +33,7 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| The default language of your application. This language will be used by
-	| default by the Lang library when doing string localization.
-	|
-	| If you are not using the Lang library, this option isn't really important.
+	| Lang library as the default language when doing string localization.
 	|
 	*/
 
@@ -46,7 +44,7 @@ return array(
 	| Application Character Encoding
 	|--------------------------------------------------------------------------
 	|
-	| This default character encoding used by your application. This is the
+	| The default character encoding used by your application. This is the
 	| character encoding that will be used by the Str, Text, and Form classes.
 	|
 	*/
@@ -73,8 +71,6 @@ return array(
 	| Your application key should be a 32 character string that is totally
 	| random and secret. This key is used by the encryption class to generate
 	| secure, encrypted strings.
-	|
-	| If you will not be using the encryption class, this doesn't matter.
 	|
 	*/
 
