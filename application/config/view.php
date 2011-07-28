554f14c10c7be1022c3810215f411ba206a91519
Taylor Otwell <taylorotwell@gmail.com>
2011-07-28 13:48:36 -0500

    Cleaning up configuration comments.

--- application/config/view.php
+++ application/config/view.php
@@ -7,17 +7,14 @@ return array(
 	| Named Views
 	|--------------------------------------------------------------------------
 	|
-	| Here you can define all of the named views for your application. 
+	| All of the named views for your application. Once you have defined the
+	| named view, you can create a View instance for that view using the
+	| View::of dynamic method.
 	|
-	| Once you have defined the named view, you can create a View instance for
-	| that view using the View::of dynamic method.
-	|
-	| For example, if you define a named view named "layout", you could create
-	| an instance of that View by calling: View::of_layout().
+	| For example, if you define a "layout" named view, you could create an
+	| instance of that View by calling: View::of_layout().
 	|
 	| For more info, check out: http://laravel.com/docs/start/views#named
-	|
-	| Note: The view path should be relative to the application/view directory.
 	|
 	*/
 
