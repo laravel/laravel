f3ddadce045958cf6db8f24cbafaf82ce27771a7
Taylor Otwell <taylorotwell@gmail.com>
2011-07-29 23:29:37 -0500

    tweaking comment in composers.php.

--- application/composers.php
+++ application/composers.php
@@ -12,8 +12,8 @@ return array(
 	| footer partial each time the view is created.
 	|
 	| The composer will receive an instance of the view being created, and is
-	| free to modify the view however you wish. But, always return the view
-	| instance at the end of the composer.
+	| free to modify the view however you wish. Be sure to always return the
+	| view instance at the end of your composer.
 	|
 	| For more information, check out: http://laravel.com/docs/start/views#composers
 	|
