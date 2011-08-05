668ecaedd9ff4e5d289fc6116b34045f816e3635
Taylor Otwell <taylorotwell@gmail.com>
2011-08-05 09:06:23 -0500

    Tweaking front controller.

--- public/index.php
+++ public/index.php
@@ -11,39 +11,34 @@
 // --------------------------------------------------------------
 // The path to the application directory.
 // --------------------------------------------------------------
-define('APP_PATH', realpath('../application').'/');
+$application = '../application';
 
 // --------------------------------------------------------------
 // The path to the system directory.
 // --------------------------------------------------------------
-define('SYS_PATH', realpath($system = '../system').'/');
-
-// --------------------------------------------------------------
-// The path to the public directory.
-// --------------------------------------------------------------
-define('PUBLIC_PATH', realpath(__DIR__).'/');
+$system      = '../system';
 
 // --------------------------------------------------------------
 // The path to the packages directory.
 // --------------------------------------------------------------
-define('PACKAGE_PATH', realpath('../packages').'/');
+$packages    = '../packages';
 
 // --------------------------------------------------------------
 // The path to the modules directory.
 // --------------------------------------------------------------
-define('MODULE_PATH', realpath('../modules').'/');
+$modules     = '../modules';
 
 // --------------------------------------------------------------
 // The path to the storage directory.
 // --------------------------------------------------------------
-define('STORAGE_PATH', realpath('../storage').'/');
+$storage     = '../storage';
 
 // --------------------------------------------------------------
-// The path to the directory containing the system directory.
+// The path to the public directory.
 // --------------------------------------------------------------
-define('BASE_PATH', realpath(str_replace('system', '', $system)).'/');
+$public      = __DIR__;
 
 // --------------------------------------------------------------
 // Launch Laravel.
 // --------------------------------------------------------------
-require SYS_PATH.'laravel.php';
+require $system.'/laravel.php';
