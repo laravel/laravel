88ebf6125cb2d384b553dfd50046b727ed825649
Taylor Otwell <taylorotwell@gmail.com>
2011-08-08 09:44:10 -0500

    Refactor the request class.

--- system/request.php
+++ system/request.php
@@ -29,15 +29,11 @@ class Request {
 
 		$uri = static::raw_uri();
 
-		if (strpos($uri, $base = parse_url(Config::get('application.url'), PHP_URL_PATH)) === 0)
-		{
-			$uri = substr($uri, strlen($base));
-		}
+		$base = parse_url(Config::get('application.url'), PHP_URL_PATH);
 
-		if (strpos($uri, $index = '/index.php') === 0)
-		{
-			$uri = substr($uri, strlen($index));
-		}
+		if (strpos($uri, $base) === 0) $uri = substr($uri, strlen($base));
+
+		if (strpos($uri, $index = '/index.php') === 0) $uri = substr($uri, strlen($index));
 
 		return static::$uri = (($uri = trim($uri, '/')) == '') ? '/' : $uri;
 	}
