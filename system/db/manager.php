73065d12c26fde8638598a8ccd67da3c53279e96
Taylor Otwell <taylorotwell@gmail.com>
2011-08-08 10:51:46 -0500

    Refactor the database manager.

--- system/db/manager.php
+++ system/db/manager.php
@@ -22,7 +22,10 @@ class Manager {
 	 */
 	public static function connection($connection = null)
 	{
-		if (is_null($connection)) $connection = Config::get('db.default');
+		if (is_null($connection))
+		{
+			$connection = Config::get('db.default');
+		}
 
 		if ( ! array_key_exists($connection, static::$connections))
 		{
