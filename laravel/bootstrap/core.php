91e3e3ddb0537790e18a977af4ff21ac1008ee17
Taylor Otwell <taylorotwell@gmail.com>
2011-09-25 22:29:23 -0500

    working on bootstrap comments.

--- laravel/bootstrap/core.php
+++ laravel/bootstrap/core.php
@@ -27,9 +27,8 @@ define('EXT',       '.php');
 define('BLADE_EXT', '.blade.php');
 
 /**
- * Load the classes that can't be resolved through the auto-loader.
- * Typically, these are classes that are used by the auto-loader
- * or configuration classes.
+ * Load the classes that can't be resolved through the auto-loader. These are typically classes
+ * that are used by the auto-loader or configuration classes, and therefore cannot be auto-loaded.
  */
 require SYS_PATH.'facades'.EXT;
 require SYS_PATH.'config'.EXT;
@@ -37,7 +36,10 @@ require SYS_PATH.'loader'.EXT;
 require SYS_PATH.'arr'.EXT;
 
 /**
- * Determine the application environment. The environment is typically
+ * Determine the application environment. The environment is typically set by an environment
+ * variable on the server, as this provides the most accident-proof method of handling
+ * application environments. However, the environment could be manually set by the developer
+ * in the front controller if access to the environment variables is not available.
  * set by an environment variable on the server.
  */
 $environment = (isset($_SERVER['LARAVEL_ENV'])) ? $_SERVER['LARAVEL_ENV'] : null;
@@ -52,10 +54,9 @@ if ( ! is_null($environment)) $configs[] = CONFIG_PATH.$environment.'/';
 Config::$paths = $configs;
 
 /**
- * Bootstrap the application inversion of control (IoC) container.
- * The container provides the convenient resolution of objects and
- * their dependencies, allowing for flexibility and testability
- * within the framework.
+ * Bootstrap the application inversion of control (IoC) container. The container provides the
+ * convenient resolution of objects and their dependencies, allowing for flexibility and
+ * testability within the framework and application.
  */
 require SYS_PATH.'container'.EXT;
 
@@ -64,9 +65,8 @@ $container = new Container(Config::get('container'));
 IoC::$container = $container;
 
 /**
- * Register the application auto-loader. The auto-loader is responsible
- * for the lazy-loading of all of the Laravel core classes, as well as
- * the developer created libraries and models.
+ * Register the application auto-loader. The auto-loader is responsible for the lazy-loading
+ * of all of the Laravel core classes, as well as the developer created libraries and models.
  */
 spl_autoload_register(array('Laravel\\Loader', 'load'));
 
