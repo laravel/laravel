1fb70a85686b028051bfec8d67341525f7821104
Taylor Otwell <taylorotwell@gmail.com>
2011-09-28 22:49:20 -0500

    cleaning up sqlite connector.

--- laravel/database/connectors/sqlite.php
+++ laravel/database/connectors/sqlite.php
@@ -12,14 +12,22 @@ class SQLite extends Connector {
 	{
 		$options = $this->options($config);
 
+		// SQLite in-memory databases only live for the lifetime of the current request, and it
+		// is impossible to create two connections to the same in-memory database.
 		if ($config['database'] == ':memory:')
 		{
 			return new PDO('sqlite::memory:', null, null, $options);
 		}
+
+		// We will check for the database in the default storage directory for the application.
+		// Typically, this directory holds all of the SQLite databases for the application.
 		elseif (file_exists($path = DATABASE_PATH.$config['database'].'.sqlite'))
 		{
 			return new PDO('sqlite:'.$path, null, null, $options);
 		}
+
+		// If we still haven't located the database, we will assume the given database name
+		// is a fully qualified path to the database on disk and attempt to load it.
 		elseif (file_exists($config['database']))
 		{
 			return new PDO('sqlite:'.$config['database'], null, null, $options);
