4263203dda3260ad196e0e78f8ee253d79c99f25
Taylor Otwell <taylorotwell@gmail.com>
2011-10-05 01:36:00 -0500

    added comments to pagination language.

--- laravel/language/en/pagination.php
+++ laravel/language/en/pagination.php
@@ -1,9 +1,20 @@
 <?php return array(
 
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Elements
+	|--------------------------------------------------------------------------
+	|
+	| These simple language lines provide the text for the Paginator class.
+	| Each line corresponds with an element of the pagination links generated
+	| by the class, and may be changed freely by the developer.
+	|
+	*/
+
+	'first'    => 'First',
 	'previous' => '&larr; Previous',
-	'first' => 'First',
-	'next' => 'Next &rarr;',
-	'last' => 'Last',
-	'status' => 'Page :current of :last',
+	'status'   => 'Page :current of :last',
+	'next'     => 'Next &rarr;',
+	'last'     => 'Last',
 
 );
