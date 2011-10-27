98d351b91e78a2cf32c57f8d65df00b1601ffa81
Taylor Otwell <taylorotwell@gmail.com>
2011-10-26 22:53:50 -0500

    added route filter tests.

---
+++ tests/Cases/Routing/RouteFilterTest.php
@@ -0,0 +1,25 @@
+<?php use Laravel\Routing\Filter;
+
+class RouteFilterTest extends PHPUnit_Framework_TestCase {
+
+	public function test_simple_filters_can_be_called()
+	{
+		$filters = array(
+			'simple' => function()
+			{
+				return 'simple';
+			},
+
+			'parameters' => function($one, $two)
+			{
+				return $one.'|'.$two;
+			},
+		);
+
+		Filter::register($filters);
+
+		$this->assertEquals(Filter::run(array('simple'), array(), true), 'simple');
+		$this->assertEquals(Filter::run(array('parameters'), array(1, 2), true), '1|2');
+	}
+
+}
