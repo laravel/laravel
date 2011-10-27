1264a7dd8dd5c73033b9660a20768a78577fba4e
Taylor Otwell <taylorotwell@gmail.com>
2011-10-26 22:57:34 -0500

    added after filter tests.

--- tests/Cases/Routing/RouteFilterTest.php
+++ tests/Cases/Routing/RouteFilterTest.php
@@ -23,4 +23,26 @@ class RouteFilterTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals(Filter::run(array('parameters:1,2'), array(3), true), '3|1|2');
 	}
 
+	public function test_after_filters_are_called()
+	{
+		$filters = array(
+			'after1' => function()
+			{
+				define('ROUTE_FILTER_AFTER_1', 1);
+			},
+
+			'after2' => function()
+			{
+				define('ROUTE_FILTER_AFTER_2', 2);
+			},
+		);
+
+		Filter::register($filters);
+
+		Filter::run(array('after1', 'after2'));
+
+		$this->assertTrue(defined('ROUTE_FILTER_AFTER_1'));
+		$this->assertTrue(defined('ROUTE_FILTER_AFTER_2'));
+	}
+
 }
