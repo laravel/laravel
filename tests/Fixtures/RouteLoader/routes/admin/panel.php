e05b05a33b56561c85597ebf460aa2faf5977071
Taylor Otwell <taylorotwell@gmail.com>
2011-10-26 22:27:19 -0500

    added route loader tests.

---
+++ tests/Fixtures/RouteLoader/routes/admin/panel.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+	'GET /admin/panel/show' => function()
+	{
+		return 'GET /admin/panel/show';
+	},
+
+	'GET /admin/panel/update' => function()
+	{
+		return 'GET /admin/panel/update';
+	},
+
+);
