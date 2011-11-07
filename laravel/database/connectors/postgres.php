05902d22d423423fc5eb7fc485647d8a15dd0967
Taylor Otwell <taylorotwell@gmail.com>
2011-11-07 13:32:31 -0600

    Cleaning up the Postgres connector.

--- laravel/database/connectors/postgres.php
+++ laravel/database/connectors/postgres.php
@@ -10,38 +10,32 @@ class Postgres extends Connector {
 	 */
 	public function connect($config)
 	{
-		$connection = new PDO($this->dsn($config), $config['username'], $config['password'], $this->options($config));
+		extract($config);
 
-		if (isset($config['charset']))
-		{
-			$connection->prepare("SET NAMES '{$config['charset']}'")->execute();
-		}
-
-		return $connection;
-	}
-
-	/**
-	 * Format the DSN connection string for a Postgres connection.
-	 *
-	 * @param  array   $config
-	 * @return string
-	 */
-	protected function dsn($config)
-	{
 		// Format the initial Postgres PDO connection string. These options are required
 		// for every Postgres connection that is established. The connection strings
 		// have the following convention: "pgsql:host=hostname;dbname=database"
-		$dsn = sprintf('%s:host=%s;dbname=%s', $config['driver'], $config['host'], $config['database']);
+		$dsn = sprintf('%s:host=%s;dbname=%s', $driver, $host, $database);
 
 		// Check for any optional Postgres PDO options. These options are not required
 		// to establish a PDO connection; however, may be needed in certain server
 		// or hosting environments used by the developer.
 		foreach (array('port') as $key => $value)
 		{
-			if (isset($config[$key])) $dsn .= ";{$key}={$value}";
+			if (isset($config[$key]))
+			{
+				$dsn .= ";{$key}={$value}";
+			}
 		}
 
-		return $dsn;
+		$connection = new PDO($dsn, $username, $password, $this->options($config));
+
+		if (isset($config['charset']))
+		{
+			$connection->prepare("SET NAMES '{$charset}'")->execute();
+		}
+
+		return $connection;
 	}
 
 }
