47ecdb043e4dcd6fbf40d8d65262bf00d06ad8cf
Taylor Otwell <taylorotwell@gmail.com>
2012-01-03 16:47:45 -0600

    added comment to bundle repository.

--- laravel/cli/bundle/repository.php
+++ laravel/cli/bundle/repository.php
@@ -12,11 +12,15 @@ class Repository {
 	/**
 	 * Get the decoded JSON information for a bundle.
 	 *
-	 * @param  string  $bundle
+	 * @param  string|int  $bundle
 	 * @return array
 	 */
 	public function get($bundle)
 	{
+		// The Bundle API will return a JSON string that we can decode and
+		// pass back to the consumer. The decoded array will contain info
+		// regarding the bundle's provider and location, as well as all
+		// of the bundle's dependencies.
 		$bundle = @file_get_contents($this->api.$bundle);
 
 		return json_decode($bundle, true);
