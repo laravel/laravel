71d9c9f25e422f115180543051fee48ed7abea83
Taylor Otwell <taylorotwell@gmail.com>
2012-01-10 09:56:09 -0600

    added comments to bundle.php

--- application/bundle.php
+++ application/bundle.php
@@ -1,3 +1,22 @@
 <?php
 
-// Comment on Auto-Loader mappings.
+/*
+|--------------------------------------------------------------------------
+| Auto-Loader Mappings
+|--------------------------------------------------------------------------
+|
+| Larave uses a simple array of class to path mappings to drive the class
+| auto-loader. This simple approach helps avoid the performance problems
+| of searching through directories by some kind of convention. It also
+| gives you the freedom to organize your application how you want.
+|
+| Registering a mapping couldn't be easier. Just pass an array of class
+| to path maps into the "map" function of Autoloader. Then, when you
+| want to use that class, just use it. It's a piece of cake.
+|
+*/
+
+Autoloader::map(array(
+	//'User' => APP_PATH.'models/user.php',
+	//'Role' => APP_PATH.'models/role.php',
+));
