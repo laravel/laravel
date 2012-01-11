3d3db89aea11f515cc165effc8801bcf8e738348
Taylor Otwell <taylorotwell@gmail.com>
2012-01-11 15:32:48 -0600

    tweaked the auth config.

--- application/config/auth.php
+++ application/config/auth.php
@@ -4,36 +4,16 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
-	| Authentication Username
-	|--------------------------------------------------------------------------
-	|
-	| This option should be set to the "username" property of your users which
-	| is typically named either "email" or "username". However, you have the
-	| freedom to use whatever you wish.
-	|
-	| The value of this property will be used by the "attempt" closure when
-	| searching for users by their username. It will also be used when the
-	| user is set to be "remembered", as the username is embedded into the
-	| encrypted cookie and is used to verify the user's identity.
-	|
-	*/
-
-	'username' => 'email',
-
-	/*
-	|--------------------------------------------------------------------------
 	| Retrieve The Current User
 	|--------------------------------------------------------------------------
 	|
-	| This closure is called by the Auth::user() method when attempting to
-	| retrieve a user by the ID that is stored in their session.
+	| This closure is called by the Auth class' "user" method when trying to
+	| retrieve a user by the ID that is stored in their session. If you find
+	| the user, just return the user object, but make sure it has an "id"
+	| property. If you can't find the user, just return null.
 	|
-	| Simply return an object representing the user with the given ID. Or, if
-	| no user with the given ID is registered to use your application, you do
-	| not need to return anything.
-	|
-	| Of course, a simple, elegant authentication solution is already provided
-	| for you using Eloquent and the default Laravel hashing engine.
+	| Of course, a simple and elegant authentication solution has already
+	| been provided for you using the query builder and hashing engine.
 	|
 	*/
 
@@ -41,7 +21,7 @@ return array(
 	{
 		if ( ! is_null($id) and filter_var($id, FILTER_VALIDATE_INT) !== false)
 		{
-			return User::find($id);
+			return DB::table('users')->find($id);
 		} 
 	},
 
@@ -51,7 +31,8 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| This closure is called by the Auth::attempt() method when attempting to
-	| authenticate a user that is logging into your application.
+	| authenticate a user that is logging into your application. It's like a
+	| super buff bouncer to your application.
 	|
 	| If the provided credentials are correct, simply return an object that
 	| represents the user being authenticated. As long as it has a property
@@ -62,7 +43,7 @@ return array(
 
 	'attempt' => function($username, $password, $config)
 	{
-		$user = User::where($config['username'], '=', $username)->first();
+		$user = DB::table('users')->where_username($username)->first();
 
 		if ( ! is_null($user) and Hash::check($password, $user->password))
 		{
