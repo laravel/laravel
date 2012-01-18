659a09a11b04aa8eb40bcc840c1fa57b357047df
Taylor Otwell <taylorotwell@gmail.com>
2012-01-18 11:28:12 -0600

    added mroe tests for route.

--- tests/cases/laravel/route.test.php
+++ tests/cases/laravel/route.test.php
@@ -15,12 +15,19 @@ class RouteTest extends PHPUnit_Framework_TestCase {
 	 *
 	 * @group laravel
 	 */
-	public function testHandlesReturnsTrueWhenRouteHandlesTheGivenURI()
+	public function testHandlesIndicatesIfTheRouteHandlesAGivenURI()
 	{
 		$route = new Laravel\Routing\Route('GET /', array('handles' => array('foo/bar')));
 
 		$this->assertTrue($route->handles('foo/*'));
 		$this->assertTrue($route->handles('foo/bar'));
+		$this->assertFalse($route->handles('baz'));
+
+		$route = new Laravel\Routing\Route('GET /', array('handles' => array('/', 'home')));
+
+		$this->assertTrue($route->handles('/'));
+		$this->assertTrue($route->handles('home'));
+		$this->assertFalse($route->handles('foo'));
 	}
 
 }
