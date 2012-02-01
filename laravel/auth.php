33f34aa479f043875e799551c2112d487d233ef8
Taylor Otwell <taylorotwell@gmail.com>
2012-02-01 10:32:06 -0600

    dont use ioc for session.

--- laravel/auth.php
+++ laravel/auth.php
@@ -55,7 +55,7 @@ class Auth {
 	{
 		if ( ! is_null(static::$user)) return static::$user;
 
-		$id = IoC::core('session')->get(Auth::user_key);
+		$id = Session::get(Auth::user_key);
 
 		// To retrieve the user, we'll first attempt to use the "user" Closure
 		// defined in the auth configuration file, passing in the ID. The user
