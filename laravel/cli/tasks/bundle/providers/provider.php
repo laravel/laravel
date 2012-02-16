1324ba368c93ab997eb36d6900686000d0d5e242
Taylor Otwell <taylorotwell@gmail.com>
2012-02-16 14:35:32 -0600

    tweak provider to use temp directory to avoid latest confusions.

--- laravel/cli/tasks/bundle/providers/provider.php
+++ laravel/cli/tasks/bundle/providers/provider.php
@@ -39,9 +39,11 @@ abstract class Provider {
 		// Once we have the Zip archive, we can open it and extract it
 		// into the working directory. By convention, we expect the
 		// archive to contain one root directory with the bundle.
-		$zip->extractTo($work);
+		mkdir($work.'zip');
 
-		$latest = File::latest($work)->getRealPath();
+		$zip->extractTo($work.'zip');
+
+		$latest = File::latest($work.'zip')->getRealPath();
 
 		@chmod($latest, 0777);
 
@@ -49,6 +51,8 @@ abstract class Provider {
 		// able to move its contents over into the bundles folder
 		// so the bundle will be usable by the develoepr.
 		File::mvdir($latest, $path);
+
+		File::rmdir($work.'zip');
 
 		@unlink($target);
 	}
