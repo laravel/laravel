5a25b5a7146273980a0c2a6a532350732736a203
Taylor Otwell <taylorotwell@gmail.com>
2012-03-01 11:22:47 -0600

    Fix typo
    Fix typo in the configuration loader event description.
    
    Signed-off-by: Taylor Otwell <taylorotwell@gmail.com>

--- application/start.php
+++ application/start.php
@@ -22,7 +22,7 @@ ini_set('display_errors', 'Off');
 | The Laravel configuration loader is responsible for returning an array
 | of configuration options for a given bundle and file. By default, we
 | use the files provided with Laravel; however, you are free to use
-| your own storage mechanism for configuration files.
+| your own storage mechanism for configuration arrays.
 |
 */
 
