230be9ebd18372606b34ca7da293fb0197a877e3
Dayle Rees <thepunkfan@gmail.com>
2012-03-02 09:49:07 +0000

    ability to show docs for methods as well as tasks
    Signed-off-by: Dayle Rees <thepunkfan@gmail.com>

--- laravel/cli/tasks/help.php
+++ laravel/cli/tasks/help.php
@@ -8,18 +8,28 @@ class Help extends Task {
 	/**
 	 * Show a global help message, or call the help() method
 	 * of a task given as parameter.
-	 * 
+	 *
 	 * @param array $arguments
 	 */
 	public function run($arguments = array())
 	{
 		if(! count($arguments)) $this->_help();
 
-		$task = IoC::resolve('task: '. Str::lower($arguments[0]));
-
-		if(is_callable(array($task, 'help')))
+		// if help param contains a method, and split it
+		if(strstr($arguments[0], ':') and $parts = explode(':', $arguments[0]))
 		{
-			$task->help();
+			$task = IoC::resolve('task: '. Str::lower($parts[0]));
+			$help = 'help_'.Str::lower($parts[1]);
+		}
+		else
+		{
+			$task = IoC::resolve('task: '. Str::lower($arguments[0]));
+			$help = 'help';
+		}
+
+		if(is_callable(array($task, $help)))
+		{
+			$task->$help();
 		}
 		else
 		{
