8204bb98b4485371aea43af3a5018b1e11e5d129
Franz Liedke <franz@develophp.org>
2012-03-12 13:39:43 +0100

    Add trailing slash to input fields generated by the form class.

--- laravel/form.php
+++ laravel/form.php
@@ -200,7 +200,7 @@ class Form {
 
 		$attributes = array_merge($attributes, compact('type', 'name', 'value', 'id'));
 
-		return '<input'.HTML::attributes($attributes).'>'.PHP_EOL;
+		return '<input'.HTML::attributes($attributes).' />'.PHP_EOL;
 	}
 
 	/**
