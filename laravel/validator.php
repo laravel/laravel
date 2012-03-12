5c8527f579cca6f659594e0f1ac3004e6d34e307
Taylor Otwell <taylorotwell@gmail.com>
2012-03-12 10:50:04 -0500

    Use str_getcsv instead of explode in validator to allow commas in in and not_in rules.
    Signed-off-by: Taylor Otwell <taylorotwell@gmail.com>

--- laravel/validator.php
+++ laravel/validator.php
@@ -927,7 +927,7 @@ class Validator {
 		// "max:3" specifies that the value may only be 3 characters long.
 		if (($colon = strpos($rule, ':')) !== false)
 		{
-			$parameters = explode(',', substr($rule, $colon + 1));
+			$parameters = str_getcsv(substr($rule, $colon + 1));
 		}
 
 		return array(is_numeric($colon) ? substr($rule, 0, $colon) : $rule, $parameters);
