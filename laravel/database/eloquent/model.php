43212f8505421935938aa28d421b02b8ffe9f081
Taylor Otwell <taylorotwell@gmail.com>
2012-03-21 10:38:39 -0500

    Fix bug in eloquent model.

--- laravel/database/eloquent/model.php
+++ laravel/database/eloquent/model.php
@@ -404,7 +404,7 @@ abstract class Model {
 	 */
 	public function changed($attribute)
 	{
-		array_get($this->attributes, $attribute) !== array_get($this->original, $attribute);
+		return array_get($this->attributes, $attribute) !== array_get($this->original, $attribute);
 	}
 
 	/**
