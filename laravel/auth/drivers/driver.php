f0a6111021a5430a7ed5e170cc2174126261710b
Taylor Otwell <taylorotwell@gmail.com>
2012-05-02 15:41:38 -0500

    Set token.

--- laravel/auth/drivers/driver.php
+++ laravel/auth/drivers/driver.php
@@ -105,6 +105,8 @@ abstract class Driver {
 	 */
 	public function login($token, $remember = false)
 	{
+		$this->token = $token;
+
 		$this->store($token);
 
 		if ($remember) $this->remember($token);
