a303d66ae0de84c94d9b09a00b8f9e881f5d6342
Taylor Otwell <taylorotwell@gmail.com>
2012-05-11 15:32:28 -0500

    Fix bug in events.

--- laravel/event.php
+++ laravel/event.php
@@ -151,6 +151,8 @@ class Event {
 			// We will simply spin through each payload registered for the event and
 			// fire the flusher, passing each payloads as we go. This allows all
 			// the events on the queue to be processed by the flusher easily.
+			if ( ! isset(static::$queued[$queue])) continue;
+
 			foreach (static::$queued[$queue] as $key => $payload)
 			{
 				array_unshift($payload, $key);
