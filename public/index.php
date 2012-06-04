ba088022bb51ef20d3d60ccaae935d7e0809b825
Taylor Otwell <taylorotwell@gmail.com>
2012-06-03 19:54:02 -0500

    updated version.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @version  3.2.1
+ * @version  3.2.2
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  * @link     http://laravel.com
  */
