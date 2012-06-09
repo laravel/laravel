31bb3e0e39ef065e0b7b93aa6000eaca6af98126
Mark Huot <mark@markhuot.com>
2012-06-09 09:25:11 -0400

    added a seed method to migrate that will standardize how data is inserted into a new project

---
+++ application/seeds/seed.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Seed
+|--------------------------------------------------------------------------
+|
+| Code here will be run when `artisan migrate:seed` is called. Any valid
+| PHP is allowed, however it is recommended you keep this file focused on
+| setting up required data for the application to function.
+|
+| For example,
+| for ($i=0; $i<100; $i++)
+| {
+|     $location = new Location();
+|     $location->name = 'Sample Location '.$i;
+|     $location->save();
+| }
+|
+| $role = new Role();
+| $role->name = 'Publishers';
+| $role->save();
+|
+| $role = new Role();
+| $role->name = 'Editors';
+| $role->save();
+|
+| $role = new Role();
+| $role->name = 'Guests';
+| $role->save();
+|
+| $user = new User();
+| $user->roles()->attach(1)
+| $user->name = 'admin';
+| $user->password = '';
+| $user->save();
+|
+| $user = new User();
+| $user->roles()->attach(2)
+| $user->name = 'john';
+| $user->password = '';
+| $user->save();
+*/
