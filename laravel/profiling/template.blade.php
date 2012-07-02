94e9106b760eb1b50eb81ac83b23f400b6a3e829
Franz Liedke <franz@develophp.org>
2012-07-02 03:42:43 +0300

    Display memory usage (and peak usage) in the profiler bar, too.

--- laravel/profiling/template.blade.php
+++ laravel/profiling/template.blade.php
@@ -62,6 +62,7 @@
 			</a>
 		</li>
 		<li><a class="anbu-tab">Time <span class="anbu-count">{{ $time }}ms</span></a></li>
+		<li><a class="anbu-tab">Memory <span class="anbu-count">{{ $memory }} ({{ $memory_peak }})</span></a></li>
 		<li class="anbu-tab-right"><a id="anbu-hide" href="#">&#8614;</a></li>
 		<li class="anbu-tab-right"><a id="anbu-close" href="#">&times;</a></li>
 		<li class="anbu-tab-right"><a id="anbu-zoom" href="#">&#8645;</a></li>
