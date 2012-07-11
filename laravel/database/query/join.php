ee41810ca86a6780c4c93a70aa4ac4b63f215ffa
Koen Schmeets <k.schmeets@gmail.com>
2012-07-11 19:51:34 +0200

    Allowing nested join clauses
    Signed-off-by: Koen Schmeets <k.schmeets@gmail.com>

--- laravel/database/query/join.php
+++ laravel/database/query/join.php
@@ -45,8 +45,16 @@ class Join {
 	 * @param  string  $connector
 	 * @return Join
 	 */
-	public function on($column1, $operator, $column2, $connector = 'AND')
+	public function on($column1, $operator = null, $column2 = null, $connector = 'AND')
 	{
+		// If a Closure is passed into the method, it means a nested ON
+		// clause is being initiated, so we will take a different course
+		// of action than when the statement is just a simple where.
+		if (is_callable($column1))
+		{
+			return $this->on_nested($column1, $connector);
+		}
+
 		$this->clauses[] = compact('column1', 'operator', 'column2', 'connector');
 
 		return $this;
@@ -63,6 +71,34 @@ class Join {
 	public function or_on($column1, $operator, $column2)
 	{
 		return $this->on($column1, $operator, $column2, 'OR');
+	}
+
+	/**
+	 * Add a nested ON clause to the join.
+	 *
+	 * @param  Closure  $callback
+	 * @param  string   $connector
+	 * @return Query
+	 */
+	public function on_nested($callback, $connector = 'AND')
+	{
+		$type = 'on_nested';
+
+		// To handle a nested ON clause, we will actually instantiate a new
+		// Join instance and run the callback over that instance
+		$join = new Join($this->type, $this->table);
+
+		call_user_func($callback, $join);
+		
+		// Once the callback has been run on the query, we will store the nested
+		// query instance on the where clause array so that it's passed to the
+		// query's query grammar instance when building.
+		if ($join->clauses !== null)
+		{
+			$this->clauses[] = compact('join', 'connector');
+		}
+
+		return $this;
 	}
 
 }
