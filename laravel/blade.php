34c746b4f8901a6b7ac61077d3074f1ff9d33798
Tobsn <orterer@gmail.com>
2012-07-25 00:23:31 +0200

    added @continue
    like @break - as of request issue #1001

--- laravel/blade.php
+++ laravel/blade.php
@@ -306,7 +306,7 @@ class Blade {
 	 */
 	protected static function compile_structure_closings($value)
 	{
-		$pattern = '/@(endif|endforeach|endfor|endwhile|break)/';
+		$pattern = '/@(endif|endforeach|endfor|endwhile|break|continue)/';
 
 		return preg_replace($pattern, '<?php $1; ?>', $value);
 	}
