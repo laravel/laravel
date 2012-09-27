d939a342714bc92501356eec950ddf3359dd7b4a
Mario Cuba <mario@mariocuba.net>
2012-09-26 21:37:25 -0430

    Spanish translation
    Signed-off-by: Mario Cuba <mario@mariocuba.net>

---
+++ application/language/es/pagination.php
@@ -0,0 +1,22 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the pagination links. You're free to change them to anything you want.
+	| If you come up with something more exciting, let us know.
+	|
+	| Spanish Translation:
+	| Mario Cuba - http://mariocuba.net
+	|
+	*/
+
+	'previous' => '&laquo; Anterior',
+	'next'     => 'Siguiente &raquo;',
+
+);
