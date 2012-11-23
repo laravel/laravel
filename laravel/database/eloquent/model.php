ff4b43c72f9ae2a4af72e3fa7b58aecd78e6029c
Franz Liedke <franz@develophp.org>
2012-11-23 03:23:13 +0100

    Ignore hidden relationships in to_array().

--- laravel/database/eloquent/model.php
+++ laravel/database/eloquent/model.php
@@ -615,6 +615,9 @@ abstract class Model {
 
 		foreach ($this->relationships as $name => $models)
 		{
+			// Relationships can be marked as "hidden", too.
+			if (in_array($name, static::$hidden)) continue;
+
 			// If the relationship is not a "to-many" relationship, we can just
 			// to_array the related model and add it as an attribute to the
 			// array of existing regular attributes we gathered.
