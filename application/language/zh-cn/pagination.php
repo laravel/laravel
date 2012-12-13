bc879e52b5b38e3386a7620c6e16a06690ef22a3
SaiWang <wangsai7885@gmail.com>
2012-12-13 15:53:36 +0800

    add Simplified Chinese translation files

---
+++ application/language/zh-cn/pagination.php
@@ -0,0 +1,22 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the pagination links. You're free to change them to anything you want.
+	| If you come up with something more exciting, let us know.
+	|
+	| Simplified Chinese translation.
+	| 简体中文版译文由 Laravel中文网(http://www.golaravel.com) 维护。
+	|
+	*/
+
+	'previous' => '&laquo; 上一页',
+	'next'     => '下一页 &raquo;',
+
+);
