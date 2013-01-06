77679a5659a5104042ec676237a5d61808155243
Pavel Puchkin <neoascetic@gmail.com>
2013-01-07 01:20:46 +1100

    Respect LARAVEL_ENV variable
    Laravel **should** respect `LARAVEL_ENV` variable when running through CLI

--- laravel/core.php
+++ laravel/core.php
@@ -171,9 +171,9 @@ Request::$foundation = RequestFoundation::createFromGlobals();
 
 if (Request::cli())
 {
-	$environment = get_cli_option('env');
+	$environment = get_cli_option('env', getenv('LARAVEL_ENV'));
 
-	if ( ! isset($environment))
+	if (empty($environment))
 	{
 		$environment = Request::detect_env($environments, gethostname());
 	}
