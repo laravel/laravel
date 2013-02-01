af4381f7de05b21246b1ba1466afa1bb6561be28
Taylor Otwell <taylorotwell@gmail.com>
2013-01-31 22:40:41 -0600

    implement remindable interface on default user.

--- app/models/User.php
+++ app/models/User.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\RemindableInterface;
 
-class User extends Eloquent implements UserInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	/**
 	 * The database table used by the model.
@@ -36,6 +37,16 @@ class User extends Eloquent implements UserInterface {
 	public function getAuthPassword()
 	{
 		return $this->password;
+	}
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
 	}
 
 }
