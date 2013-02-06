24e158e88929f397a4b418222bc23a1e619176ba
Ben Corlett <bencorlett@me.com>
2013-02-07 10:58:28 +1100

    Adding bootstrap/paths.php to allow specification of custom paths for sections of Laravel.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

---
+++ bootstrap/paths.php
@@ -0,0 +1,42 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Path
+	|--------------------------------------------------------------------------
+	|
+	| Here we just defined the path to the application directory. Most likely
+	| you will never need to change this value as the default setup should
+	| work perfectly fine for the vast majority of all our applications.
+	|
+	*/
+
+	'app' => __DIR__.'/../app',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Public Path
+	|--------------------------------------------------------------------------
+	|
+	| We understand that not all hosting environments allow flexibility with
+	| public paths. That's why we allow you to change where your public path
+	| is below.
+	|
+	*/
+
+	'public' => __DIR__.'/../public',
+
+	/*
+	|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+	| Base Path
+	|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+	|
+	| You probably shouldn't be editing this.
+	|
+	*/
+
+	'base' => __DIR__.'/..',
+
+);
