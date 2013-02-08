4355a334f754fff68093e0c17d0bfc43cc6ecd7e
Taylor Otwell <taylorotwell@gmail.com>
2013-02-08 09:31:43 -0600

    update session config.

--- app/config/session.php
+++ app/config/session.php
@@ -83,4 +83,17 @@ return array(
 
 	'lottery' => array(2, 100),
 
+	/*
+	|--------------------------------------------------------------------------
+	| Session Cookie Name
+	|--------------------------------------------------------------------------
+	|
+	| Here you may change the name of the cookie used to identify a session
+	| instance by ID. The name specified here will get used every time a
+	| new session cookie is created by the framework for every driver.
+	|
+	*/
+
+	'cookie' => 'laravel_session',
+
 );
