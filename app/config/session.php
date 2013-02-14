79d18a3122e4211b3f8b32587df34cae20bb070c
Taylor Otwell <taylorotwell@gmail.com>
2013-02-14 08:32:38 -0600

    Update app/config/session.php

--- app/config/session.php
+++ app/config/session.php
@@ -8,7 +8,7 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| This option controls the default session "driver" that will be used on
-	| requets. By default, we will use the light-weight cookie driver but
+	| requests. By default we will use the light-weight cookie driver but
 	| you may specify any of the other wonderful drivers provided here.
 	|
 	| Supported: "cookie", file", "database", "apc",
