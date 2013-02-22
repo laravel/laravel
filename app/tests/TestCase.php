6d3eabf9b1fa5fd270dffd8674b4160860abbb32
Ben Corlett <bencorlett@me.com>
2013-02-22 14:49:15 -0500

    Remove tabs / spaces mix.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -3,17 +3,17 @@
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
     /**
-     * Creates the application.
-     *
-     * @return Symfony\Component\HttpKernel\HttpKernelInterface
-     */
-    public function createApplication()
-    {
-    	$unitTesting = true;
+	 * Creates the application.
+	 *
+	 * @return Symfony\Component\HttpKernel\HttpKernelInterface
+	 */
+	public function createApplication()
+	{
+		$unitTesting = true;
 
-        $testEnvironment = 'testing';
+		$testEnvironment = 'testing';
 
-    	return require __DIR__.'/../../bootstrap/start.php';
-    }
+		return require __DIR__.'/../../bootstrap/start.php';
+	}
 
 }
