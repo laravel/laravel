c45ef54c5867fec7e604d652b2bc13012370fbb6
Bering <bering@ringlogic.com>
2013-02-22 17:25:49 -0500

    Fixes the validation error message for regex validation rule

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -37,7 +37,6 @@ return array(
 	"in"              => "The selected :attribute is invalid.",
 	"integer"         => "The :attribute must be an integer.",
 	"ip"              => "The :attribute must be a valid IP address.",
-	"match"           => "The :attribute format is invalid.",
 	"max"             => array(
 		"numeric"     => "The :attribute must be less than :max.",
 		"file"        => "The :attribute must be less than :max kilobytes.",
@@ -51,6 +50,7 @@ return array(
 	),
 	"notin"           => "The selected :attribute is invalid.",
 	"numeric"         => "The :attribute must be a number.",
+	"regex"           => "The :attribute format is invalid.",
 	"required"        => "The :attribute field is required.",
 	"required_with"   => "The :attribute field is required when :values is present.",
 	"same"            => "The :attribute and :other must match.",
