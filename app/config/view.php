c1cd1a1cb5fa973b7e9cb8742baac1ea8124065e
Ben Corlett <bencorlett@me.com>
2013-03-04 09:06:08 +1100

    Adding configuration for Blade content tags.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- app/config/view.php
+++ app/config/view.php
@@ -28,4 +28,32 @@ return array(
 
 	'pagination' => 'pagination::slider',
 
+	/*
+	|--------------------------------------------------------------------------
+	| Blade Content Tags
+	|--------------------------------------------------------------------------
+	|
+	| When using "blade" views, you may specify the content tags used in Blade.
+	| Defaults are provided, but feel free to change them to avoid conflicts
+	| with other engines.
+	|
+	| Tags must be a two-value array, where the first element is the opening
+	| tag and the last element is the closing tag.
+	|
+	*/
+
+	'content_tags' => array('{{', '}}'),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Blade Raw Content Tags
+	|--------------------------------------------------------------------------
+	|
+	| These are the tags used when outputting raw (not escaped) content in a
+	| Blade view. The same rules apply as 'content_tags' above.
+	|
+	*/
+
+	'raw_content_tags' => array('{{{', '}}}'),
+
 );
