216bc077332a18743a811739e9361998672fd262
Taylor Otwell <taylorotwell@gmail.com>
2013-03-13 11:32:36 -0500

    Added new options to session config.

--- app/config/session.php
+++ app/config/session.php
@@ -96,4 +96,30 @@ return array(
 
 	'cookie' => 'laravel_session',
 
+	/*
+	|--------------------------------------------------------------------------
+	| Session Cookie Path
+	|--------------------------------------------------------------------------
+	|
+	| The session cookie path determines the path for which the cookie will
+	| be regarded as available. Typically, this will be the root path of
+	| your application but you are free to change this when necessary.
+	|
+	*/
+
+	'path' => '/',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session Cookie Domain
+	|--------------------------------------------------------------------------
+	|
+	| Here you may change the domain of the cookie used to identify a session
+	| in your application. This will determine which domains the cookie is
+	| available to in your application. A sensible default has been set.
+	|
+	*/
+
+	'domain' => null,
+
 );
