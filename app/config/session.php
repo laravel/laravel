6253fe0ae57da093ba56f0c56024ef87efb3a9be
AndreiCanta <andrei.canta@deiu.ro>
2013-03-20 11:36:11 +0200

    Typo: missing quotes in session.php doc block

--- app/config/session.php
+++ app/config/session.php
@@ -11,7 +11,7 @@ return array(
 	| requests. By default we will use the light-weight cookie driver but
 	| you may specify any of the other wonderful drivers provided here.
 	|
-	| Supported: "cookie", file", "database", "apc",
+	| Supported: "cookie", "file", "database", "apc",
 	|            "memcached", "redis", "array"
 	|
 	*/
