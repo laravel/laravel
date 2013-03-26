857dd663fdb8590b5f6a327747473c9cd6e33c16
Taylor Otwell <taylorotwell@gmail.com>
2013-03-26 11:22:41 -0500

    Add driver to mail config.

--- app/config/mail.php
+++ app/config/mail.php
@@ -4,6 +4,21 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
+	| Mail Driver
+	|--------------------------------------------------------------------------
+	|
+	| Laravel supports both SMTP and PHP's "mail" function as drivers for the
+	| sending of e-mail. You may specify which one you're using throughout
+	| your application here. By default, Laravel is setup for SMTP mail.
+	|
+	| Supported: "smtp", "mail"
+	|
+	*/
+
+	'driver' => 'smtp',
+
+	/*
+	|--------------------------------------------------------------------------
 	| SMTP Host Address
 	|--------------------------------------------------------------------------
 	|
