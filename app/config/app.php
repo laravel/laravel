6838586e8229d3e60ee02c4bab7183200bff51b9
Sam Jordan <sam@netsells.co.uk>
2013-04-15 09:41:23 +0100

    Indenting!

--- app/config/app.php
+++ app/config/app.php
@@ -15,16 +15,16 @@ return array(
 
 	'debug' => true,
 
-/*
-|--------------------------------------------------------------------------
-| Application URL
-|--------------------------------------------------------------------------
-|
-| This URL is used by the console to properly generate URLs when using
-| the Artisan command line tool. You should set this to the root of
-| your application so that it is used when running Artisan tasks.
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Application URL
+	|--------------------------------------------------------------------------
+	|
+	| This URL is used by the console to properly generate URLs when using
+	| the Artisan command line tool. You should set this to the root of
+	| your application so that it is used when running Artisan tasks.
+	|
+	*/
 
 	'url' => 'http://localhost',
 
