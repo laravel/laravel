452956551b701bfe40631894cf4e6c0568c5fd2f
Taylor Otwell <taylorotwell@gmail.com>
2013-04-17 00:29:48 -0500

    Fix comments on session.

--- app/config/session.php
+++ app/config/session.php
@@ -11,7 +11,7 @@ return array(
 	| requests. By default we will use the light-weight cookie driver but
 	| you may specify any of the other wonderful drivers provided here.
 	|
-	| Supported: "native", "file", "database", "apc",
+	| Supported: "native", "database", "apc",
 	|            "memcached", "redis", "array"
 	|
 	*/
@@ -36,7 +36,7 @@ return array(
 	| Session File Location
 	|--------------------------------------------------------------------------
 	|
-	| When using the "file" session driver, we need a location where session
+	| When using the native session driver, we need a location where session
 	| files may be stored. A default has been set for you but a different
 	| location may be specified. This is only needed for file sessions.
 	|
