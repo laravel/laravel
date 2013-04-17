9c19025ca50e4aaee1088fc50be4df3ec22b23d5
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-04-17 19:30:40 +0300

    Turkish Language Files

---
+++ app/lang/tr/reminders.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Language
+ * @version  4.x
+ * @author   Sinan Eldem <sinan@sinaneldem.com.tr>
+ * @link     http://sinaneldem.com.tr
+ */
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Şifreler altı karakter olmalı ve onay ile eşleşmelidir.",
+
+	"user"     => "Bu e-mail adresi ile kayıtlı üye bulunamıyor.",
+
+	"token"    => "Şifre sıfırlama bileti geçersiz.",
+
+);
