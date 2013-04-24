763f1d5181053bcf6b714d1dc905a6e9483e6215
Taylor Otwell <taylorotwell@gmail.com>
2013-04-23 22:27:51 -0500

    Use Redirect::guest in "auth" filter.

--- app/filters.php
+++ app/filters.php
@@ -35,7 +35,7 @@ App::after(function($request, $response)
 
 Route::filter('auth', function()
 {
-	if (Auth::guest()) return Redirect::route('login');
+	if (Auth::guest()) return Redirect::guest('login');
 });
 
 
