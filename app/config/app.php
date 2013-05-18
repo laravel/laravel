0508b6f5d44bac06270819d6f30ce321038ee353
warren <warren@onehomebase.com>
2013-05-19 02:55:53 +0800

    Fix the error "Class 'Html' not found" in case sensitive OS filesystem
    Signed-off-by: warren <warren@onehomebase.com>

--- app/config/app.php
+++ app/config/app.php
@@ -144,7 +144,7 @@ return array(
 		'File'            => 'Illuminate\Support\Facades\File',
 		'Form'            => 'Illuminate\Support\Facades\Form',
 		'Hash'            => 'Illuminate\Support\Facades\Hash',
-		'Html'            => 'Illuminate\Support\Facades\Html',
+		'Html'            => 'Illuminate\Support\Facades\HTML',
 		'Input'           => 'Illuminate\Support\Facades\Input',
 		'Lang'            => 'Illuminate\Support\Facades\Lang',
 		'Log'             => 'Illuminate\Support\Facades\Log',
