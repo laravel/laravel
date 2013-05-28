58e6024e13283a6fbf80a1e8db580a5e3f95b8a9
Adam Nojszewski <adam@nojszewski.com>
2013-05-28 22:53:15 +0200

    Polish translation
    Polish translation for pagination, reminders and validation.

---
+++ app/lang/pl/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Hasło musi mieć przynajmniej sześć znaków i zgadzać się z potwierdzeniem.",
+
+	"user"     => "Nie znaleziono użytkownika z takim adresem e-mail.",
+
+	"token"    => "Token resetowania hasła jest nieprawidłowy.",
+
+);
