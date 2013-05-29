84fced783039bd98297ebe0a08cf000d1ee651f1
Dwight Watson <dwight@studentservices.com.au>
2013-05-29 17:56:10 +1000

    Fixed example test due to new landing page.
    Signed-off-by: Dwight Watson <dwight@studentservices.com.au>

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase {
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 
-		$this->assertCount(1, $crawler->filter('h1:contains("Hello World!")'));
+		$this->assertCount(1, $crawler->filter('h1:contains("You have arrived.")'));
 	}
 
 }
