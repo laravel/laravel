ef697d7ed3bea58b542e49b6a2480ae5e7c1db9b
Patrick Bielen <pbielen@gmail.com>
2013-05-29 13:25:41 +0200

    Dutch Translation

---
+++ app/lang/nl/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Paswoorden moeten 6 tekens lang zijn en overeenkomen met de herhaling.",
+
+	"user"     => "We kunnen geen gebruiker vinden met dat e-mail adres.",
+
+	"token"    => "Deze token is niet geldig.",
+
+);
