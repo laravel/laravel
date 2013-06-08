e526c088005409a360bd844d0382a26b04c2f2cf
Taylor Otwell <taylorotwell@gmail.com>
2013-06-08 13:14:10 -0500

    Added "pretend" option to mail config.

--- app/config/mail.php
+++ app/config/mail.php
@@ -108,4 +108,17 @@ return array(
 
 	'sendmail' => '/usr/sbin/sendmail -bs',
 
+	/*
+	|--------------------------------------------------------------------------
+	| Mail "Pretend"
+	|--------------------------------------------------------------------------
+	|
+	| When this option is enabled, e-mail will not actually be sent over the
+	| web and will instead be written to your application's logs files so
+	| you may inspect the message. This is great for local development.
+	|
+	*/
+
+	'pretend' => false,
+
 );
