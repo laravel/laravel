cbf56e76aae304313f69dcd591646ebea8e07108
Taylor Otwell <taylorotwell@gmail.com>
2013-06-12 21:11:40 -0500

    added remote config.

---
+++ app/config/remote.php
@@ -0,0 +1,39 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Remote Connection Name
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify the default connection that will be used for SSH
+	| operations. This name should correspond to a connection name below
+	| in the server list. Each connection will be manually accessible.
+	|
+	*/
+
+	'default' => 'production',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Remote Server Connections
+	|--------------------------------------------------------------------------
+	|
+	| These are the servers that will be accessible via the SSH task runner
+	| facilities of Laravel. This feature radically simplifies executing
+	| tasks on your servers, such as deploying out these applications.
+	|
+	*/
+
+	'connections' => array(
+
+		'production' => array(
+			'host'     => '',
+			'username' => 'root',
+			'password' => '',
+		),
+
+	),
+
+);
