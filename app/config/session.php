45f2234bd6cb249f961c0dc6a82dd168dd243d4a
Michael Meyer <m.meyer2k@gmail.com>
2013-08-05 16:30:06 -0500

    Update comment for clarity

--- app/config/session.php
+++ app/config/session.php
@@ -24,7 +24,7 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| Here you may specify the number of minutes that you wish the session
-	| to be allowed to remain idle for it is expired. If you want them
+	| to be allowed to remain idle before it is expired. If you want them
 	| to immediately expire when the browser closes, set it to zero.
 	|
 	*/
