268831ed7636fc93ca3fdeff3af18381fe1f5f3a
robi50 <fivoneto@outlook.com>
2013-08-25 20:47:38 +0300

    update css code for beauty

--- app/views/hello.php
+++ app/views/hello.php
@@ -7,9 +7,9 @@
         @import url(//fonts.googleapis.com/css?family=Lato:300,400,700);
 
         body {
-            margin:0;
-            font-family:'Lato', sans-serif;
-            text-align:center;
+            margin: 0;
+            font-family: 'Lato', sans-serif;
+            text-align: center;
             color: #999;
         }
 
@@ -24,22 +24,22 @@
         }
 
         a, a:visited {
-            color:#FF5949;
-            text-decoration:none;
+            color: #FF5949;
+            text-decoration :none;
         }
 
         a:hover {
-            text-decoration:underline;
+            text-decoration: underline;
         }
 
         ul li {
-            display:inline;
-            margin:0 1.2em;
+            display: inline;
+            margin: 0 1.2em;
         }
 
         p {
-            margin:2em 0;
-            color:#555;
+            margin: 2em 0;
+            color: #555;
         }
     </style>
 </head>
