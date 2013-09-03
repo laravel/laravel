bcf5a85e7ef3f82b380f12927b2eb1f7c361fca2
Dries Vints <dries.vints@gmail.com>
2013-09-03 10:35:15 +0200

    Clarify expire time type
    It wasn't really clear in which time the current expire time was set. After digging into the core it became clear that it was set in minutes.

--- app/config/auth.php
+++ app/config/auth.php
@@ -50,7 +50,8 @@ return array(
 	|
 	| Here you may set the settings for password reminders, including a view
 	| that should be used as your password reminder e-mail. You will also
-	| be able to set the name of the table that holds the reset tokens.
+	| be able to set the name of the table that holds the reset tokens. The 
+	| expire time is set in minutes.
 	|
 	*/
 
