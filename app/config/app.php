42ff9d0ebb7e8307a755440a4c473803aa75dd5a
Taylor Otwell <taylorotwell@gmail.com>
2013-09-06 00:32:42 -0500

    Setup live debugger in app.

--- app/config/app.php
+++ app/config/app.php
@@ -90,6 +90,7 @@ return array(
 		'Illuminate\Cookie\CookieServiceProvider',
 		'Illuminate\Database\DatabaseServiceProvider',
 		'Illuminate\Encryption\EncryptionServiceProvider',
+		'Illuminate\Exception\LiveServiceProvider',
 		'Illuminate\Filesystem\FilesystemServiceProvider',
 		'Illuminate\Hashing\HashServiceProvider',
 		'Illuminate\Html\HtmlServiceProvider',
