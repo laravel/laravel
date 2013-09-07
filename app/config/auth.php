fbd93f6997a64abb1457aa7328a39ff3df8c5a18
Taylor Otwell <taylorotwell@gmail.com>
2013-09-06 23:42:43 -0500

    added note about expiry time on reminders.

--- app/config/auth.php
+++ app/config/auth.php
@@ -52,6 +52,10 @@ return array(
 	| that should be used as your password reminder e-mail. You will also
 	| be able to set the name of the table that holds the reset tokens.
 	|
+	| The "expire" time is the number of minutes that the reminder should be
+	| considered valid. This security feature keeps tokens short-lived so
+	| they have less time to be guessed. You may change this as needed.
+	|
 	*/
 
 	'reminder' => array(
