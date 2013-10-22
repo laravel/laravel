6dd766c0c065565d50348c283232be1d42e4a759
Taylor Otwell <taylorotwell@gmail.com>
2013-10-22 16:11:10 -0500

    Fix typos.

--- app/config/database.php
+++ app/config/database.php
@@ -92,7 +92,7 @@ return array(
 	|
 	| This table keeps track of all the migrations that have already run for
 	| your application. Using this information, we can determine which of
-	| the migrations on disk have not actually be run in the databases.
+	| the migrations on disk haven't actually been run in the database.
 	|
 	*/
 
