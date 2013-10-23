ed78f78fa927376ea5e254092432ce8a92047b47
Taylor Otwell <taylorotwell@gmail.com>
2013-10-22 21:20:18 -0500

    Fix comment.

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -9,7 +9,7 @@ return array(
 	|
 	| The following language lines contain the default error messages used by
 	| the validator class. Some of these rules have multiple versions such
-	| such as the size rules. Feel free to tweak each of these messages.
+	| as the size rules. Feel free to tweak each of these messages here.
 	|
 	*/
 
