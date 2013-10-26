ce64714b2fda6d6a14ea6649a03aa59bfbd6d079
Taylor Otwell <taylorotwell@gmail.com>
2013-10-26 11:27:40 -0500

    Tweak front controller. Htaccess.

--- public/index.php
+++ public/index.php
@@ -36,21 +36,6 @@ $app = require_once __DIR__.'/../bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
-| Capture The Request
-|--------------------------------------------------------------------------
-|
-| Next we will capture the HTTP request into an instance of the Symfony
-| request class. We will then pass that to a Laravel application for
-| processing and return the response we receive back from the app.
-|
-*/
-
-use Symfony\Component\HttpFoundation\Request;
-
-$request = Request::createFromGlobals();
-
-/*
-|--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
@@ -61,35 +46,6 @@ $request = Request::createFromGlobals();
 |
 */
 
-$response = with(new Stack\Builder)
-				->push('Illuminate\Foundation\TrailingSlashRedirector')
-				->resolve($app)
-				->handle($request);
+use Symfony\Component\HttpFoundation\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Close The Application & Send Response
-|--------------------------------------------------------------------------
-|
-| When closing the application, the session cookies will be set on the
-| request. Also, this is an opportunity to finish up any other work
-| that needs to be done before sending this response to browsers.
-|
-*/
-
-$app->callCloseCallbacks($request, $response);
-
-$response->send();
-
-/*
-|--------------------------------------------------------------------------
-| Shutdown The Application
-|--------------------------------------------------------------------------
-|
-| Once the app has finished running we'll fire off the shutdown events
-| so that any end work may be done by an application before we shut
-| off the process. This is the final thing to happen to requests.
-|
-*/
-
-$app->terminate($request, $response);
+$app->run(Request::createFromGlobals());
