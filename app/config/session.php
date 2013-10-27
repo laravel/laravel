ccdc7141aa87dd4e931098e3cfe596667f8ea1f5
Taylor Otwell <taylorotwell@gmail.com>
2013-10-26 19:03:30 -0500

    Tweak session config.

--- app/config/session.php
+++ app/config/session.php
@@ -25,11 +25,13 @@ return array(
 	|
 	| Here you may specify the number of minutes that you wish the session
 	| to be allowed to remain idle before it expires. If you want them
-	| to immediately expire when the browser closes, set it to zero.
+	| to immediately expire on the browser closing, set that option.
 	|
 	*/
 
 	'lifetime' => 120,
+
+	'expire_on_close' => false,
 
 	/*
 	|--------------------------------------------------------------------------
