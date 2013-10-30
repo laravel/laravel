eea4713e7c2505c87b8b035024da0925bb704fd2
Taylor Otwell <taylorotwell@gmail.com>
2013-10-30 09:46:32 -0500

    Fix grammar.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -50,7 +50,7 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 | Load The Application
 |--------------------------------------------------------------------------
 |
-| Here we will load the Illuminate application. We'll keep this is in a
+| Here we will load this Illuminate application. We will keep this in a
 | separate location so we can isolate the creation of an application
 | from the actual running of the application with a given request.
 |
