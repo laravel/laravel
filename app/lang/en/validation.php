d895d2c972578c8cc18174ca2874074028a85edc
Andreas Lutro <anlutro@gmail.com>
2013-10-31 15:19:49 +0100

    added required_without_all

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -59,6 +59,7 @@ return array(
 	"required_if"      => "The :attribute field is required when :other is :value.",
 	"required_with"    => "The :attribute field is required when :values is present.",
 	"required_without" => "The :attribute field is required when :values is not present.",
+	"required_without_all" => "The :attribute field is required when none of :values are present.",
 	"same"             => "The :attribute and :other must match.",
 	"size"             => array(
 		"numeric" => "The :attribute must be :size.",
