f387853ecadcdd1f87a22b5c3dad41abeb71a976
Taylor Otwell <taylorotwell@gmail.com>
2013-11-01 08:46:15 -0500

    Added secure option.

--- app/config/session.php
+++ app/config/session.php
@@ -124,4 +124,17 @@ return array(
 
 	'domain' => null,
 
+	/*
+	|--------------------------------------------------------------------------
+	| HTTPS Only Cookies
+	|--------------------------------------------------------------------------
+	|
+	| By setting this option to true, session cookies will only be sent back
+	| to the server if the browser has a HTTPS connection. This will keep
+	| the cookie from being sent to you if it can not be done securely.
+	|
+	*/
+
+	'secure' => false,
+
 );
