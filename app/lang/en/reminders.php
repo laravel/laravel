55239c4c8ad41c0083484e10ca4de68a5fc74200
Taylor Otwell <taylorotwell@gmail.com>
2013-11-14 21:36:12 -0600

    Tweak reminder language lines.

--- app/lang/en/reminders.php
+++ app/lang/en/reminders.php
@@ -13,10 +13,12 @@ return array(
 	|
 	*/
 
-	"password" => "Passwords must be at least six characters long and match the confirmation.",
+	"password" => "Passwords must be at least six characters and match the confirmation.",
 
-	"user"     => "We can't find a user with that e-mail address.",
+	"user" => "We can't find a user with that e-mail address.",
 
-	"token"    => "This password reset token is invalid.",
+	"token" => "This password reset token is invalid.",
+
+	"sent" => "Password reminder sent!",
 
 );
