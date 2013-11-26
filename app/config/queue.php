9186d7a31dbd13aa436c526c4fc1220f2c2db7dc
Taylor Otwell <taylorotwell@gmail.com>
2013-11-26 10:11:29 -0600

    Adding failed queue job options.

--- app/config/queue.php
+++ app/config/queue.php
@@ -58,7 +58,24 @@ return array(
 		'redis' => array(
 			'driver' => 'redis',
 			'queue'  => 'default',
-		),		
+		),
+
+	),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Failed Queue Jobs
+	|--------------------------------------------------------------------------
+	|
+	| These options configure the behavior of failed queue job logging so you
+	| can control which database and table are used to store the jobs that
+	| have failed. You may change them to any database / table you wish.
+	|
+	*/
+
+	'failed' => array(
+
+		'database' => 'mysql', 'table' => 'failed_jobs',
 
 	),
 
