aeda6b13cdfcd662089c30d428839965c7443ab9
Taylor Otwell <taylorotwell@gmail.com>
2014-01-18 21:58:24 -0600

    Shorten path line.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -54,7 +54,8 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 |
 */
 
-$framework = $app['path.base'].'/vendor/laravel/framework/src';
+$framework = $app['path.base'].
+                 '/vendor/laravel/framework/src';
 
 require $framework.'/Illuminate/Foundation/start.php';
 
