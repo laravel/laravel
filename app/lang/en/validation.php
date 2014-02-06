6ac2e9bf379398aa227277ca7a4f25f1768c4752
Linus Odenring <linus@odenring.se>
2014-02-06 19:50:21 +0100

    Update validation.php

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -34,7 +34,7 @@ return array(
 	"digits"               => "The :attribute must be :digits digits.",
 	"digits_between"       => "The :attribute must be between :min and :max digits.",
 	"email"                => "The :attribute format is invalid.",
-	"exists"               => "The selected :attribute is already existing.",
+	"exists"               => "The selected :attribute are already existing.",
 	"image"                => "The :attribute must be an image.",
 	"in"                   => "The selected :attribute is invalid.",
 	"integer"              => "The :attribute must be an integer.",
