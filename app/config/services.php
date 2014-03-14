7d60aa4235b21aff26e2a720bbd5480c3651db8f
Taylor Otwell <taylorotwell@gmail.com>
2014-03-14 16:22:03 -0500

    Working on configuration files.

---
+++ app/config/services.php
@@ -0,0 +1,16 @@
+<?php return array(
+
+	'mailgun' => array(
+		'secret' => '',
+	),
+
+	'mandrill' => array(
+		'secret' => '',
+	),
+
+	'stripe' => array(
+		'model'  => 'User',
+		'secret' => '',
+	),
+
+);
