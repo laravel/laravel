4aa42ee2802f8418c25a725fcc2518597a36d4bf
Taylor Otwell <taylorotwell@gmail.com>
2014-04-11 11:00:57 -0500

    Set default cipher.

--- app/config/app.php
+++ app/config/app.php
@@ -67,6 +67,8 @@ return array(
 
 	'key' => 'YourSecretKey!!!',
 
+	'cipher' => MCRYPT_RIJNDAEL_128,
+
 	/*
 	|--------------------------------------------------------------------------
 	| Autoloaded Service Providers
