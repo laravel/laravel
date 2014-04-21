65db159b2d2204d091da07de351171d207b8b779
Juukie14 <Juukie@gmail.com>
2014-04-21 20:58:38 +0200

    Added remember_token to protected $hidden variable

--- app/models/User.php
+++ app/models/User.php
@@ -17,7 +17,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password');
+	protected $hidden = array('password', 'remember_token');
 
 	/**
 	 * Get the unique identifier for the user.
