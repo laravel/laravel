c7ff71111b623ea6fee7f32ac842ae25657e5fb3
Taylor Otwell <taylorotwell@gmail.com>
2014-05-13 08:14:16 -0500

    Change namespace for RemindableTrait.

--- app/models/User.php
+++ app/models/User.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
