4ebab0db205ffe3765cadf863bfb2c52dfa39fc3
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2014-06-29 17:55:59 +0200

    avoid logical operators - replaced 'and' by '&&'
    "The and operator does not have the same precedence as &&. This could lead to unexpected behavior, use && instead."
    see : https://insight.sensiolabs.com/projects/eedbee2c-cfb3-4642-a8e3-eb319b909987/analyses/1

--- server.php
+++ server.php
@@ -11,7 +11,7 @@ $requested = $paths['public'].$uri;
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' and file_exists($requested))
+if ($uri !== '/' && file_exists($requested))
 {
 	return false;
 }
