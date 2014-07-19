9c0de2186f8dbdccda2203db584350bc9192e8c9
Josh Hornby <hello@joshhornby.co.uk>
2014-07-19 22:54:30 +0100

    Hash a users password by default

--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,6 @@
 <?php
 
+use Hash;
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
@@ -22,5 +23,15 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
+
+    /**
+     * Hashing a users password.
+     *
+     * @param $password
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
 
 }
