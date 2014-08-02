3a34a99694c68256be700fa7e6d7553ad9256994
Taylor Otwell <taylorotwell@gmail.com>
2014-08-01 23:51:30 -0500

    Working on default route setup.

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -20,11 +20,8 @@ require __DIR__.'/filters.php';
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| then declare the method to execute when that URI is requested.
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', 'HomeController@index');
