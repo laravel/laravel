e1f4797bc230615d260cb67dff87648e69027237
Tuva Ergün <yasin@ergn.org>
2014-08-13 20:31:18 +0300

    image eklenti alias
    image eklentisi için alias detayları

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Intervention\Image\ImageServiceProvider',
 
 	),
 
@@ -188,6 +189,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
+		'Image' => 'Intervention\Image\Facades\Image',
 
 	),
 
