a27f7486dcc9e8f230a00280e67ce9f9e8e227a1
Taylor Otwell <taylorotwell@gmail.com>
2014-08-29 13:58:51 +0200

    Extend model.

--- app/User.php
+++ app/User.php
@@ -1,12 +1,12 @@
 <?php namespace App;
 
-use Eloquent;
 use Illuminate\Auth\UserTrait;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Contracts\Auth\User as UserContract;
 use Illuminate\Contracts\Auth\Remindable as RemindableContract;
 
-class User extends Eloquent implements UserContract, RemindableContract {
+class User extends Model implements UserContract, RemindableContract {
 
 	use UserTrait, RemindableTrait;
 
