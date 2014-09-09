eff1f84050c9e2711ce552000d24a47e7482c06d
Taylor Otwell <taylorotwell@gmail.com>
2014-09-09 10:01:59 -0500

    Remove extra lines.

--- app/Providers/ErrorServiceProvider.php
+++ app/Providers/ErrorServiceProvider.php
@@ -17,7 +17,6 @@ class ErrorServiceProvider extends ServiceProvider {
 		// even register several error handlers to handle different types of
 		// exceptions. If nothing is returned, the default error view is
 		// shown, which includes a detailed stack trace during debug.
-
 		App::error(function(Exception $e)
 		{
 			Log::error($e);
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,7 +31,6 @@ class RouteServiceProvider extends ServiceProvider {
 			// Once the application has booted, we will include the default routes
 			// file. This "namespace" helper will load the routes file within a
 			// route group which automatically sets the controller namespace.
-
 			$this->namespaced(function()
 			{
 				require app_path().'/Http/routes.php';
