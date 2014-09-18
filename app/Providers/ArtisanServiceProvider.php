7c8b8e7dba900264cdb436495bd7f557b8bc0c08
Taylor Otwell <taylorotwell@gmail.com>
2014-09-17 22:30:43 -0500

    Make Artisan service provider be deferred.

--- app/Providers/ArtisanServiceProvider.php
+++ app/Providers/ArtisanServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\ServiceProvider;
 class ArtisanServiceProvider extends ServiceProvider {
 
 	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
 	 * Register the service provider.
 	 *
 	 * @return void
@@ -13,6 +20,16 @@ class ArtisanServiceProvider extends ServiceProvider {
 	public function register()
 	{
 		$this->commands('App\Console\InspireCommand');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return ['App\Console\InspireCommand'];
 	}
 
 }
