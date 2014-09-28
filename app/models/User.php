ce22e70c2e2a878b816f06cfccebec5c851da35c
Bart Nagel <bart@tremby.net>
2014-09-27 22:45:10 -0700

    Liferaft app showing timezone issue

--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,11 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+	/**
+	 * The 'born' attribute is a date
+	 */
+	public function getDates() {
+		return ['born'];
+	}
+
 }
--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,26 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	ob_start();
+
+	$user = new User;
+	echo "At start, \$user->born is: "; var_dump($user->born);
+
+	$born = \Carbon\Carbon::parse('1983-01-01 12:00:00 UTC');
+	echo "New UTC Carbon date: {$born->toRssString()}\n";
+
+	echo "Change its timezone to America/Vancouver\n";
+	$born->timezone('America/Vancouver');
+	echo "Vancouver Carbon date: {$born->toRssString()}\n";
+
+	echo "Now set \$user->born to this\n";
+	$user->born = $born;
+
+	echo "Now retrieve \$user->born: {$user->born->toRssString()}\n";
+
+	echo "We should see 12 noon +0000, but we see 4am +0000\n";
+
+	$response = Response::make(ob_get_clean(), 200);
+	$response->header('Content-Type', 'text/plain');
+	return $response;
 });
