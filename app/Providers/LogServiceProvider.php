65dce4d093b48c00312531a6b2bea49811df2391
Taylor Otwell <taylorotwell@gmail.com>
2014-10-02 20:35:44 -0500

    Shorten type-hint in docblock.

--- app/Providers/LogServiceProvider.php
+++ app/Providers/LogServiceProvider.php
@@ -8,7 +8,7 @@ class LogServiceProvider extends ServiceProvider {
 	/**
 	 * Configure the application's logging facilities.
 	 *
-	 * @param  \Illuminate\Contracts\Logging\Log  $log
+	 * @param  Log  $log
 	 * @return void
 	 */
 	public function boot(Log $log)
