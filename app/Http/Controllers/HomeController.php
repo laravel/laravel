8a2f8bb2c746ae3a6ef40842d987c4ceed72ff74
Taylor Otwell <taylorotwell@gmail.com>
2014-10-03 21:30:51 -0500

    Working on a few files.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,10 +13,13 @@ class HomeController extends Controller {
 	| based routes. That's great! Here is an example controller method to
 	| get you started. To route to this controller, just add the route:
 	|
-	|	Route::get('/', 'HomeController@index');
+	|	$router->get('/', 'HomeController@index');
 	|
 	*/
 
+	/**
+	 * @Get("/", as="home")
+	 */
 	public function index()
 	{
 		return view('hello');
