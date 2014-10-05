9726ef7563f1bc02b24bc1c3b26808379c659236
sandrosansan <sandrosansan@gmail.com>
2014-10-05 02:14:54 -0300

    Add language Portuguese
    I suggest create a new language for the laravel on Portuguese, this is my first contribution for start it.

---
+++ app/lang/pt/pagination.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Linhas de paginação
+	|--------------------------------------------------------------------------
+	|
+	| A seguir os textos que irão aparecer na paginação da biblioeta, para criar
+	| uma simples paginação de links. Você pode se sentir livre para alterar
+	| qualquer coisa que quiser, customizar suas visualizações para 
+	| melhorar sua aplicação.
+	|
+	*/
+
+	'previous' => '&laquo; Anterior',
+
+	'next'     => 'Próximo &raquo;',
+
+);
