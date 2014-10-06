43e8c60a111737a1363e67f000f7b55d496dea0b
Taylor Otwell <taylorotwell@gmail.com>
2014-10-06 15:35:23 -0500

    Write a base app service provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,8 @@
 <?php namespace App\Providers;
 
 use Illuminate\Routing\Router;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Stack\Builder as Stack;
+use Illuminate\Foundation\Support\Providers\AppServiceProvider as ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -30,37 +30,5 @@ class AppServiceProvider extends ServiceProvider {
 		'Illuminate\Session\Middleware\Reader',
 		'Illuminate\Session\Middleware\Writer',
 	];
-
-	/**
-	 * Bootstrap any necessary services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		// This service provider is a convenient place to register your services
-		// in the IoC container. If you wish, you may make additional methods
-		// or service providers to keep the code more focused and granular.
-
-		$this->app->stack(function(Stack $stack, Router $router)
-		{
-			return $stack
-				->middleware($this->stack)
-				->then(function($request) use ($router)
-				{
-					return $router->dispatch($request);
-				});
-			});
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
 
 }
