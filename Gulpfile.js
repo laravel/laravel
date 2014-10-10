a2dacbb0faa21ca6364d09eccf52fecc22bfaa9a
Taylor Otwell <taylorotwell@gmail.com>
2014-10-10 12:51:20 -0500

    Something special.

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+var elixir = require('./vendor/laravel/elixir/Elixir');
+
+/*
+ |----------------------------------------------------------------
+ | Have a Drink!
+ |----------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic
+ | Gulp tasks for your Laravel application. Elixir supports
+ | several common CSS, JavaScript and even testing tools!
+ |
+ */
+
+elixir(function(mix) {
+    mix.less("bootstrap.less")
+       .routes()
+       .events()
+       .phpUnit();
+});
