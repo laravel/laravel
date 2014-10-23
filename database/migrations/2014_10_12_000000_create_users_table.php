36e0014a6a4bdb6c33e2146c0d38203e5cb4aa7a
Taylor Otwell <taylorotwell@gmail.com>
2014-10-22 19:16:02 -0500

    Fix a few password reminder things.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration {
 			$table->increments('id');
 			$table->string('email')->unique();
 			$table->string('password', 60);
+			$table->rememberToken();
 			$table->timestamps();
 		});
 	}
