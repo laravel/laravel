3dfc5fc768352d6a883ea9756a2ce73b9ca3b9a6
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-10-23 22:26:50 +0200

    bootstrap/paths.php disappeared
    Fixing php artisan serve functionality

--- server.php
+++ server.php
@@ -4,9 +4,9 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = urldecode($uri);
 
-$paths = require __DIR__.'/bootstrap/paths.php';
+$public = __DIR__ . '/public';
 
-$requested = $paths['public'].$uri;
+$requested = $public . $uri;
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
@@ -16,4 +16,4 @@ if ($uri !== '/' and file_exists($requested))
 	return false;
 }
 
-require_once $paths['public'].'/index.php';
+require_once $public . '/index.php';
