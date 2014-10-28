08c6996d77feab2a5b4f3437c8253267a9e23a96
Nathaniel Blackburn <nblackburn@users.noreply.github.com>
2014-10-28 19:06:26 +0000

    Update RouteServiceProvider.php
    Removed the CSRF provider as it is specified in the Kernel.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,6 @@ class RouteServiceProvider extends ServiceProvider {
 	protected $middleware = [
 		'auth' => 'App\Http\Middleware\Authenticated',
 		'auth.basic' => 'App\Http\Middleware\AuthenticatedWithBasicAuth',
-		'csrf' => 'App\Http\Middleware\VerifyCsrfToken',
 		'guest' => 'App\Http\Middleware\IsGuest',
 	];
 
