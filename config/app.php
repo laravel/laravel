3c614ac5d622a73f607f489e9a9f382035a9fa44
Taylor Otwell <taylorotwell@gmail.com>
2014-11-03 10:26:04 -0600

    Working on logging.

--- config/app.php
+++ config/app.php
@@ -84,6 +84,21 @@ return [
 
 	/*
 	|--------------------------------------------------------------------------
+	| Logging Configuration
+	|--------------------------------------------------------------------------
+	|
+	| Here you may configure the log settings for your application. Out of
+	| the box, Laravel uses the Monolog PHP logging library. This gives
+	| you a variety of powerful log handlers / formatters to utilize.
+	|
+	| Available Settings: "single", "daily", "syslog"
+	|
+	*/
+
+	'log' => 'daily',
+
+	/*
+	|--------------------------------------------------------------------------
 	| Autoloaded Service Providers
 	|--------------------------------------------------------------------------
 	|
