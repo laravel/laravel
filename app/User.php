55965c73c5d15c61c2eac5d53662e57ca81070d0
Taylor Otwell <taylorotwell@gmail.com>
2014-11-07 10:00:30 -0600

    Rename.

--- app/User.php
+++ app/User.php
@@ -1,14 +1,14 @@
 <?php namespace App;
 
-use Illuminate\Auth\Authenticates;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\ResetsPassword;
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\User as UserContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements UserContract, CanResetPasswordContract {
 
-	use Authenticates, ResetsPassword;
+	use Authenticatable, CanResetPassword;
 
 	/**
 	 * The database table used by the model.
