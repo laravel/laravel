f66122149a5d9b3a685f48f33302279b222ab309
Taylor Otwell <taylorotwell@gmail.com>
2014-11-07 10:07:31 -0600

    Bootstrap the application when testing.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,11 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	 */
 	public function createApplication()
 	{
-		return require __DIR__.'/../bootstrap/app.php';
+		$app = require __DIR__.'/../bootstrap/app.php';
+
+		$app->make('Illuminate\Contracts\Http\Kernel')->bootstrap();
+
+		return $app;
 	}
 
 }
