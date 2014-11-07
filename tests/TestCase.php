0a01aca6b4fcda26c8893d3cd5fa45578fc808ab
Taylor Otwell <taylorotwell@gmail.com>
2014-11-07 11:56:21 -0600

    Use the console kernel.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,7 +11,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	{
 		$app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Http\Kernel')->bootstrap();
+		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
 		return $app;
 	}
