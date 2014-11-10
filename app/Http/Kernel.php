97287c89fece5e4e152ad04da307ed6b7a8c1a16
Taylor Otwell <taylorotwell@gmail.com>
2014-11-10 13:25:08 -0600

    Update middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel {
 		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
-		'App\Http\Middleware\VerifyCsrfToken',
+		'Illuminate\Foundation\Http\Middleware\VerifyCsrfToken',
 	];
 
 	/**
