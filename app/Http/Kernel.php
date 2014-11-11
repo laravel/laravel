d2aebd283e9107fde4825ebeaa91843a1bfe5cdd
Taylor Otwell <taylorotwell@gmail.com>
2014-11-11 10:27:30 -0600

    Rename middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $middleware = [
-		'Illuminate\Foundation\Http\Middleware\UnderMaintenance',
+		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
 		'Illuminate\Cookie\Middleware\EncryptCookies',
 		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
 		'Illuminate\Session\Middleware\StartSession',
