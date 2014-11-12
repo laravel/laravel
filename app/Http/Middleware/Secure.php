b21cf4ec33c3f97a66b29cb022c7ba050bf00ffb
Nathaniel Blackburn <nblackburn@users.noreply.github.com>
2014-11-12 21:06:35 +0000

    Update Secure.php

--- app/Http/Middleware/Secure.php
+++ app/Http/Middleware/Secure.php
@@ -12,7 +12,8 @@ use Illuminate\Contracts\Foundation\Application;
  * @param $next The next closure.
  * @return redirects to the secure counterpart of the requested uri.
 */
-class Secure implements Middleware {
+class Secure implements Middleware
+{
 	protected $app;
 	
 	public function __construct(Application $app)
