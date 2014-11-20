29ad85dbd13d15af3204cbb06eac80817d714293
Taylor Otwell <taylorotwell@gmail.com>
2014-11-19 21:16:59 -0600

    Change wording.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,7 +8,7 @@ class Handler extends ExceptionHandler {
 	/**
 	 * Report or log an exception.
 	 *
-	 * This is a great location to send exceptions to Sentry, Bugsnag, etc.
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
 	 *
 	 * @param  \Exception  $e
 	 * @return void
