70a80658ab8f36535c712e2457c8ccab7eeabb1a
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-11-21 16:22:13 -0500

    Add starter Sass file

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass("bootstrap.scss")
+    mix.sass("app.scss")
        .phpUnit()
        .publish("vendor/bower_components");
 });
