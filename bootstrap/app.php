3a2dd312bec79807e6c73411e02b6ca435736a01
Taylor Otwell <taylorotwell@gmail.com>
2014-11-21 08:20:05 -0600

    Fix comment.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,12 +17,12 @@ $app = new Illuminate\Foundation\Application(
 
 /*
 |--------------------------------------------------------------------------
-| Create The Application
+| Bind Important Interfaces
 |--------------------------------------------------------------------------
 |
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
 |
 */
 
