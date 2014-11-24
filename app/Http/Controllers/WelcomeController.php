a9c9b202b079437daa25b64dececa6c8684238b3
Taylor Otwell <taylorotwell@gmail.com>
2014-11-24 12:24:01 -0600

    Remove comment.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,19 +2,6 @@
 
 class WelcomeController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	$router->get('/', 'WelcomeController@index');
-	|
-	*/
-
 	/**
 	 * Create a new controller instance.
 	 *
