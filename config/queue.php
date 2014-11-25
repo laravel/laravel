372a6d4907d5bed1e68dbe447f9baa8045cfdd80
Taylor Otwell <taylorotwell@gmail.com>
2014-11-25 09:31:10 -0600

    List null driver in list of queue drivers.

--- config/queue.php
+++ config/queue.php
@@ -11,7 +11,7 @@ return [
 	| API, giving you convenient access to each back-end using the same
 	| syntax for each one. Here you may set the default queue driver.
 	|
-	| Supported: "sync", "beanstalkd", "sqs", "iron", "redis"
+	| Supported: "null", "sync", "beanstalkd", "sqs", "iron", "redis"
 	|
 	*/
 
