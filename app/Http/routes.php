e558ac6eb24f005ea7661ae2c73a79b13025b2da
Taylor Otwell <taylorotwell@gmail.com>
2014-11-26 11:34:18 -0600

    Change wording.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,7 +7,7 @@
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| and give it the controller to call when that URI is requested.
 |
 */
 
