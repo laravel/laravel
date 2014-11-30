5194a437d5f11e9770839cf79c4dfa25ff6a31ac
Taylor Otwell <taylorotwell@gmail.com>
2014-11-30 14:12:20 -0600

    Add a note to the AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
+		// This service provider is a great spot to register your various container
+		// bindings with the application. As you can see, we are registering our
+		// "Registrar" implementation here. You can add your own bindings too!
+
 		$this->app->bind(
 			'Illuminate\Contracts\Auth\Registrar',
 			'App\Services\Registrar'
