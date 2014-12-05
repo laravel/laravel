9864aa3190d3c2b76b5af40a5fe29a74f6a69aaa
lorcanfurey <jericpaul.deleon@gmail.com>
2014-12-05 18:55:48 +0800

    Config Setup

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'PnI7zaFQBog03K8Pu7w9cKobSirXHnJd',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'clique',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
