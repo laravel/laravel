8d9bbc1c26ef88ec72eacd6a82388d8a16d27614
Taylor Otwell <taylorotwell@gmail.com>
2014-12-16 17:44:56 -0600

    Update cache configuration.

--- config/cache.php
+++ config/cache.php
@@ -4,71 +4,59 @@ return [
 
 	/*
 	|--------------------------------------------------------------------------
-	| Default Cache Driver
+	| Default Cache Store
 	|--------------------------------------------------------------------------
 	|
-	| This option controls the default cache "driver" that will be used when
-	| using the Caching library. Of course, you may use other drivers any
-	| time you wish. This is the default when another is not specified.
-	|
-	| Supported: "file", "database", "apc", "memcached", "redis", "array"
+	| This option controls the default cache connection that gets used while
+	| using this caching library. This connection is used when another is
+	| not explicitly specified when executing a given caching function.
 	|
 	*/
 
-	'driver' => env('CACHE_DRIVER') ?: 'file',
+	'default' => env('CACHE_DRIVER') ?: 'file',
 
 	/*
 	|--------------------------------------------------------------------------
-	| File Cache Location
+	| Cache Stores
 	|--------------------------------------------------------------------------
 	|
-	| When using the "file" cache driver, we need a location where the cache
-	| files may be stored. A sensible default has been specified, but you
-	| are free to change it to any other place on disk that you desire.
+	| Here you may define all of the cache "stores" for your application as
+	| well as their drivers. You may even define multiple stores for the
+	| same cache driver to group types of items stored in your caches.
 	|
 	*/
 
-	'path' => storage_path().'/framework/cache',
+	'stores' => [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Database Cache Connection
-	|--------------------------------------------------------------------------
-	|
-	| When using the "database" cache driver you may specify the connection
-	| that should be used to store the cached items. When this option is
-	| null the default database connection will be utilized for cache.
-	|
-	*/
+		'apc' => [
+			'driver' => 'apc'
+		],
 
-	'connection' => null,
+		'array' => [
+			'driver' => 'array'
+		],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Database Cache Table
-	|--------------------------------------------------------------------------
-	|
-	| When using the "database" cache driver we need to know the table that
-	| should be used to store the cached items. A default table name has
-	| been provided but you're free to change it however you deem fit.
-	|
-	*/
+		'database' => [
+			'driver' => 'database',
+			'table'  => 'cache',
+		],
 
-	'table' => 'cache',
+		'file' => [
+			'driver' => 'file',
+			'path'   => storage_path().'/framework/cache',
+		],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Memcached Servers
-	|--------------------------------------------------------------------------
-	|
-	| Now you may specify an array of your Memcached servers that should be
-	| used when utilizing the Memcached cache driver. All of the servers
-	| should contain a value for "host", "port", and "weight" options.
-	|
-	*/
+		'memcached' => [
+			'driver'  => 'memcached',
+			'servers' => [
+				'host' => '127.0.0.1', 'port' => 11211, 'weight' => 100
+			],
+		],
 
-	'memcached' => [
-		['host' => '127.0.0.1', 'port' => 11211, 'weight' => 100],
+		'redis' => [
+			'driver' => 'redis'
+		],
+
 	],
 
 	/*
