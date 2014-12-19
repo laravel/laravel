9a6ebe61a34691d7393d53570faebe42571bcc9d
Christopher Pecoraro <chrispecoraro@gmail.com>
2014-12-19 19:19:24 +0100

    Improve comment to make the instructions clear.
    changing from 'should' to 'must' and that it must be changed from 'YourSecretKey!!!'

--- config/app.php
+++ config/app.php
@@ -72,9 +72,10 @@ return [
 	| Encryption Key
 	|--------------------------------------------------------------------------
 	|
-	| This key is used by the Illuminate encrypter service and should be set
-	| to a random, 32 character string, otherwise these encrypted strings
-	| will not be safe. Please do this before deploying an application!
+	| This key is used by the Illuminate encrypter service and *MUST* be set
+	| to a complex, random 32 character length string. Please change this default string
+	| (YourSecretKey!!!) before deploying an application in production!
+	| 
 	|
 	*/
 
