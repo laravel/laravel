b60f57e77b9284d823292e711ee8f86b615679f3
Taylor Otwell <taylorotwell@gmail.com>
2014-12-23 16:45:03 -0600

    Simplify routes file.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,17 +15,6 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-/*
-|--------------------------------------------------------------------------
-| Authentication & Password Reset Controllers
-|--------------------------------------------------------------------------
-|
-| These two controllers handle the authentication of the users of your
-| application, as well as the functions necessary for resetting the
-| passwords for your users. You may modify or remove these files.
-|
-*/
-
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
