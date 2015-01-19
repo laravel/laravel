05eb99fc83c00eb7c75280a5e99d766364f59b44
darrylcoder <engrdarrylfernandez@gmail.com>
2015-01-18 18:06:14 -0800

    align array spaces

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
-			'port'  	=> env('DB_PORT', 3306),
+			'port'      => env('DB_PORT', 3306),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
