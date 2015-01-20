013007a9e11d23a8e1a2ed39d80c4a4484d38107
Taylor Otwell <taylorotwell@gmail.com>
2015-01-20 11:09:40 -0600

    Move constructor to app.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,6 +1,8 @@
 <?php namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller {
--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,6 +1,8 @@
 <?php namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller {
@@ -17,5 +19,20 @@ class PasswordController extends Controller {
 	*/
 
 	use ResetsPasswords;
+
+	/**
+	 * Create a new password controller instance.
+	 *
+	 * @param  Guard  $auth
+	 * @param  PasswordBroker  $passwords
+	 * @return void
+	 */
+	public function __construct(Guard $auth, PasswordBroker $passwords)
+	{
+		$this->auth = $auth;
+		$this->passwords = $passwords;
+
+		$this->middleware('guest');
+	}
 
 }
