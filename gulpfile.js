fd7f5a73a57db070e9fb5afebd67ae09040da82a
Jay Linski <jakob@linskeseder.com>
2015-01-22 21:32:32 +0100

    updated elixir description in gulpfile from sass to less

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we are compiling the Less
  | file for our application, as well as publishing vendor resources.
  |
  */
