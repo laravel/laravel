cae45b4819f37fa0a8cb1d4fc330231e6a5e0cac
Joseph Silber <contact@josephsilber.com>
2015-01-30 10:56:47 -0500

    Hash password in model

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -32,7 +32,7 @@ class Registrar implements RegistrarContract {
 		return User::create([
 			'name' => $data['name'],
 			'email' => $data['email'],
-			'password' => bcrypt($data['password']),
+			'password' => $data['password'],
 		]);
 	}
 
