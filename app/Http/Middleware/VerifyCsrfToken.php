79ccfdfbae73cba7c0314aa684ef23d58098c9e4
Caleb Fidecaro <calebfidecaro@gmail.com>
2015-02-03 17:49:51 +1300

    Make default middlewares implement contract

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,9 +1,10 @@
 <?php namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Routing\Middleware;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
-class VerifyCsrfToken extends BaseVerifier {
+class VerifyCsrfToken extends BaseVerifier implements Middleware {
 
 	/**
 	 * Handle an incoming request.
