d5f285ba8b36e4533b4b9b228285dd2c941866cc
Taylor Otwell <taylorotwell@gmail.com>
2015-02-16 23:36:48 -0600

    Simplify authentication. Remove service.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,18 +17,11 @@ class AppServiceProvider extends ServiceProvider {
 	/**
 	 * Register any application services.
 	 *
-	 * This service provider is a great spot to register your various container
-	 * bindings with the application. As you can see, we are registering our
-	 * "Registrar" implementation here. You can add your own bindings too!
-	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		$this->app->bind(
-			'Illuminate\Contracts\Auth\Registrar',
-			'App\Services\Registrar'
-		);
+		//
 	}
 
 }
