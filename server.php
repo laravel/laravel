3bd058492053e2d5fd5a6adf12eff84fc96f34b4
Eliu Florez <eliufz@gmail.com>
2015-03-03 22:43:33 -0430

    Update server.php
    Replace and by &&

--- server.php
+++ server.php
@@ -13,7 +13,7 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' and file_exists(__DIR__.'/public'.$uri))
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri))
 {
 	return false;
 }
