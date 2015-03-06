c49317d0b02fcd672ce47915558874acedd27baa
Mads Lundholm <mads@madslundholm.dk>
2015-03-06 15:12:00 +0100

    Use tabs instead of spaces

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -19,7 +19,7 @@ class PasswordController extends Controller {
 	|
 	*/
 
-    use RedirectPaths, ResetsPasswords;
+	use RedirectPaths, ResetsPasswords;
 
 	/**
 	 * Create a new password controller instance.
