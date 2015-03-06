7f2c219acf8485834247755e2399455212164cac
Mads Lundholm <mads@madslundholm.dk>
2015-03-06 14:56:44 +0100

    Update PasswordController.php
    Changes made on the basis of this pull request: https://github.com/laravel/framework/pull/7903

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,9 @@
-<?php namespace App\Http\Controllers\Auth;
+<?php namespace BlueCms\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use BlueCms\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
+use Illuminate\Foundation\Auth\RedirectPaths;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller {
@@ -18,14 +19,13 @@ class PasswordController extends Controller {
 	|
 	*/
 
-	use ResetsPasswords;
+    use RedirectPaths, ResetsPasswords;
 
 	/**
 	 * Create a new password controller instance.
 	 *
 	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
 	 * @param  \Illuminate\Contracts\Auth\PasswordBroker  $passwords
-	 * @return void
 	 */
 	public function __construct(Guard $auth, PasswordBroker $passwords)
 	{
