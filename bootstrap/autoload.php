4e0b15fba1c997405926049d683e25c2101ceb9f
Taylor Otwell <taylorotwell@gmail.com>
2015-03-25 10:20:08 -0500

    Use new bootstrap/cache for cached optimizations loaded during bootstrapping.

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -27,7 +27,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$compiledPath = __DIR__.'/../vendor/compiled.php';
+$compiledPath = __DIR__.'/cache/compiled.php';
 
 if (file_exists($compiledPath)) {
     require $compiledPath;
