d09bf657023c3d15cb3d11a6ef0d331db958777a
Qi Yang <i@yangqi.me>
2015-03-29 00:42:59 -0400

    add config parameter for mysql unix socket

--- config/database.php
+++ config/database.php
@@ -53,15 +53,16 @@ return [
         ],
 
         'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', 'localhost'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'database'    => env('DB_DATABASE', 'forge'),
+            'username'    => env('DB_USERNAME', 'forge'),
+            'password'    => env('DB_PASSWORD', ''),
+            'charset'     => 'utf8',
+            'collation'   => 'utf8_unicode_ci',
+            'prefix'      => '',
+            'strict'      => false,
         ],
 
         'pgsql' => [
