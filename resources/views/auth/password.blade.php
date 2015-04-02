30553ac33eb526cbd1b8ce6fb64273fcf0b67008
Lado Lomidze <lado.lomidze@gmail.com>
2015-04-02 09:50:24 +0200

    Use $errors->any() instead of count($errors) > 0

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -13,7 +13,7 @@
 						</div>
 					@endif
 
-					@if (count($errors) > 0)
+					@if ($errors->any())
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
