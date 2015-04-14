a875eec265b3d95fdac1d9f8756104ede4d8fa41
Martin Lindhe <martin@ubique.se>
2015-04-14 09:48:15 +0200

    fix phpdoc: constructors cant return anything

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -15,8 +15,6 @@ class WelcomeController extends Controller {
 
 	/**
 	 * Create a new controller instance.
-	 *
-	 * @return void
 	 */
 	public function __construct()
 	{
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,7 +16,6 @@ class Authenticate {
 	 * Create a new filter instance.
 	 *
 	 * @param  Guard  $auth
-	 * @return void
 	 */
 	public function __construct(Guard $auth)
 	{
