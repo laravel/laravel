111fdeba8fb13d1ffe65de7d6988d4bfeb01e3bd
Taylor Otwell <taylorotwell@gmail.com>
2015-04-21 14:39:47 -0500

    Don't compile service providers by default.

--- config/compile.php
+++ config/compile.php
@@ -14,11 +14,7 @@ return [
     */
 
     'files' => [
-
-        realpath(__DIR__.'/../app/Providers/AppServiceProvider.php'),
-        realpath(__DIR__.'/../app/Providers/EventServiceProvider.php'),
-        realpath(__DIR__.'/../app/Providers/RouteServiceProvider.php'),
-
+        //
     ],
 
     /*
