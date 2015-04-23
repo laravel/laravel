f3c4238ff27bd28d78b3b5a88e199c3a99934f03
Proshanta Barman <proshanto.barman@gmail.com>
2015-04-23 20:42:33 +0600

    Use unicode charset in form explicitly

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -24,7 +24,7 @@
 						</div>
 					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}" accept-charset="UTF-8">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
