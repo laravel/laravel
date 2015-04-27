bd9a4f5436bfe4b87c7a982bd47c98a4932787a1
Taylor Otwell <taylorotwell@gmail.com>
2015-04-27 15:21:32 -0500

    Some sample configuration.

--- config/app.php
+++ config/app.php
@@ -115,6 +115,7 @@ return [
          */
         'Illuminate\Foundation\Providers\ArtisanServiceProvider',
         'Illuminate\Auth\AuthServiceProvider',
+        'Illuminate\Broadcasting\BroadcastServiceProvider',
         'Illuminate\Bus\BusServiceProvider',
         'Illuminate\Cache\CacheServiceProvider',
         'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
