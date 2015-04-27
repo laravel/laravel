ae78cd94573b32550980d390e4d58ac8a8021619
Chris Fidao <fideloper@gmail.com>
2015-04-27 08:33:30 -0500

    Documented availability of SES mail driver in mail config

--- config/mail.php
+++ config/mail.php
@@ -11,7 +11,7 @@ return [
     | sending of e-mail. You may specify which one you're using throughout
     | your application here. By default, Laravel is setup for SMTP mail.
     |
-    | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "log"
+    | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "ses", "log"
     |
     */
 
