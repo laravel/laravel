b22a1f48c85d60cc747105c77ca8adcb76a43751
Taylor Otwell <taylorotwell@gmail.com>
2015-04-30 14:19:00 -0500

    Fix typo.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 class VerifyCsrfToken extends BaseVerifier
 {
     /**
-     * The URIs that shoudl be excluded from CSRF verification.
+     * The URIs that should be excluded from CSRF verification.
      *
      * @var array
      */
