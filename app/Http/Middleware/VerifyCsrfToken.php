89f568156f59e374d98523f18493672ab380c42e
Taylor Otwell <taylorotwell@gmail.com>
2015-04-30 14:59:22 -0500

    No need to override this by default anymore.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,16 +13,4 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return parent::handle($request, $next);
-    }
 }
