6029a54bd32dd5c5c904cb15d1deda8f04c449b7
Brad Smith <brad@compasshb.com>
2015-05-08 11:51:34 -0700

    Add autofocus attribute to forms in auth view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								<input type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
 							</div>
 						</div>
 
