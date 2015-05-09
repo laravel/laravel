9a6c3df7a03a1382e2878919b9d1523dfe3ae5e3
Taylor Otwell <taylorotwell@gmail.com>
2015-05-09 16:43:43 -0500

    Use define syntax.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,11 @@
 |
 */
 
-$factory['App\User'] = function ($faker) {
+$factory->define('App\User', function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
     ];
-};
+});
