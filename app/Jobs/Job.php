d502747428494e634fe699dbc44c703845e2a79f
Taylor Otwell <taylorotwell@gmail.com>
2015-05-09 23:14:32 -0500

    Show example base variables.

--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -2,5 +2,17 @@
 
 abstract class Job
 {
-    //
+    /**
+     * The name of the queue the job should be sent to.
+     *
+     * @var string
+     */
+    public $queue;
+
+    /**
+     * The seconds before the job should be made available.
+     *
+     * @var int
+     */
+    public $delay;
 }
