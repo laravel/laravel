e111f1ef9b998cecae128e3d51725aa7bf85e44d
Taylor Otwell <taylorotwell@gmail.com>
2015-05-22 15:42:17 -0500

    add period.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
-        <title>Be right back</title>
-        
+        <title>Be right back.</title>
+
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
         <style>
