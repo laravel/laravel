2cbd6caf0c711bc6e57793d7e4596588e38ee75e
N.Ilya <nekrasov@greensight.ru>
2015-05-24 11:33:18 +0000

    custom stubs config

---
+++ config/console.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Stubs
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify paths for you custom stub files. They will be
+    | used by the `artisan make:*` commands instead of default stubs.
+    |
+    */
+
+    'stubs' => [
+
+        'controller' => [
+            'default' => '',
+            'plain'   => ''
+        ],
+
+        'model' => [
+            'default' => '',
+        ],
+
+        'request' => [
+            'default' => '',
+        ],
+
+    ],
+];
