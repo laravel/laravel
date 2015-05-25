59ee1ec0a036221e4cc77a0cdc5c5092d26a103e
Rafael Ferreira Silveira <rafaelfs17@users.noreply.github.com>
2015-05-25 09:51:50 -0300

    Added title tag

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>Be right back soon</title>
 
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
