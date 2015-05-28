c3128ad92a15dc8eb4ae93aee8530d519ef9dac0
Taylor Otwell <taylorotwell@gmail.com>
2015-05-28 10:15:09 -0500

    import trait

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
