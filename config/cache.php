4ee6523dfa096ea87932bf63fea586ed1f65ea78
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-05-28 15:31:26 -0400

    Using the path parameter in the path method.

--- config/cache.php
+++ config/cache.php
@@ -44,7 +44,7 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path'   => storage_path().'/framework/cache',
+            'path'   => storage_path('framework/cache'),
         ],
 
         'memcached' => [
