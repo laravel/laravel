6ab4004af9a6dce1f59278bf1134bfd7dd035907
Antony Budianto <antonybudianto@gmail.com>
2015-06-08 09:50:22 +0700

    Update latest CDN
    If needed.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,7 +56,7 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </body>
 </html>
