8f3e5c1a67b524d157b4a3f875d92d287891754a
PSmolic <psmolic@extensionengine.com>
2015-06-18 13:08:49 -0400

    running php-cs-fixer fix .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth'       => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'guest'      => \App\Http\Middleware\RedirectIfAuthenticated::class,
     ];
 }
