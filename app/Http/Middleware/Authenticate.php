2fa33128cfd741f51508b2d03eecade641268789
rizerzero <deepspring85@gmail.com>
2015-06-24 18:00:23 +0100

    having a named route rather than a static route
    having a named route rather than a static route makes it easier to change change login route ,

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -38,7 +38,7 @@ class Authenticate
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->guest('auth/login');
+                return redirect()->guest(route('login'));
             }
         }
 
