09d028f14bfcde9061d6622c9d1d0d9775792e6e
Taylor Otwell <taylorotwell@gmail.com>
2015-06-25 13:30:52 -0500

    change welcome splash

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,6 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
@@ -35,17 +34,12 @@
                 font-size: 96px;
                 margin-bottom: 40px;
             }
-
-            .quote {
-                font-size: 24px;
-            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
             </div>
         </div>
     </body>
