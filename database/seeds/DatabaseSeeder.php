74b507b6d33ee251b29e3ac676f16cff95df4263
Duilio Palacios <admin@styde.net>
2015-07-13 17:06:15 +0000

    Default UserTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
 
         Model::reguard();
     }
