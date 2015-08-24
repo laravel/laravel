97b1e6d6c9975f32407d245209f1235d8881e376
Syed Raza Mehdi (01162) <raza.mehdi@nxb.com.pk>
2015-08-24 16:40:16 +0500

    Added Password Reset Token expiration date field to the default passwords_reset table.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -17,6 +17,7 @@ class CreatePasswordResetsTable extends Migration {
 			$table->string('email')->index();
 			$table->string('token')->index();
 			$table->timestamp('created_at');
+            $table->timestamp('expire_at');
 		});
 	}
 
