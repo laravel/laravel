72a10c64c2772df7e30cbeb9cfd09d85e9f24f7b
Taylor Otwell <taylorotwell@gmail.com>
2015-08-29 16:42:19 -0500

    Define HasAbilities trait on default user.

--- app/User.php
+++ app/User.php
@@ -5,12 +5,13 @@ namespace App;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\HasAbilities;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword, HasAbilities;
 
     /**
      * The database table used by the model.
