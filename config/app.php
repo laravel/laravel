c82c5eceda363db1dde8bf9a8a9b1da7b316307f
Taylor Otwell <taylorotwell@gmail.com>
2015-08-29 16:36:22 -0500

    acl changes

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
         'Event'     => Illuminate\Support\Facades\Event::class,
         'File'      => Illuminate\Support\Facades\File::class,
+        'Gate'      => Illuminate\Support\Facades\Gate::class,
         'Hash'      => Illuminate\Support\Facades\Hash::class,
         'Input'     => Illuminate\Support\Facades\Input::class,
         'Inspiring' => Illuminate\Foundation\Inspiring::class,
