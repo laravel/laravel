7d4b5d75efb7ec201a0eb9486963cb7bd91e323a
Taylor Otwell <taylorotwell@gmail.com>
2015-08-29 20:27:18 -0500

    Default auth service provider.

--- config/app.php
+++ config/app.php
@@ -141,6 +141,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
