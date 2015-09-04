602519fc4c2f884a16c58ca760b17248415e52e4
Lukas Kämmerling <admin@myonepager.net>
2015-09-04 17:09:20 +0200

    Make Style CI Test passable

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL','http://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
