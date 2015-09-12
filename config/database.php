af84a0c1ea884daa405fd1a05b413b59b9629d52
Zach Mueller <ZachAlanMueller@users.noreply.github.com>
2015-09-11 20:54:25 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', '4415698385e083de'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
