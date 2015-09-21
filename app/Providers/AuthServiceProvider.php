50357d8bab3c43ac6b76a82038847c1061bf21c6
crynobone <crynobone@gmail.com>
2015-09-21 12:00:44 +0800

    Use $this instead of parent.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(GateContract $gate)
     {
-        parent::registerPolicies($gate);
+        $this->registerPolicies($gate);
 
         //
     }
