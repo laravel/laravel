a8d8d8623b09a901887b104bb701d4f5e6c2f43c
Mike Bronner <mike@genealabs.com>
2015-09-24 08:46:18 -0700

    Change db conf to allow running of migrations from outside of Homestead

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST') . (('homestead' === env('DB_USERNAME', 'forge') && 'homestead' !== gethostname()) ? ':33060' : ''),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
@@ -66,7 +66,7 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST') . (('homestead' === env('DB_USERNAME', 'forge') && 'homestead' !== gethostname()) ? ':54320' : ''),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
