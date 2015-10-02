0a9c861b7326724a1ef004b6f12105c6873c1348
Antério vieira da silva <anteriovieira@gmail.com>
2015-10-02 11:05:11 -0300

    You can set the database directory on file .env
    t enables me to do something like:
    
    ```
    DB_CONNECTION=sqlite
    DB_DATABASE=/home/user/database/project-name.sqlite
    ```

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path('database.sqlite'),
+            'database' => env('DB_DATABASE', storage_path('database.sqlite')),
             'prefix'   => '',
         ],
 
