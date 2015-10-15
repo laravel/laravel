e08e50718ddda756224af8e51c3677ccfbb5a4a5
Димитър Янакиев <TheEVIL@users.noreply.github.com>
2015-10-16 00:45:29 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('user');
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->rememberToken();
