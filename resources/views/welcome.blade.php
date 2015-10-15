6e41fa5b3905aba4cce84ab100438e24a5a3dc99
Димитър Янакиев <TheEVIL@users.noreply.github.com>
2015-10-16 00:46:46 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Page Name</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
