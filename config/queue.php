77c5c1726f4a8d9fff8c21dec838c2091c4c6afc
Tuomas Koponen <tuomas.koponen@oss-solutions.fi>
2015-10-20 22:23:54 +0300

    Change default value for queue.failed.database
    Use same DB_CONNECTION enviroment variable for queue.failed.database config

--- config/queue.php
+++ config/queue.php
@@ -87,7 +87,8 @@ return [
     */
 
     'failed' => [
-        'database' => 'mysql', 'table' => 'failed_jobs',
+        'database'  => env('DB_CONNECTION', 'mysql'),
+        'table'     => 'failed_jobs',
     ],
 
 ];
