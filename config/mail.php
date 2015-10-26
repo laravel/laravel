17f42993288369ab2fa01f4b702d4402c32a9179
Raza Mehdi <raza.sds@nxvt.com>
2015-10-26 12:02:55 +0500

    Added configuration settings for default values for Global "From" Address & Name.

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_FROM_EMAIL', null), 'name' => env('MAIL_FROM_NAME', null)],
 
     /*
     |--------------------------------------------------------------------------
