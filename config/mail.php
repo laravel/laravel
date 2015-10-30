ed18fd99fd5955f60ba8436293c7d0d8a7416644
Graham Campbell <graham@alt-three.com>
2015-10-30 18:15:40 +0000

    Removed config option for deleted feature

--- config/mail.php
+++ config/mail.php
@@ -108,17 +108,4 @@ return [
 
     'sendmail' => '/usr/sbin/sendmail -bs',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Mail "Pretend"
-    |--------------------------------------------------------------------------
-    |
-    | When this option is enabled, e-mail will not actually be sent over the
-    | web and will instead be written to your application's logs files so
-    | you may inspect the message. This is great for local development.
-    |
-    */
-
-    'pretend' => false,
-
 ];
