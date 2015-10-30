13b799196bc6957dd57b6b6d07ace32299a9f336
Graham Campbell <graham@alt-three.com>
2015-10-30 18:54:41 +0000

    Revert bad changes to the exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,7 +29,7 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Throwable  $e
+     * @param  \Exception  $e
      * @return void
      */
     public function report(Exception $e)
@@ -41,7 +41,7 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $e
+     * @param  \Exception  $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
