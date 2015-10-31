a383c8447c33f1ee8a3464696aae957d12c55ef1
Taylor Otwell <taylorotwell@gmail.com>
2015-10-31 14:40:32 -0400

    Applied fixes from StyleCI

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends BaseUser
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password'
+        'name', 'email', 'password',
     ];
 
     /**
@@ -21,6 +21,6 @@ class User extends BaseUser
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token'
+        'password', 'remember_token',
     ];
 }
