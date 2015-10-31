8a49b4f137003a91496de52ea4f48781802b2afb
Taylor Otwell <taylorotwell@gmail.com>
2015-10-31 13:38:22 -0500

    add required_unless lang line

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -60,6 +60,7 @@ return [
     'regex'                => 'The :attribute format is invalid.',
     'required'             => 'The :attribute field is required.',
     'required_if'          => 'The :attribute field is required when :other is :value.',
+    'required_unless'      => 'The :attribute field is required unless :other is in :value.',
     'required_with'        => 'The :attribute field is required when :values is present.',
     'required_with_all'    => 'The :attribute field is required when :values is present.',
     'required_without'     => 'The :attribute field is required when :values is not present.',
