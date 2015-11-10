b3bd9016e5a22bbb7a5ba65074f70bfb2710cfc7
Marc Ojales <marc@paracore.com>
2015-11-10 14:10:38 -0700

    Update gulpfile with elixir to 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,34 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 
-/*-----------------------------------------
- | Task 'gulp' will error
- | -  A recent update broke this process, don't know what but running the task gulp will error
- | 
- | Task 'gulp watch' will work fine
- | - A recent update broke the gulp task, whereas watch still works fine
- | ----------------------------------------
- */
-
-
-/*-----------------------------------------
- | Custom Gulp Tasks
- |
- | -  Browserify already watches/runs by default so we don't need to double up
- | ----------------------------------------
- */
-gulp.task('buildAssets', ['sass', 'version']);
-
 elixir(function (mix) {
-
-    /*
-     *  TO DO: use npm to import vendor dependencies directly via browserify (will remove the need for a .copy task for js files
-     */
     mix
         /*-----------------------------------------
          | Public site (www)
          | ----------------------------------------
          */
-        .sass('../../assets/www/sass/import.scss', 'public/dist/www/css/styles.css')
-        .browserify('../../assets/www/js/main.js', 'public/dist/www/js/bundle.js')
-        .copy('node_modules/font-awesome/fonts', 'public/dist/www/fonts/font-awesome')
+        .sass('import.scss', './public/dist/www/css/styles.css')
+        .browserify('www/js/main.js', './public/dist/www/js/bundle.js')
+        
 
         /*-----------------------------------------
-         | Admin site (cms) [ NOT SETUP YET ]
+         | Admin site (cms) [ NOT SETUP ]
          | ----------------------------------------
          */
-//        .sass('../../assets/cms/sass/import.scss', 'public/dist/cms/css/styles.css')
-//        .browserify('../../assets/cms/js/main.js', 'public/dist/cms/js/bundle.js')
+        .sass('import.scss', './public/dist/cms/css/styles.css')
+        .browserify('cms/js/main.js', './public/dist/cms/js/bundle.js')
 
 
         /*-----------------------------------------
-         | Versions
+         | Copy
          | ----------------------------------------
          */
+        .copy('node_modules/font-awesome/fonts', '/dist/www/fonts/font-awesome')
+
         .version([
-//            'dist/cms/js/bundle.js',
-//            'dist/cms/css/styles.css',
-            'dist/www/js/bundle.js',
-            'dist/www/css/styles.css'
+            '/public/dist/cms/js/bundle.js',
+            '/public/dist/cms/css/styles.css',
+            './public/dist/www/js/bundle.js',
+            './public/dist/www/css/styles.css'
         ])
-
-
-        /*-----------------------------------------
-         | Watcher (Not really, but maybe?)
-         | ----------------------------------------
-         */
-        .task('buildAssets', 'resources/assets/**');
 });
