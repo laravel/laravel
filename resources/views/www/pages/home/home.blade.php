44ccb446d44ca41a8489aad6c6d1d5c7d57f179c
Marc Ojales <marc@paracore.com>
2015-11-10 14:43:37 -0700

    Remove known issues on home page; resolved elixir

--- resources/views/www/pages/home/home.blade.php
+++ resources/views/www/pages/home/home.blade.php
@@ -23,27 +23,6 @@
 
 			<hr />
 
-			<h4>Known Issues</h4>
-			<p>
-				<strong>Laravel Elixir</strong><br />
-
-				Gulp build process is semi-broken. <span style="color:#f00">Running the task 'gulp' will error</span>.<br /><br />
-
-				<strong>To start the initial build process run these gulp tasks</strong><br />
-<pre style="max-width: 350px;">gulp copy
-gulp sass
-gulp browserify
-gulp version
-gulp watch</pre>
-			</p>
-
-			<p>
-				Gulp watch looks for changes in this directory<br />
-<pre style="max-width: 350px;">resources/assets/**</pre>
-				and will recompile when changes are detected.
-			</p>
-
-
         </div>
 
     </div>
