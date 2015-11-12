c8a63766056504dc2479c09c1adf06907bb5e077
Hudson Tavares <hudson.tavares@gmail.com>
2015-11-12 17:05:41 -0200

    Fixed namespacing and removed an unused entry - Removed the classmap entry from autoload-dev at composer.json - Added absolute namespaces references on \Tests\TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
     /**
      * The base URL to use while testing the application.
@@ -20,7 +20,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
     }
