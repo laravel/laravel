4eb28ba0692abde5e881c6ed9dfd2fa051fee4bf
Taylor Otwell <taylorotwell@gmail.com>
2015-12-02 08:21:23 -0600

    base controller doesn't have to be abstract.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
