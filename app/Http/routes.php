c2c8ab6f5f26ab91a83f26370ee2a589611a5a67
Taylor Otwell <taylorotwell@gmail.com>
2015-12-05 21:52:46 -0600

    line length

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,7 +5,7 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you can register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
