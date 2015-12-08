1d0853b638a6fb0594cc0a3b5154072aa76e9e2e
Taylor Otwell <taylorotwell@gmail.com>
2015-12-07 21:27:16 -0600

    Add throttle middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,5 +29,6 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
