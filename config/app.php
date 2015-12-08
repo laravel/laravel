60d782a1bb25d8a9685261adb009cb5f6067b003
Taylor Otwell <taylorotwell@gmail.com>
2015-12-08 15:28:26 -0600

    remove unneeded service provider

--- config/app.php
+++ config/app.php
@@ -131,7 +131,6 @@ return [
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Routing\ControllerServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
