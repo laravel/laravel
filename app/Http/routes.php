fa3495a28dcaf64ce74672f5b8687d9e2f7b4be9
Taylor Otwell <taylorotwell@gmail.com>
2015-12-11 16:31:19 -0600

    comment changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes in an application.
@@ -16,9 +16,14 @@ Route::get('/', function () {
 });
 
 /*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
 | This route group applies the "web" middleware group to every route
 | it contains. The "web" middleware group is defined in your HTTP
 | kernel and includes session state, CSRF protection, and more.
+|
 */
 
 Route::group(['middleware' => 'web'], function () {
