3eb8613ff8a5d86ada32f1e0e011d36b89219c1f
Taylor Otwell <taylorotwell@gmail.com>
2015-12-14 09:44:17 -0600

    Comment fix.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,7 +5,7 @@
 | Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes in an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
