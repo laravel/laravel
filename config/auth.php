435104304e9f6afc326f639cf3d884ae60644617
Taylor Otwell <taylorotwell@gmail.com>
2015-12-14 16:37:16 -0600

    Basic token configuration.

--- config/auth.php
+++ config/auth.php
@@ -31,7 +31,7 @@ return [
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
-    | Supported: "session"
+    | Supported: "session", "token"
     |
     */
 
@@ -42,7 +42,8 @@ return [
         ],
 
         // 'api' => [
-
+        //     'driver' => 'token',
+        //     'source' => 'users',
         // ],
     ],
 
