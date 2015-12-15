1865c2993edf9327a922c037d4fb50cce5e1a042
Taylor Otwell <taylorotwell@gmail.com>
2015-12-15 15:01:35 -0600

    use provider for consistent language

--- config/auth.php
+++ config/auth.php
@@ -25,9 +25,9 @@ return [
     |
     | Next, you may define every authentication guard for your application.
     | Of course, a great default configuration has been defined for you
-    | here which uses "session" storage and the Eloquent user source.
+    | here which uses session storage and the Eloquent user provider.
     |
-    | All authentication drivers have a user "source". This defines how the
+    | All authentication drivers have a user provider. This defines how the
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
@@ -38,21 +38,21 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'source' => 'users',
+            'provider' => 'users',
         ],
 
         'api' => [
             'driver' => 'token',
-            'source' => 'users',
+            'provider' => 'users',
         ],
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | User Sources
+    | User Providers
     |--------------------------------------------------------------------------
     |
-    | All authentication drivers have a user "source". This defines how the
+    | All authentication drivers have a user provider. This defines how the
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'sources' => [
+    'providers' => [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
@@ -97,7 +97,7 @@ return [
 
     'passwords' => [
         'users' => [
-            'source' => 'users',
+            'provider' => 'users',
             'email' => 'emails.password',
             'table' => 'password_resets',
             'expire' => 60,
