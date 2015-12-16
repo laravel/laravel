895c5fead87ad8261d75784c65648720df904481
Taylor Otwell <taylorotwell@gmail.com>
2015-12-16 15:18:11 -0600

    readable name

--- app/User.php
+++ app/User.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as BaseUser;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends BaseUser
+class User extends Authenticatable
 {
     /**
      * The attributes that are mass assignable.
