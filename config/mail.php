11c11f0b7de62b850ad528768cd17edbc345f654
Leandro Henrique <leandro@lstech.com.br>
2015-12-22 16:38:44 -0200

    env to from too

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "ses", "log"
     |
-    */
+     */
 
     'driver' => env('MAIL_DRIVER', 'smtp'),
 
@@ -26,7 +26,7 @@ return [
     | applications. A default option is provided that is compatible with
     | the Mailgun mail service which will provide reliable deliveries.
     |
-    */
+     */
 
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
@@ -39,7 +39,7 @@ return [
     | users of the application. Like the host we have set this value to
     | stay compatible with the Mailgun e-mail application by default.
     |
-    */
+     */
 
     'port' => env('MAIL_PORT', 587),
 
@@ -52,9 +52,9 @@ return [
     | the same address. Here, you may specify a name and address that is
     | used globally for all e-mails that are sent by your application.
     |
-    */
+     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env("MAIL_FROM_ADDRESS", null), 'name' => env("MAIL_FROM_NAME", null)],
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
     | the application send e-mail messages. A sensible default using the
     | transport layer security protocol should provide great security.
     |
-    */
+     */
 
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
 
@@ -78,7 +78,7 @@ return [
     | set it here. This will get used to authenticate with your server on
     | connection. You may also set the "password" value below this one.
     |
-    */
+     */
 
     'username' => env('MAIL_USERNAME'),
 
@@ -91,7 +91,7 @@ return [
     | messages from your application. This will be given to the server on
     | connection so that the application will be able to send messages.
     |
-    */
+     */
 
     'password' => env('MAIL_PASSWORD'),
 
@@ -104,7 +104,7 @@ return [
     | the path to where Sendmail lives on this server. A default path has
     | been provided here, which will work well on most of your systems.
     |
-    */
+     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
 
