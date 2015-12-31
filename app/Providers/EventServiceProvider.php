735be1c2ccf1d77c04489dcb97199c9d7cbff2e9
ChrisEsser <esser32@gmail.com>
2015-12-31 15:09:58 -0600

    use class notation

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
+        App\Events\SomeEvent::class => [
+            App\Listeners\EventListener::class,
         ],
     ];
 
