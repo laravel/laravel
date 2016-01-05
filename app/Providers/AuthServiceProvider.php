2c3968217736e84b9e2b88d84ca8b074d038e8e0
Sven <svenluijten@users.noreply.github.com>
2016-01-05 13:53:07 +0100

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        App\Model::class => App\Policies\ModelPolicy::class,
+        \App\Model::class => \App\Policies\ModelPolicy::class,
     ];
 
     /**
