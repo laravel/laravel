2d86e4cbfd0927b818dd54b6f1bfacbfb8ec2697
Sven <svenluijten@users.noreply.github.com>
2016-01-05 13:03:40 +0100

    Use ::class notation

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        App\Model::class => App\Policies\ModelPolicy::class,
     ];
 
     /**
