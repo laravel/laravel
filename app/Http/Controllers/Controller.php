5c8b12fdf5647b0e1682633621e843b8139b4857
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-01-16 04:53:54 -0200

    adjust import order

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
