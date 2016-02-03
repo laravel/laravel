89d693b5e611ee82a4c8d7db1b538ecf7625fcd5
david-ridgeonnet <david@ridgeonnet.com>
2016-02-03 14:34:03 +0000

    Added default engine in configuration

--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
+            'engine'    => null,
         ],
 
         'pgsql' => [
