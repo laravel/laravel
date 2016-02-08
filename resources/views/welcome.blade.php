744ba5e3a189af72c0a5b290f70baca4b1fb15a3
Let Aurn IV <let.aurn@gmail.com>
2016-02-08 10:14:28 +0100

    Move welcome style to a Sass file
    This change will the drive the user to :
    - use CSS Preprocessor (modern web development best practice)
    - be familiar with Elixir

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,37 +3,10 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:100">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+        
     </head>
     <body>
         <div class="container">
