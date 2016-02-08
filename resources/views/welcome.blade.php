feb9d6218e0c54537aff0bf85f56b287f54a2b62
Let Aurn IV <let.aurn@gmail.com>
2016-02-08 16:06:42 +0100

    Move font stylesheet to sass

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,7 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:100">
-
         <link rel="stylesheet" type="text/css" href="/css/app.css">
-        
     </head>
     <body>
         <div class="container">
