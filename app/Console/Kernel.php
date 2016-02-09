b1210d09fc7fc978fce1b2997690ad379e4d95f8
Gordon Diggs <gordon@gordondiggs.com>
2016-02-09 12:19:31 -0500

    Fix unused variable issue in Code Climate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule()
     {
         // $schedule->command('inspire')
         //          ->hourly();
