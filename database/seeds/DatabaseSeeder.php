94b568b59d61444bab06e34ada887214d843220f
Anthony Holmes <me@anthonyholmes.me>
2016-02-16 13:42:33 -0600

    Rename commented default seeder call
    Rename commented default seeder call to be consistent with official docs

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
