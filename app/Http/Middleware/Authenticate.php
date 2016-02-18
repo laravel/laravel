062254bb5053230a8eafec4cddf2a3c3ccef8099
amosmos <amos@boaideas.com>
2016-02-18 20:48:35 +0200

    allow a specific guard login URL
    Since Laravel already supports calling auth middleware with specific guard, this will allow customizing each guard login url

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,7 +21,8 @@ class Authenticate
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->guest('login');
+                $loginUrl = config("auth.guards.{$guard}.loginUrl", 'login');
+                return redirect()->guest($loginUrl);
             }
         }
 
