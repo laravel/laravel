9b0a8481457378591836f6a80ea65ec54b875230
amosmos <amos@boaideas.com>
2016-02-18 22:31:14 +0200

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -22,7 +22,7 @@ class Authenticate
                 return response('Unauthorized.', 401);
             } else {
                 $loginUrl = config("auth.guards.{$guard}.loginUrl", 'login');
-                
+
                 return redirect()->guest($loginUrl);
             }
         }
