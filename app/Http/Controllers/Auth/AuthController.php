ce955e0697ccb1f2e0873256d05e557265431730
amosmos <amos@boaideas.com>
2016-02-19 03:42:54 +0200

    Guest middleware can be called with specific guard
    And so shouldn't it be called with the same guard this controller is using?
    
    Not sure if my syntax is the best though...

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware("guest:{$this->guard}", ['except' => 'logout']);
     }
 
     /**
