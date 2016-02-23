62d62a0524f501f1aa9b54db8110ae7d3b892fe4
Taylor Otwell <taylorotwell@gmail.com>
2016-02-22 22:19:31 -0600

    update method call

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
     }
 
     /**
