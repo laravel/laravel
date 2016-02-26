ef6b5a6343f6e1b0ab48e7feb5fac5a07584752d
Mohamed Said <themohamedsaid@gmail.com>
2016-02-26 08:49:55 +0000

    Add language line for the "present" validation rule.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -58,6 +58,7 @@ return [
     'not_in'               => 'The selected :attribute is invalid.',
     'numeric'              => 'The :attribute must be a number.',
     'regex'                => 'The :attribute format is invalid.',
+    'present'              => 'The :attribute field must be present.',
     'required'             => 'The :attribute field is required.',
     'required_if'          => 'The :attribute field is required when :other is :value.',
     'required_unless'      => 'The :attribute field is required unless :other is in :values.',
