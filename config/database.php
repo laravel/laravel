8dbd26020ae09d0563c092a484471ae9efa890d6
TGM <marius.cdm@hotmail.com>
2016-03-01 14:31:07 +0200

    Replaced TAB with space

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-			'port'      => env('DB_PORT', '3306'),
+            'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
