d998b5bd0392f76dcaa461fdec919658947c2e65
Taylor Otwell <taylorotwell@gmail.com>
2016-03-06 19:56:30 -0600

    formatting

--- config/services.php
+++ config/services.php
@@ -19,18 +19,14 @@ return [
         'secret' => env('MAILGUN_SECRET'),
     ],
 
-    'mandrill' => [
-        'secret' => env('MANDRILL_SECRET'),
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
-
     'ses' => [
         'key' => env('SES_KEY'),
         'secret' => env('SES_SECRET'),
         'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
     ],
 
     'stripe' => [
