8b699f683b6313d97d9451e1a4c041bac2c1677b
vlakoff <vlakoff@gmail.com>
2016-03-19 05:28:20 +0100

    Be kind to the upgrading user

--- bootstrap/clear-compiled.php
+++ bootstrap/clear-compiled.php
@@ -1,4 +1,5 @@
 <?php
 
 @unlink(__DIR__.'/cache/compiled.php');
-@unlink(__DIR__.'/cache/services.json');
+@unlink(__DIR__.'/cache/services.json'); // Laravel 5.1
+@unlink(__DIR__.'/cache/services.php'); // Laravel 5.2
