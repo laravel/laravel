a77fa359d9fa9a974f4c3677a67fadf4299e8b6c
Taylor Otwell <taylorotwell@gmail.com>
2016-03-23 18:05:18 -0400

    Applied fixes from StyleCI

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -51,7 +51,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes(Router $router)
     {
         $router->group([
-            'namespace' => $this->namespace, 'middleware' => 'web'
+            'namespace' => $this->namespace, 'middleware' => 'web',
         ], function ($router) {
             require app_path('Http/routes.php');
         });
