bcc54357bdb80c5ffcf41bdda222706b7aee5637
Taylor Otwell <taylorotwell@gmail.com>
2016-03-23 17:05:17 -0500

    add to comment

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,6 +45,8 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Define the "web" routes for the application.
      *
+     * These routes all receive session state, CSRF protection, etc.
+     *
      * @param  \Illuminate\Routing\Router  $router
      * @return void
      */
