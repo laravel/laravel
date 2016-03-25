a42e65acfb24e18c8408d541c9c59e5324cbe01d
Ahmed Ashraf <ahmed29329@gmail.com>
2016-03-25 19:10:23 +0200

    return json response from controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function json($data = [], $status = 200, array $headers = [], $options = 0)
+        return Response::json($data,$status,$headers,$options);
+    }
 }
