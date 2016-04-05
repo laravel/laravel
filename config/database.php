90a48b2d96ea9b47e1af67615088375088a38e89
larschinkel <lars.schinkel@gmail.com>
2016-04-05 22:06:21 +0200

    Bring back SQL Server settings

--- config/database.php
+++ config/database.php
@@ -78,6 +78,17 @@ return [
             'schema' => 'public',
         ],
 
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+        ],
+
     ],
 
     /*
