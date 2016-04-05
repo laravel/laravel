7975ac8bb8717a0b5b3c6fccb44293120ca47c1f
larschinkel <lars.schinkel@gmail.com>
2016-04-05 21:43:17 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -77,6 +77,17 @@ return [
             'prefix' => '',
             'schema' => 'public',
         ],
+        
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+        ],        
 
     ],
 
