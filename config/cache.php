9dc2d60336414a985fdfc1fd5d4430a46515d9f8
Tom Castleman <tomcastleman@users.noreply.github.com>
2016-04-29 17:45:51 +0100

    Add config for new Memcached features
    Adds config for persistent connections, SASL authentication, and custom options

--- config/cache.php
+++ config/cache.php
@@ -49,6 +49,14 @@ return [
 
         'memcached' => [
             'driver' => 'memcached',
+            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
+            'sasl'       => [
+                env('MEMCACHED_USERNAME'),
+                env('MEMCACHED_PASSWORD'),
+            ],
+            'options'    => [
+                //Memcached::OPT_CONNECT_TIMEOUT  => 2000,
+            ],
             'servers' => [
                 [
                     'host' => env('MEMCACHED_HOST', '127.0.0.1'),
