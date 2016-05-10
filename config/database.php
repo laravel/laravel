8a4fa978da93bb7f403bd19bdc0a55e86b783543
Adam14Four <adam@14four.com>
2016-05-10 11:46:46 -0700

    Enabled MySQL "strict" mode by default

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
-            'strict'    => false,
+            'strict'    => true,
         ],
 
         'pgsql' => [
