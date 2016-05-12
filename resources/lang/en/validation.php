8b02ea6b478bcae4ae389107bea55da944137ef0
Jungle Gecko <jungle-gecko@users.noreply.github.com>
2016-05-12 22:10:30 +0200

    Added localization for ValidationException constructor
    Linked to PR https://github.com/laravel/framework/pull/13536

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -12,6 +12,7 @@ return [
     | as the size rules. Feel free to tweak each of these messages here.
     |
     */
+    'exception'     => 'The given data failed to pass validation.',
 
     'accepted'             => 'The :attribute must be accepted.',
     'active_url'           => 'The :attribute is not a valid URL.',
