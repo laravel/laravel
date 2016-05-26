19f85378298e81dd227c814ed9e9bbd7f9c0b5da
Taylor Otwell <taylorotwell@gmail.com>
2016-05-26 13:53:39 -0500

    fix namespace

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Illuminate\Http\Middleware\Authenticate::class,
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
