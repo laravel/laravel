4f018159b8865155fff3a47e0b7bb878902328d3
Joseph Silber <contact@josephsilber.com>
2016-05-29 22:27:14 -0400

    Update can middleware to new namespace

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,7 +49,7 @@ class Kernel extends HttpKernel
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
