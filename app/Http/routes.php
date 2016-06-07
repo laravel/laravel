e5dfb052473c93f23dfc951f42afcfabbf4a6d58
Taylor Otwell <taylorotwell@gmail.com>
2016-06-06 21:43:34 -0500

    message updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,9 +5,9 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a given Closure or controller and enjoy the fresh air.
 |
 */
 
