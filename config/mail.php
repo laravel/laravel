0b63a692fce186c450a3190d3ef8c09c475d9f97
Dan Rovito <DanRovito@users.noreply.github.com>
2016-06-10 14:22:57 -0400

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_ADDRESS', null), 'name' => env('MAIL_NAME', null)],
 
     /*
     |--------------------------------------------------------------------------
