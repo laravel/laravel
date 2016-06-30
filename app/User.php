9575714700fd1c1796f7376a4bdc65d3683409ff
Taylor Otwell <taylorotwell@gmail.com>
2016-06-29 21:02:16 -0500

    Add notifiable trait to default user.

--- app/User.php
+++ app/User.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
