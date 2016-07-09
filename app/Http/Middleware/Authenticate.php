02274da8fd04f7fe57ff97fe00bd99a7c4cd1d7c
Taylor Otwell <taylorotwell@gmail.com>
2016-07-09 11:27:46 -0500

    code formatting

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -20,7 +20,8 @@ class Authenticate
         if (Auth::guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
-            } 
+            }
+
             return redirect()->guest('login');
         }
 
