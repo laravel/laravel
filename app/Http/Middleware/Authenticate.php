e3bd984b012fdd05a43d3c9fd5db0f5e986edbb5
Kamaro Lambert <kamaroly@gmail.com>
2016-07-09 11:44:42 +0200

    Removed unnecessary else

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -20,9 +20,8 @@ class Authenticate
         if (Auth::guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
-            } else {
-                return redirect()->guest('login');
-            }
+            } 
+            return redirect()->guest('login');
         }
 
         return $next($request);
