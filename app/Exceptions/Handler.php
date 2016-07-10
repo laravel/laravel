677c1bdef3984db46045f2cd8ab8fda811329e62
Taylor Otwell <taylorotwell@gmail.com>
2016-07-10 16:35:03 -0500

    import class

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
