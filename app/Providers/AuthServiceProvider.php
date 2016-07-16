126fea641a03e71367c3fe80b49828e37c8bd24a
Kennedy Tedesco <kennedyt.tw@gmail.com>
2016-07-15 22:02:43 -0300

    [5.3] Remove unused imports

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
