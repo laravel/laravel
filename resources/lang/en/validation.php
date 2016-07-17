be8463d249188c9e51eb6c753dc10aafd84a1610
Dries Vints <dries.vints@gmail.com>
2016-07-17 15:25:39 +0200

    Add new password validation rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -61,6 +61,7 @@ return [
     ],
     'not_in'               => 'The selected :attribute is invalid.',
     'numeric'              => 'The :attribute must be a number.',
+    'password'             => 'The filled in password is incorrect.',
     'present'              => 'The :attribute field must be present.',
     'regex'                => 'The :attribute format is invalid.',
     'required'             => 'The :attribute field is required.',
