93cdacf32c4a4fd9b0bc23c5ab9ba056b3882291
Taylor Otwell <taylorotwell@gmail.com>
2016-07-18 10:41:51 -0500

    remove word

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -17,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
     ];
 
     /**
-     * Register any application authentication / authorization services.
+     * Register any authentication / authorization services.
      *
      * @return void
      */
