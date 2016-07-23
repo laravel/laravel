3f197331b6de88f3b2ce1a3470c328b1b5f42f1c
Taylor Otwell <taylorotwell@gmail.com>
2016-07-23 11:39:11 -0500

    bootstrap vue in app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,2 +1,26 @@
 
-//
+window.Cookies = require('js-cookie');
+
+/**
+ * Vue.js
+ *
+ * Vue is a modern JavaScript for building interactive web interfaces using
+ * reacting data binding and reusable components. Vue's API is clean and
+ * simple, leaving you to focus only on building your next great idea.
+ */
+window.Vue = require('vue');
+
+require('vue-resource');
+
+/**
+ * The XSRF Header
+ *
+ * We'll register a HTTP interceptor to attach the "XSRF" header to each of
+ * the outgoing requests issued by this application. The CSRF middleware
+ * included with Laravel will automatically verify the header's value.
+ */
+Vue.http.interceptors.push(function (request, next) {
+    request.headers['X-XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN');
+
+    next();
+});
