24766d479d6fb978a7b00b838225ead63f983e9a
Taylor Otwell <taylorotwell@gmail.com>
2016-07-23 15:39:33 -0500

    redirect to home if authed

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/');
+            return redirect('/home');
         }
 
         return $next($request);
