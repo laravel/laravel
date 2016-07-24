c309b4fda02a3bc327bb06dd8ee1ae94207330e2
Taylor Otwell <taylorotwell@gmail.com>
2016-07-24 09:20:40 -0500

    add forge to links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                     <a href="https://twitter.com/laravelphp">Twitter</a>
                 </div>
