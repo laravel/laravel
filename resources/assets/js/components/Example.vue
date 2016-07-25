9659e8db112d4729f6ea26f1571ff1909197b065
Taylor Otwell <taylorotwell@gmail.com>
2016-07-24 23:00:18 -0500

    Example component.

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example component!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        ready() {
+            console.log('Component ready.')
+        }
+    }
+</script>
