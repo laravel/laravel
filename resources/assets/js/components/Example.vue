4cc30225eee60833719d22681584cb8623afef32
Taylor Otwell <taylorotwell@gmail.com>
2016-07-24 23:47:31 -0500

    Tweak stub width.

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Example Component</div>
 
