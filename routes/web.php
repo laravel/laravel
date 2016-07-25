e2b297b9fc11f48a14378195c5e6b18691982914
Taylor Otwell <taylorotwell@gmail.com>
2016-07-24 22:22:23 -0500

    Tweak marketing text.

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@
 |
 | This file is where you may define all of the routes that are handled
 | by your application. Just tell Laravel the URIs it should respond
-| to using a given Closure or controller and enjoy the fresh air.
+| to using a Closure or controller method. Build something great!
 |
 */
 
