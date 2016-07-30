96594b4c417e62efdcfb35180d41333d391151a7
Sercan Çakır <srcnckr@gmail.com>
2016-07-30 23:16:03 +0300

    removed unnecessary directives

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -57,8 +57,8 @@ class Handler extends ExceptionHandler
     {
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
-        } else {
-            return redirect()->guest('login');
         }
+
+        return redirect()->guest('login');
     }
 }
