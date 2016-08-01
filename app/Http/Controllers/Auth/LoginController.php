ca7c9732809cb60b80b4be64e0cdefdc5c5b7834
Taylor Otwell <taylorotwell@gmail.com>
2016-08-01 08:51:52 -0500

    fix length

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,7 +14,7 @@ class LoginController extends Controller
     |
     | This controller handles authenticating users for the application and
     | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide this functionality to your applications.
+    | to conveniently provide its functionality to your applications.
     |
     */
 
