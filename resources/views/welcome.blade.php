7f24b9183a1604e5d1592714b5605f332ad9c2e3
Taylor Otwell <taylorotwell@gmail.com>
2016-08-01 10:54:47 -0500

    working on landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,10 +81,10 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laravel-news.com">News</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                    <a href="https://twitter.com/laravelphp">Twitter</a>
                 </div>
             </div>
         </div>
