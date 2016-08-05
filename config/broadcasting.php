51113bd5317f4f847462d1470474ab2897d11e46
Taylor Otwell <taylorotwell@gmail.com>
2016-08-05 15:51:19 -0500

    Default broadcasting driver to null.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'pusher'),
+    'default' => env('BROADCAST_DRIVER', 'null'),
 
     /*
     |--------------------------------------------------------------------------
