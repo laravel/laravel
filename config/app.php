e6cc60349df90190c5c5597e0980bb7f636a3866
Taylor Otwell <taylorotwell@gmail.com>
2016-08-08 16:10:19 -0500

    add notification facade

--- config/app.php
+++ config/app.php
@@ -208,6 +208,7 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
