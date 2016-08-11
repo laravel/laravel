8a9f89d3cfcaad12cacb74e9cb1a1eef9636811d
Taylor Otwell <taylorotwell@gmail.com>
2016-08-11 14:33:22 -0500

    fix wording

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -20,7 +20,7 @@ window.Vue = require('vue');
 require('vue-resource');
 
 /**
- * We'll register a HTTP interceptor to attach the "XSRF" header to each of
+ * We'll register a HTTP interceptor to attach the "CSRF" header to each of
  * the outgoing requests issued by this application. The CSRF middleware
  * included with Laravel will automatically verify the header's value.
  */
