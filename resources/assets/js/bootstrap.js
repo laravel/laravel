655dbadd1eceab8ff17e084b14943db59ddec40a
Taylor Otwell <taylorotwell@gmail.com>
2016-08-11 14:47:29 -0500

    tweaking default setup

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -30,3 +30,16 @@ Vue.http.interceptors.push(function (request, next) {
 
     next();
 });
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from "laravel-echo"
+
+// window.Echo = new Echo({
+//     connector: 'pusher',
+//     key: 'your-pusher-key'
+// });
