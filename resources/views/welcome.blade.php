788a648165bd254a403193a84327db427079a7f4
Taylor Otwell <taylorotwell@gmail.com>
2016-08-11 21:31:42 -0500

    match order to website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,8 +80,8 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laravel-news.com">News</a>
                     <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
