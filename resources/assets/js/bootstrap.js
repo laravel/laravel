7571f2b5b4ecca329cf508ef07dd113e6dbe53fc
Adriaan Zonnenberg <adriaanzon@users.noreply.github.com>
2016-08-18 21:46:38 +0200

    Use module name instead of path

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,7 +8,7 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass/assets/javascripts/bootstrap');
+require('bootstrap-sass');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
