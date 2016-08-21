792dcd48c8ad9887f08db6ad9efd0890603ddd53
Adam Wathan <adam.wathan@gmail.com>
2016-08-21 14:23:50 +0200

    Compute hash only once and store in static variable

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,15 +12,12 @@
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password = null;
+
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
-
-        // Use a precomputed hash of the word "secret" instead of using bcrypt directly.
-        // Since bcrypt is intentionally slow, it can really slow down test suites in
-        // large applications that use factories to generate models in many tests.
-        'password' => '$2y$10$oPCcCpaPQ69KQ1fdrAIL0eptYCcG/s/NmQZizJfVdB.QOXUn5mGE6',
-
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
