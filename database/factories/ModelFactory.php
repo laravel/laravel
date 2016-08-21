b041a0387808bb90ffa56308c70665e1c8d01efd
Adam Wathan <adam.wathan@gmail.com>
2016-08-21 12:29:55 +0200

    Use precomputed hash instead of bcrypt in ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+
+        // Use a precomputed hash of the word "secret" instead of using bcrypt directly.
+        // Since bcrypt is intentionally slow, it can really slow down test suites in
+        // large applications that use factories to generate models in many tests.
+        'password' => '$2y$10$oPCcCpaPQ69KQ1fdrAIL0eptYCcG/s/NmQZizJfVdB.QOXUn5mGE6',
+
         'remember_token' => str_random(10),
     ];
 });
