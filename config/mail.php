13c3b73bee304741f466a06cdeaa5fc8dd27d844
Muhammad Habib Rohman <mhrohman@live.com>
2016-08-25 06:14:23 +0700

    Update config/mail.php add custom from address and name to .env file

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
