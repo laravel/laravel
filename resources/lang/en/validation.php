7da6edf8c14772f14ff11616199fb16bd19909ae
Taylor Otwell <taylorotwell@gmail.com>
2016-09-02 08:48:07 -0500

    working on message

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -81,7 +81,7 @@ return [
     'string'               => 'The :attribute must be a string.',
     'timezone'             => 'The :attribute must be a valid zone.',
     'unique'               => 'The :attribute has already been taken.',
-    'uploaded'             => 'The :attribute uploading failed.',
+    'uploaded'             => 'The :attribute failed to upload.',
     'url'                  => 'The :attribute format is invalid.',
 
     /*
