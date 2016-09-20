dcf971805df5e5cb86b03ced4240c5bbbe516164
Kai Rienow <krienow@users.noreply.github.com>
2016-09-20 08:23:40 +0200

    Add unique modifier

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,7 +16,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->name,
-        'email' => $faker->safeEmail,
+        'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
