a80e5bca0b36e2377b884772c560f7c871d0bf1f
Taylor Otwell <taylorotwell@gmail.com>
2016-09-20 08:38:45 -0500

    add bus alias

--- config/app.php
+++ config/app.php
@@ -197,6 +197,7 @@ return [
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
+        'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
