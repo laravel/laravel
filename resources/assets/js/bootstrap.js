0ab1eb9a0c5e4f93d66503bc7782ad5c3a995eab
lbausch <lbausch@users.noreply.github.com>
2016-09-21 10:48:44 +0200

    Fix setting X-CSRF-TOKEN header
    See https://github.com/vuejs/vue-resource/issues/412 for reference

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -26,7 +26,7 @@ require('vue-resource');
  */
 
 Vue.http.interceptors.push((request, next) => {
-    request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
+    request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
 
     next();
 });
