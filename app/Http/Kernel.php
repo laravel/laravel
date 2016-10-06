38d8baa563fdabbf790ca15afae10a2d14a76acf
Christopher Pitt <cgpitt@gmail.com>
2016-10-06 21:42:03 +1300

    Reuse the bindings alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'bindings',
         ],
 
         'api' => [
