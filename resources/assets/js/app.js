8d148fb9974406611472e2037bd658bdac188be9
Jeff Lawrence <shogun88888888@users.noreply.github.com>
2016-10-09 21:28:23 -0400

    Update app.js
    Change comment to point to #app instead of body

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,8 +9,8 @@ require('./bootstrap');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
+ * a page element with an id of #app. From here, you may begin adding
+ * components to the application, or feel free to tweak this setup for your needs.
  */
 
 Vue.component('example', require('./components/Example.vue'));
