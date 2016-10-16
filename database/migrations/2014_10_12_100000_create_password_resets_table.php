3ecc0e39ed3b6c3c1053a7a5454ebd751f69a0d4
Diogo Azevedo <diogoazevedos@gmail.com>
2016-10-16 14:34:56 -0200

    Use dropIfExists instead of drop

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::dropIfExists('password_resets');
     }
 }
