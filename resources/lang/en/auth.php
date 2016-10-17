a1d4ad54039dd2ecf27da98b3eb92aa06230d23f
corentin <corentin@headoo.com>
2016-10-17 11:39:38 +0200

    Clean code: align translations in array

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed'   => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
