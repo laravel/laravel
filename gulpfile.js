e7a111c17348d92a9f1d0c59fa61358a0f7ccabc
Mubashar Abbas <mubashir.abbas@outlook.com>
2016-10-18 13:07:56 +0500

    made it a little more consistent with the previous sentence.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ require('laravel-elixir-vue');
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
+ | file for your application, as well as publishing vendor resources.
  |
  */
 
