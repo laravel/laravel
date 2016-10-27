74e35eb0849669c4305fb787a42b559683287d90
Taylor Otwell <taylorotwell@gmail.com>
2016-10-27 09:39:51 -0500

    formatting

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "web" middleware group. Build something great!
+| contains the "web" middleware group. Now create something great!
 |
 */
 
