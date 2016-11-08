e72463dc6c2f977f8e49aa0bd535a5dbed968aad
Taylor Otwell <taylorotwell@gmail.com>
2016-11-08 10:52:43 -0600

    change var name

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_SENDER_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_SENDER_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
