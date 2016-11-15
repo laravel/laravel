9ca8ed99609abd473da42f5affdab266d867b532
Diogo Azevedo <diogoazevedos@gmail.com>
2016-11-14 23:44:01 -0200

    Update the default redis host

--- config/database.php
+++ config/database.php
@@ -110,7 +110,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
