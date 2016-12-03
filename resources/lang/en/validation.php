55df01b6028f250d18c3f9ddf76b7913d494e0ad
Andrey Helldar <helldar@ai-rus.com>
2016-12-03 21:04:30 +0900

    Correction values for "exists"

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -39,7 +39,7 @@ return [
     'dimensions'           => 'The :attribute has invalid image dimensions.',
     'distinct'             => 'The :attribute field has a duplicate value.',
     'email'                => 'The :attribute must be a valid email address.',
-    'exists'               => 'The selected :attribute is invalid.',
+    'exists'               => 'The selected :attribute already exists.',
     'file'                 => 'The :attribute must be a file.',
     'filled'               => 'The :attribute field is required.',
     'image'                => 'The :attribute must be an image.',
