ba5bde7c91efca9d6f6fbf034de45872767be7df
Taylor Otwell <taylor@laravel.com>
2016-12-05 12:58:44 -0600

    add trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,29 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
-    }
+    use CreatesApplication;
 }
