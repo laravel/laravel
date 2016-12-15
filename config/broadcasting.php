0c33812af763f11735d1136e628163a0b177ba64
Kevin Houghton <KevinHoughton@users.noreply.github.com>
2016-12-15 19:54:45 +0100

    StyleCI check

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_CLUSTER', 'mt1'),
-                'encrypted' => env('PUSHER_ENCRYPTED', false)
+                'encrypted' => env('PUSHER_ENCRYPTED', false),
             ],
         ],
 
