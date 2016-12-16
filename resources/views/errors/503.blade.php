696ab5149e6b6169f73b75321eaabf47a4a26645
Taylor Otwell <taylor@laravel.com>
2016-12-16 16:49:17 -0600

    make 503 page more consistent with welcome page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,46 +1,61 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <title>Be right back.</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <title>Laravel</title>
 
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
         <style>
             html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
                 font-weight: 100;
-                font-family: 'Lato', sans-serif;
+                height: 100vh;
+                margin: 0;
             }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
             }
 
             .content {
                 text-align: center;
-                display: inline-block;
             }
 
             .title {
-                font-size: 72px;
-                margin-bottom: 40px;
+                font-size: 84px;
             }
         </style>
     </head>
     <body>
-        <div class="container">
+        <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">
+                    Be right back.
+                </div>
             </div>
         </div>
     </body>
