e78ef3ee423d451c6aa210dae372473ae9740994
Taylor Otwell <taylor@laravel.com>
2016-12-17 10:14:49 -0600

    tweak js file

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -25,7 +25,10 @@ window.Vue = require('vue');
  */
 
 window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest'
+};
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
