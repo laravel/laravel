52f0196fd3f99113fb838f813564e0c78f133b4d
Taylor Otwell <taylor@laravel.com>
2016-12-30 15:46:01 -0600

    Move broadcast channel registration to a routes file.
    These are very similar to routes in that they are channel endpoints
    that your application supports and they also fully support route model
    binding in Laravel 5.4. Upgraded applications do not need to make this
    change if they do not want to.

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
+        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
         'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
