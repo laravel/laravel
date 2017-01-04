da62be60e8b15b087f48a057f2460409d55f3b1b
Taylor Otwell <taylor@laravel.com>
2017-01-04 08:51:47 -0600

    Put URL config option on the disk to make it obvious how to customize.

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
+            'url' => url('storage'),
             'visibility' => 'public',
         ],
 
