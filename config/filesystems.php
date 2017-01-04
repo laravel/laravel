251140ef8b2e283dec3025aa6a0cbfd6594c9dad
Taylor Otwell <taylor@laravel.com>
2017-01-04 12:09:43 -0600

    use env var

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
