b136a14c54240288234ad6a9d464e4c2ddd4e1ec
Ryan Brereton-Canham <ryzaar@hotmail.com>
2017-01-17 11:58:53 +1030

    Update s3 filesystem to use environment variables

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,10 +56,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => 'your-key',
-            'secret' => 'your-secret',
-            'region' => 'your-region',
-            'bucket' => 'your-bucket',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_REGION'),
+            'bucket' => env('AWS_S3_BUCKET'),
         ],
 
     ],
