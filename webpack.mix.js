a205b127c654e5152d6dc49d98494d7b84e1ea85
Taylor Otwell <taylor@laravel.com>
2017-01-17 14:30:48 -0600

    add mix settings

---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+let mix = require('laravel-mix').mix;
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
