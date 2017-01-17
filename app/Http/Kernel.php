54c5e1585a4b99acb749e3c6ab73043270768e7c
Taylor Otwell <taylor@laravel.com>
2017-01-17 15:35:36 -0600

    Trim and convert strings to nulls.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,6 +16,8 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
