9b526e4f62a78ea02d190b4447842186ffb28219
Romain Lanz <romain.lanz@slynova.ch>
2017-01-24 10:22:30 +0100

    chore(mix): extract by default vendor js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+   .extract(['vue', 'jquery'])
    .sass('resources/assets/sass/app.scss', 'public/css');
