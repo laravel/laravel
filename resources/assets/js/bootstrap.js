0cdcc779bfe4d2aedf15de26b14e1ff940202334
Michael Zhang <hi@monomichael.com>
2017-01-30 12:07:53 -0500

    Make axios automatically send the `X-CSRF-TOKEN`

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -28,6 +28,7 @@ window.Vue = require('vue');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
     'X-Requested-With': 'XMLHttpRequest'
 };
 
