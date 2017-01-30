6a3e6cd88634aaca89551e74ab256db1839d0b7b
Ian Rodrigues <ianrodriguesbr@gmail.com>
2017-01-30 10:50:00 -0300

    Added X-CSRF-TOKEN to axios default header

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -28,7 +28,8 @@ window.Vue = require('vue');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest'
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': Laravel.csrfToken
 };
 
 /**
