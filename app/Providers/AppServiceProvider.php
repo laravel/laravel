701a24aa1a99fbc8df32f5e4562139582121b0e2
Advaith <advaitharunjeena@gmail.com>
2017-03-03 11:14:18 +0530

    StyleCI fix applied

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
