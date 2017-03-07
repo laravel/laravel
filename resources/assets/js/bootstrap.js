37798398c1cfe806cf74e462abbc719183d17f30
Renjith VK <renjithrajeevvk@gmail.com>
2017-03-07 17:42:54 +0530

    Globally initialized JQuery
    Resolve "Uncaught ReferenceError: jQuery is not defined" while try to install plugins

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -7,7 +7,7 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-window.$ = window.jQuery = require('jquery');
+global.$ = global.jQuery = require('jquery');
 
 require('bootstrap-sass');
 
