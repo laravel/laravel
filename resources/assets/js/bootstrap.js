31c262301899b6cd1a4ce2631ad0e313b444b131
Taylor Otwell <taylor@laravel.com>
2017-03-14 08:31:41 -0500

    update example

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -40,6 +40,8 @@ window.axios.defaults.headers.common = {
 
 // import Echo from "laravel-echo"
 
+// window.Pusher = require('pusher-js');
+
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: 'your-pusher-key'
