be5f97a7934f9b1c246ea55d7ca1d5af172ec122
Hafiq <hafiqiqmal93@gmail.com>
2017-03-22 10:13:06 +0800

    Syntax error or access violation: 1071 Specified key was too long; max key length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
