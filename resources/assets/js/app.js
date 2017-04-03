34a25e0e62a1c0e2727baac19e89df5633eecdc9
Matthew Davis <matt@mattdavis.co.uk>
2017-04-03 21:46:58 +0100

    Remove blank lines in assets

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,3 @@
-
 window._ = require('lodash');
 
 /**
