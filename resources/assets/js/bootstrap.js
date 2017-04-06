0d31e2993b3ee681f4aeef486619c90a26f9086e
Matt Isenhower <matt@isenhower.com>
2017-04-06 15:58:57 -0700

    Add to axios defaults instead of overwriting them
    This change ensures the default "Accept" header specified by axios is retained.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -27,10 +27,8 @@ window.Vue = require('vue');
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Laravel.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest'
-};
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
