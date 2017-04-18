aab61eaa368d1893ce9921ad8aeefb383c595d57
Laurence Ioannou <github@theshiftexchange.com>
2017-04-18 13:37:10 +0100

    Update CreatesApplication.php

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -13,6 +13,8 @@ trait CreatesApplication
      */
     public function createApplication()
     {
+        define('LARAVEL_START', microtime(true));
+        
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
