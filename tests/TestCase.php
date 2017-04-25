6ebb329d4cd75a389eb380290b080185192001e0
cmac <chrisnowlan321@gmail.com>
2017-04-25 15:38:33 -0400

    Added ajax get and post request helpers
    I use these to test ajax protected routes.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,20 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    /**
+    * Make ajax POST request
+    */
+    protected function ajaxPost($uri, array $data = [])
+    {
+        return $this->post($uri, $data, array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
+    }
+
+    /**
+    * Make ajax GET request
+    */
+    protected function ajaxGet($uri, array $data = [])
+    {
+        return $this->get($uri, array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
+    }
 }
