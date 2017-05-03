94b39dc576a091f45d72e59c6ac479743f8f9546
Hélio <insign@gmail.com>
2017-05-03 13:22:10 -0300

    Fix the commit for pass StyleCI

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-        
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
