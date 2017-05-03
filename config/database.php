e26bd3ffb01f431935f207dc7a6fdcbd5600d6f7
Hélio <insign@gmail.com>
2017-05-03 13:20:25 -0300

    Add sqlsrv as group connection
    Since the doc says that the Laravel supports SQL Server out of the box, makes sense add it, out of the box.

--- config/database.php
+++ config/database.php
@@ -66,6 +66,16 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+        
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+        ],
 
     ],
 
