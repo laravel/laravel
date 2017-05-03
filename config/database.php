bdca9d478115d21ef236cdf4d69802b5967437dd
Frederik Sauer <fritten.keez@gmail.com>
2017-05-03 23:55:48 +0200

    Added port to sqlsrv settings
    Most installations won't work without it.

--- config/database.php
+++ config/database.php
@@ -70,6 +70,7 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
