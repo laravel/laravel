fbd3c3bf5db3a1efc80cf937fd566c1c1d7b599f
Quezler <Quezler@me.com>
2017-05-15 17:16:15 +0200

    Return json if sucessfull login expects it

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'These credentials do not match our records.',
+    'success' => 'Successfully authenticated.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
