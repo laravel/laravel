ef5bfb1daed65dcf8b0d339d21c05bd21a999e88
Mohamed Said <themohamedsaid@gmail.com>
2017-05-25 09:48:50 +0200

    update JSON error message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,7 +52,7 @@ class Handler extends ExceptionHandler
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         return $request->expectsJson()
-                    ? response()->json(['error' => 'Unauthenticated.'], 401)
+                    ? response()->json(['message' => 'Unauthenticated.'], 401)
                     : redirect()->guest(route('login'));
     }
 }
