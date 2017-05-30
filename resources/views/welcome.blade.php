742fcbf2ca8d960d8226a07f7a853cce579aec88
Hasib Ullah Khan <technohasibullah@gmail.com>
2017-05-30 15:11:23 +0530

    support multi lingual
    valid even if locale is changed at run time

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ config('app.locale') }}">
+<html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
