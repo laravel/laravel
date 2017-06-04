74e0a50d6c9f5c336f55b7fa8abc8a547def59df
Mateus Guimarães <mateus.jatene@gmail.com>
2017-06-04 17:37:54 -0300

    always respond with json on api routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'json',
         ],
     ];
 
@@ -56,5 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'json' => \App\Http\Middleware\RespondWithJson::class,
     ];
 }
