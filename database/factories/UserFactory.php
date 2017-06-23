64641aa092f3c1fd899a85027e0c9d428ce68329
Chabib Nurochman <chabib.nr@gmail.com>
2017-06-23 15:50:26 +0700

    Use null coalescing operator

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?? bcrypt('secret'),
+        'password' => $password ?? $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
