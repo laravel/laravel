c9e47da89503a2a7e07849695803e80c209a429c
Brian Retterer <bretterer@gmail.com>
2017-06-28 17:52:56 -0400

    Changes header keys for TrustProxies to non deprecated version

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -21,9 +21,9 @@ class TrustProxies extends Middleware
      */
     protected $headers = [
         Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_CLIENT_IP => 'X_FORWARDED_FOR',
-        Request::HEADER_CLIENT_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_CLIENT_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
     ];
 }
