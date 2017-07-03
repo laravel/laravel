61518316b9228cfd74c039fc83428f5647e34b43
Burak Olgun <burakolgun91@gmail.com>
2017-07-03 15:48:46 +0300

    fist com mit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <style>
-            html, body {
+            html, body{
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
