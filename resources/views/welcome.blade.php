2b5ecab37db304e24d2ad0545be5ad8cce3c40b1
Eren Akpınar <erenakpinar123@gmail.com>
2017-07-03 23:22:02 +0300

    Initial commit.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    İkamp
+                    İkamp 2017
                 </div>
 
                 <div class="links">
