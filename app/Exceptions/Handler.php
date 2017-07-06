cec1b1bf947ce47a2c218a37544dff2b24635db2
Taylor Otwell <taylor@laravel.com>
2017-07-06 12:27:35 -0500

    fix error code

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -55,7 +55,7 @@ class Handler extends ExceptionHandler
         $errors = $exception->validator->errors()->messages();
 
         return $request->expectsJson()
-                    ? response()->json(['message' => $exception->getMessage(), 'errors' => $errors])
+                    ? response()->json(['message' => $exception->getMessage(), 'errors' => $errors], 422)
                     : redirect()->back()->withInput()->withErrors(
                             $errors, $exception->errorBag
                       );
