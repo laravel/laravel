d75800052de15ef3ed251349f60898984a8fbcd3
Taylor Otwell <taylor@laravel.com>
2017-07-11 16:53:04 -0500

    change default

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -8,12 +8,14 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 class EventServiceProvider extends ServiceProvider
 {
     /**
-     * The event listeners for the application.
+     * The event listener mappings for the application.
      *
      * @var array
      */
-    protected $listeners = [
-        //
+    protected $listen = [
+        'App\Events\Event' => [
+            'App\Listeners\EventListener',
+        ],
     ];
 
     /**
