5d54c21ea869a7a5b503f0899307e4728feed11b
Taylor Otwell <taylor@laravel.com>
2017-07-17 09:11:10 -0500

    auto loads commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,6 +35,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
+        $this->load(__DIR__.'/Commands');
+
         require base_path('routes/console.php');
     }
 }
