71d38b044dad82eb8dfdba2ee2edb69eca415324
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2017-07-25 20:26:13 +0200

    Add auth directive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,12 +68,12 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @if (Auth::check())
+                    @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
-                    @endif
+                    @endauth
                 </div>
             @endif
 
