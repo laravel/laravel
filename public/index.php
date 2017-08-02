91a80916851b255892e56bf07e077de684fe5f92
Yusuf Damiloju <yusufdamiloju@gmail.com>
2017-08-02 10:57:00 +0100

    fix comment

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
 |
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
+| into the script here so that we don't have to worry about manually
 | loading any of our classes later on. It feels great to relax.
 |
 */
