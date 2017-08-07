b40de4e033f6fd99711700875bd9a04be259ab29
Guilherme Siani <guilhermesiani@users.noreply.github.com>
2017-08-06 21:56:29 -0300

    Created migration for example model

---
+++ database/migrations/2017_06_10_200000_create_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 20);
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
