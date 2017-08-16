bc17b19f62c6bcb6c96ea61f8879835927d70995
Alexandre Bordiere <al.bordiere@gmail.com>
2017-08-16 14:30:25 +1000

    Create signIn method in TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param  mixed  $user
+     * @return $this
+     */
+    protected function signIn($user = null)
+    {
+        if (is_null($user)) {
+            $user = factory(\App\User::class)->create();
+        }
+
+        return $this->actingAs($user);
+    }
 }
