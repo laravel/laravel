80e96d276a5c232705ed62dfd55dec50e6030c8c
Taylor Otwell <taylor@laravel.com>
2017-09-11 08:04:12 -0500

    Revert "Use database_path helper for sqlite database filename specified in .env"

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => database_path(env('DB_DATABASE', 'database.sqlite')),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
 
