1341b66b79bc2367d3475148d85a49dc69a0856f
Keangkai Poungmanee <m.keangkai@gmail.com>
2017-10-07 23:53:24 +0700

    Update server.php
    change a little comment

--- server.php
+++ server.php
@@ -11,9 +11,10 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
+
+/* This file allows us to emulate Apache's "mod_rewrite" functionality from the
+   built-in PHP web server. This provides a convenient way to test a Laravel
+   application without having installed a "real" web server software here.*/
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
