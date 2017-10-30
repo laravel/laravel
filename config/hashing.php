d1b496bd330b6a928d69f63ba34838c8ee7f9615
Michael Lundbøl <michael.lundboel@gmail.com>
2017-10-31 00:32:49 +0100

    Add new configuration file.

---
+++ config/hashing.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Password hashing Algorithm
+    |--------------------------------------------------------------------------
+    |
+    | Laravel supports both Bcrypt and Argon2i Hashing algorithm as drivers for the
+    | Protection of passwords. You may specify which one you're using throughout
+    | your application here. By default, Laravel is setup for the bcrypt algorithm.
+    |
+    */
+
+    'driver' => env('HASHING_DRIVER', 'bcrypt'),
+
+];
