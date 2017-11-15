e7c882c02203db9409b73a44bf36a3e6b71c3666
Enimiste <enimiste@users.noreply.github.com>
2017-11-15 10:50:09 +0000

    Fix CycleCi failed
    Remplace  * by |

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         /*
-        * If you receive this SQL Error: 
-        * "SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;", 
-        * 
-        * Uncomment the next line
+        | If you receive this SQL Error:
+        | "SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;",
+        |
+        | Uncomment the next line
         */
         // \Schema::defaultStringLength(191);
     }
