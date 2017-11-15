9b781dd7c9c8dae6e25724c126d81e61a34ee440
Laurence Ioannou <github@theshiftexchange.com>
2017-11-15 18:08:21 +0000

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,9 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
 }
