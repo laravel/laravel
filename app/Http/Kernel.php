6e9c0c885257d73d6af0dd60cf75a76266d3c66d
Kévin Dunglas <dunglas@gmail.com>
2017-12-18 16:29:25 +0100

    Add the SetCacheHeaders middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cache' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
     ];
 }
