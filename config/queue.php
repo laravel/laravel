bafe4599636cdd31dd500447f3e92c539ef40d91
Pavinthan <pavinthan@outlook.com>
2017-12-19 12:44:14 +0530

    Name sort

--- config/queue.php
+++ config/queue.php
@@ -53,8 +53,8 @@ return [
             'key' => env('SQS_KEY', 'your-public-key'),
             'secret' => env('SQS_SECRET', 'your-secret-key'),
             'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
-            'queue' => env('SQS_QUEUE_NAME', 'your-queue-name'),
-            'region' => env('SQS_QUEUE_REGION', 'us-east-1'),
+            'queue' => env('SQS_NAME', 'your-queue-name'),
+            'region' => env('SQS_REGION', 'us-east-1'),
         ],
 
         'redis' => [
