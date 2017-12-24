80b59fd375effd36e56fec87112e7fc4dbc12283
Mohamed Said <themohamedsaid@gmail.com>
2017-12-24 13:24:14 +0200

    fix style

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'encrypted' => true,
-                'cluster' => env('PUSHER_APP_CLUSTER')
+                'cluster' => env('PUSHER_APP_CLUSTER'),
             ],
         ],
 
