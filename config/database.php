f0d3dfeb8ef1b2a50a673c765a21c11f6a28326a
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2017-12-24 19:01:59 +0300

    Redis Database Env upgraded

--- config/database.php
+++ config/database.php
@@ -112,7 +112,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+            'database' => env('REDIS_DATABASE', 0),
         ],
 
     ],
