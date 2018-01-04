5f28d1de825a70dcc4e834071636ace57ed639ad
vagrant <vagrant@homestead>
2018-01-04 07:14:57 +0000

    tesing

---
+++ app/Http/Controllers/test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class test extends Controller
+{
+    public function index()
+    {
+    	return view('test');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', 'test@index');
