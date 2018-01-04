cbef1d5fbac253837b9feb9427581b2be4e82e70
vagrant <vagrant@homestead>
2018-01-04 07:25:06 +0000

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,6 +88,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
