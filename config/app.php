e4e8eea1bc9d3ba7d4181954a3c700981e17db01
Dmitry Borzyonok <dima.matters@gmail.com>
2018-01-14 21:04:13 +0200

    Remove unused alias "Eloquent"

--- config/app.php
+++ config/app.php
@@ -203,7 +203,6 @@ return [
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
         'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
