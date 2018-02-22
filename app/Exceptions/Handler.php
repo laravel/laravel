e059c7ffc6549bfe9556a5695cc0aacb6ac8c229
Andrey Helldar <helldar@ai-rus.com>
2018-02-22 10:17:58 +0300

    Update phpdoc

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,7 +32,7 @@ class Handler extends ExceptionHandler
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
      * @param  \Exception  $exception
-     * @return void
+     * @throws \Exception
      */
     public function report(Exception $exception)
     {
@@ -44,7 +44,7 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function render($request, Exception $exception)
     {
