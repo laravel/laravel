2572ce1e3698cfdb6563a725aa5d36692665e805
Michal <m.putkowski@gmail.com>
2018-02-27 19:03:03 +0100

    add sftp to supported storage drivers

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,8 +37,9 @@ return [
     | may even configure multiple disks of the same driver. Defaults have
     | been setup for each driver as an example of the required options.
     |
-    | Supported Drivers: "local", "ftp", "s3", "rackspace"
+    | Supported Drivers: "local", "ftp", "sftp", "s3", "rackspace"
     |
+    | Note: SFTP driver is supported as of Laravel 5.6.6.
     */
 
     'disks' => [
