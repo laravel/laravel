e3312827e5ecb2db8e51f80d21b7d279ed63316f
Gaurav Makhecha <gauravmakhecha@gmail.com>
2018-03-08 10:49:16 +0530

    No closure based route by default to allow Route caching
    Many projects do not touch API routes at all and this prevents Route caching on the server without removing it.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,4 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'APIController@getUser');
