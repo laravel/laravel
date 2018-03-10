293fae6bd8285d076cedf2b8147a20e4090aa5bc
vlakoff <vlakoff@gmail.com>
2018-03-10 02:44:56 +0100

    Add message for "Not Regex" validation rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -65,6 +65,7 @@ return [
         'array'   => 'The :attribute must have at least :min items.',
     ],
     'not_in'               => 'The selected :attribute is invalid.',
+    'not_regex'            => 'The :attribute format is invalid.',
     'numeric'              => 'The :attribute must be a number.',
     'present'              => 'The :attribute field must be present.',
     'regex'                => 'The :attribute format is invalid.',
