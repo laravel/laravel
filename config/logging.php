29a1739099a2011f5de2a60fccf1c4ac16f007cf
Taylor Otwell <taylor@laravel.com>
2018-03-14 14:19:07 -0500

    add wording

--- config/logging.php
+++ config/logging.php
@@ -27,7 +27,8 @@ return [
     | you a variety of powerful log handlers / formatters to utilize.
     |
     | Available Drivers: "single", "daily", "slack", "syslog",
-    |                    "errorlog", "custom", "stack"
+    |                    "errorlog", "monolog",
+    |                    "custom", "stack"
     |
     */
 
