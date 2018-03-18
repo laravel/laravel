4f8a093dcc347e830023e3d1e5a8c9060120e573
Hugues Joyal <moi@huguesjoyal.com>
2018-03-18 07:47:34 -0400

    Add hashing configuration

--- config/hashing.php
+++ config/hashing.php
@@ -17,4 +17,43 @@ return [
 
     'driver' => 'bcrypt',
 
+    /*
+    |--------------------------------------------------------------------------
+    | bcrypt options
+    |--------------------------------------------------------------------------
+    |
+    | We could define the number of rounds the bcrypt algo will be using.
+    |
+    | The two digit cost parameter is the base-2 logarithm of the iteration
+    | count for the underlying Blowfish-based hashing algorithmeter and must
+    | be in range 04-31, values outside this range will cause crypt() to fail
+    |
+    | Default: 10
+    */
+    'bcrypt' => [
+        'rounds' => 10
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | argon options
+    |--------------------------------------------------------------------------
+    |
+    | These settings could be adjusted depending on your hardware.
+    |
+    | time: Maximum amount of time it may take to compute the Argon2 hash.
+    |        (default: 2)
+    |
+    | memory: Maximum memory (in bytes) that may be used to compute the Argon2 hash
+    |        (default : 1024)
+    |
+    | threads: Number of threads to use for computing the Argon2 hash
+    |        (default : 2)
+    |
+    */
+    'argon' => [
+        'time' => 2,
+        'memory' => 1024,
+        'threads' => 2
+    ]
 ];
