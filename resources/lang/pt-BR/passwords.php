a060f5cf1588d3471179dc72eb76399369d47d15
Fábio Jânio <fabiojaniolima@gmail.com>
2018-04-04 11:42:40 -0300

    Corrected style

--- resources/lang/pt-BR/passwords.php
+++ resources/lang/pt-BR/passwords.php
@@ -17,6 +17,6 @@ return [
     'reset' => 'Sua senha foi redefinida!',
     'sent' => 'Enviamos seu link de redefinição de senha por e-mail!',
     'token' => 'Este token de redefinição de senha é inválido.',
-    'user' => "Não encontramos um usuário com esse endereço de e-mail.",
+    'user' => 'Não encontramos um usuário com esse endereço de e-mail.',
 
 ];
