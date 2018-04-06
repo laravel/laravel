20b9daa8f365639607a4578f2bfda276253067cc
Viktor Szépe <viktor@szepe.net>
2018-04-06 20:13:20 +0200

    Request::HEADER_X_FORWARDED_ALL is an integer
    it is int(30)

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -17,7 +17,7 @@ class TrustProxies extends Middleware
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var string
+     * @var integer
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
