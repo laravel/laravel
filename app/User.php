ba7c505bc3a53af5dd83d34c8fffc64c0667a990
Alex Mayer <amayer5125@gmail.com>
2018-04-11 21:47:38 -0400

    User Array Elements To Multiple Lines
    Match other formatting of array elements
    Alphabetize elements

--- app/User.php
+++ app/User.php
@@ -15,7 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email',
+        'name',
+        'password',
     ];
 
     /**
@@ -24,6 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 }
