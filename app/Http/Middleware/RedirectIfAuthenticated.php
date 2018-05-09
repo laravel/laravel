43f70f11aadbef51263e53cb09521486449fa7e6
Dionysios Arvanitis <dionysios.arvanitis@opekepe.gr>
2018-05-09 11:53:44 +0300

    Redirect to dynamically defined home route

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
@@ -18,7 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return Redirect::home();
         }
 
         return $next($request);
