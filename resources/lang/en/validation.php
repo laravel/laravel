7ff917aa203b2f0869761c99283d18b6a2eb6cf8
Dries Vints <dries.vints@gmail.com>
2018-05-18 14:52:24 +0200

    Fix alpha_dash
    It also accepts underscores as valid signs as explicitly stated in the docs and source code so I believe it would be best to also add it to the validation message.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -18,7 +18,7 @@ return [
     'after'                => 'The :attribute must be a date after :date.',
     'after_or_equal'       => 'The :attribute must be a date after or equal to :date.',
     'alpha'                => 'The :attribute may only contain letters.',
-    'alpha_dash'           => 'The :attribute may only contain letters, numbers, and dashes.',
+    'alpha_dash'           => 'The :attribute may only contain letters, numbers, dashes and underscores.',
     'alpha_num'            => 'The :attribute may only contain letters and numbers.',
     'array'                => 'The :attribute must be an array.',
     'before'               => 'The :attribute must be a date before :date.',
