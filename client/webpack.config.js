20b2e8cb9a956db3c0e865055893d711efb50386
Dario Govergun <dpgover@gmail.com>
2018-05-22 11:44:10 -0300

    Digbang's way (Part 4): - Added a node container

---
+++ client/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: [
+        'webpack-dev-server/client?http://project-name.local/', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        './client/main',
+    ],
+    customerService: [
+        'webpack-dev-server/client?http://project-name.local/', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        './client/customerService',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: '[name].js',
+    publicPath: '/assets/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.(svg|woff|woff2|eot|ttf|png|jpg)(\?.*$|$)/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEBUG__: true,
+    }),
+    // Avoid publishing files when compilation fails
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  // Create Sourcemaps for the bundle
+  devtool: '#eval-source-map',
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+    modules: [path.resolve(__dirname, '..', 'client'), 'node_modules'],
+  },
+};
