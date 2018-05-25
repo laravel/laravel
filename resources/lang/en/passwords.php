5237e0c3a6e3359ddf98ef3a9e67dd6417b08f90
Lasse Rafn <lasserafn@gmail.com>
2018-05-26 00:28:15 +0200

    Update passwords.php
    StyleCI

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -18,7 +18,7 @@ return [
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
-    
+
     'mail' => [
         'intro' => 'You are receiving this email because we received a password reset request for your account.',
         'button' => 'Reset Password',
