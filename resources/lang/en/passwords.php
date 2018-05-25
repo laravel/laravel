f6c51e8e23049fdaf15121748a58f8f8c33b877e
Lasse Rafn <lasserafn@gmail.com>
2018-05-26 00:26:43 +0200

    Update passwords.php
    StyleCI

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -22,7 +22,7 @@ return [
     'mail' => [
         'intro' => 'You are receiving this email because we received a password reset request for your account.',
         'button' => 'Reset Password',
-        'disclaimer' => 'If you did not request a password reset, no further action is required.'
+        'disclaimer' => 'If you did not request a password reset, no further action is required.',
     ]
 
 ];
