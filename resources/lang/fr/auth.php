ed773fad54ecda6765e0dac9666b40bf7fc88194
Adnan RIHAN <adnan@rihan.fr>
2018-06-05 01:15:17 +0100

    Added french localization

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de traduction de "Authentification"
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes suivantes sont utilisées durant l'authentification
+    | pour afficher divers messages à l'utilisateur. Vous êtes libre de modifier
+    | ces lignes de langue suivant les besoins de votre application.
+    |
+    */
+
+    'failed' => 'Ces identifiants sont incorrects.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+
+];
