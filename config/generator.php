6cbc0b64905bf9979a0381ca10df0bb4e1778946
Lucas Michot <lucas@semalead.com>
2018-06-07 17:12:32 +0200

    Add a custom generator.php config file.

---
+++ config/generator.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Generator paths
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the namespaces that will be used when generating
+    | classes using artisan make:* command.
+    | If no namespace is specified, a default will be used.
+    |
+    | Possible values are Channel, Command, Controller, Event, Exception,
+    |        Factory, Job, Listener, Mail, Middleware, Model, Notification,
+    |        Observer, Policy, Provider, Request, Resource, Rule & Test.
+    |
+    | Only Migration namespace cannot be overridden.
+    */
+
+    // 'Model' => 'App',
+    // 'Controller' => 'App\Http\Controllers',
+];
