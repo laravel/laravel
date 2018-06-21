69e92d6454af859ba34bec3374d94e2929281dc0
Taylor Otwell <taylor@laravel.com>
2018-06-21 11:08:49 -0500

    initial pass

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
