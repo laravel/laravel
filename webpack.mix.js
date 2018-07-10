765a0042a492c7e4ab47b1dee95911324458bac4
Guillaume Briday <guillaumebriday@gmail.com>
2018-07-10 09:50:24 +0200

    Versionning assets in production

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
