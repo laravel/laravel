7c987ad2ffbc3e9067d7f299a29f17b272734334
Thomas Nadin <thomas.nadin@engageinteractive.co.uk>
2018-08-21 18:30:35 +0100

    Adds trust all proxies to TrustProxies middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
