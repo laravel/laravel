4e6838a6cca79daea71b6f7daf2efb936f3e3985
Taylor Otwell <taylor@laravel.com>
2018-08-22 14:12:42 -0500

    add nova

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,6 +86,7 @@
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
+                    <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
