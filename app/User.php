ed3c8b2d64ec18f9c3a3276d18a1fe14018a1459
Taylor Otwell <taylor@laravel.com>
2018-09-06 08:11:07 -0500

    fix stub

--- app/User.php
+++ app/User.php
@@ -3,8 +3,8 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
 class User extends Authenticatable
 {
