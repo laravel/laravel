8c3e7603eb4377eb5a6cc791dd98866d7b5445e7
Laurence Ioannou <github@theshiftexchange.com>
2018-09-07 19:44:33 +1000

    Update VerificationController.php

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Routing\Controller;
+use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
