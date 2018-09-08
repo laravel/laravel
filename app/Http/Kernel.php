9838f79d2c07c6196afec0363dbabe369e95cc75
Taylor Otwell <taylor@laravel.com>
2018-09-08 11:06:07 -0500

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
     /**
      * The priority-sorted list of middleware.
      *
-     * Forces the listed middleware to always be in the given order.
+     * This forces the listed middleware to always be in the given order.
      *
      * @var array
      */
