a5fc02d832ed91ac3da6408c2548314b8116fb79
Taylor Otwell <taylor@laravel.com>
2018-09-14 10:38:02 -0500

    use class

--- config/cache.php
+++ config/cache.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -88,7 +90,7 @@ return [
 
     'prefix' => env(
         'CACHE_PREFIX',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_cache'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'
     ),
 
 ];
