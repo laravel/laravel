62c5bbf8205fbec61d8a77f6000a927bc58e945c
Gaurav Makhecha <gauravmakhecha@gmail.com>
2018-09-15 10:14:06 +0530

    Seeded users should be verified by default
    the email_verified_at column value is used to determine whether a user has verified her account.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
