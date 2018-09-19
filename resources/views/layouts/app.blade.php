1fb6002522dab6b0d198aabe11e79b074ee558d1
Simon Willans <simon@engageinteractive.co.uk>
2018-09-19 14:30:05 +0100

    Adds theme-color meta property

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="theme-color" content="#ff585d">
 
 	<title>{{ $page['title'] }}</title>
 	<meta name="description" content="{{ $page['description'] }}">
