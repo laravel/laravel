a15f3cfe307bbd1023b16b7d0ff0111480f6f18d
Sammy Kaye Powers <sammyk@sammykmedia.com>
2018-09-28 13:39:08 -0400

    Update config/cache.php slug generation to conform to config/session.php

--- config/cache.php
+++ config/cache.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
 
     /*
@@ -88,6 +86,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix' => env('CACHE_PREFIX', str_slug(env('APP_NAME', 'laravel'), '_').'_cache'),
 
 ];
