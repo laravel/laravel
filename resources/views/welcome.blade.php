e9211a4ed43503a9df771687ddffe46c8f2fc729
Arjay Q. Angeles <aqangeles@gmail.com>
2018-09-28 10:36:34 +0800

    Check if register route is enabled.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,9 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        @if (Request::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
                     @endauth
                 </div>
             @endif
