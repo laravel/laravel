15314aa1ca181f044e3a516338df6fa15c5a6355
Vishal Sancheti <mail@vishal.pro>
2018-10-04 18:30:56 +0530

    Security Precuation
    Blacklisting by default security information from debug to prevent security issues.

--- config/app.php
+++ config/app.php
@@ -224,4 +224,27 @@ return [
 
     ],
 
+    'debug_blacklist' => [
+        '_ENV' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+            'REDIS_PASSWORD',
+            'MAIL_USERNAME',
+            'MAIL_PASSWORD',
+            'PUSHER_APP_KEY',
+            'PUSHER_APP_SECRET'
+        ],
+        '_SERVER' => [
+            'APP_KEY',
+            'DB_PASSWORD',
+            'REDIS_PASSWORD',
+            'MAIL_USERNAME',
+            'MAIL_PASSWORD',
+            'PUSHER_APP_KEY',
+            'PUSHER_APP_SECRET'
+        ],
+        '_POST' => [
+            'password',
+        ],
+
 ];
