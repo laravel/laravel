08dba08228452d5d00578f24c89e3d8c7f88130f
Pedro Felipe de Azevedo Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2018-10-05 15:53:58 -0300

    Update validation.php

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -40,6 +40,7 @@ return [
     'distinct'             => 'The :attribute field has a duplicate value.',
     'email'                => 'The :attribute must be a valid email address.',
     'exists'               => 'The selected :attribute is invalid.',
+    'failed'               => 'The given data was invalid.',
     'file'                 => 'The :attribute must be a file.',
     'filled'               => 'The :attribute field must have a value.',
     'gt'                   => [
