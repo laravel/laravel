325ae7ac6971e6f570039c5d45e2b7679a835044
jakebathman <jake.bathman@gmail.com>
2018-10-05 13:54:15 -0500

    Update `RegisterController` password validation rule and associated lang file

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'password' => 'Passwords must be at least eight characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
