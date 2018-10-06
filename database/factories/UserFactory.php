e18d91f8eb2e83da15e73433c2814fed7349a390
Kushal <kushal.billaiya@gmail.com>
2018-10-06 21:46:42 +0530

    Update password
    change from secretpass to password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$wOY5GEMqZ2onAWGeNXlTd.DEgxnT.dIovWTvxfgMqvRCk1LRbjPIm', // secretpass
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => str_random(10),
     ];
 });
