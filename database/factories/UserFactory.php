354fece693c28bcc331d610e11fad6968efc423a
Kushal <kushal.billaiya@gmail.com>
2018-10-06 12:29:17 +0530

    Update UserFactory password
    The default password is 'secret' which is 6 characters. This is less than the new requirement of minimum 8 character password.
    
    The new default is set to 'secretpass'

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$wOY5GEMqZ2onAWGeNXlTd.DEgxnT.dIovWTvxfgMqvRCk1LRbjPIm', // secretpass
         'remember_token' => str_random(10),
     ];
 });
