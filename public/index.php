6844a3d677432519feb2691a37a32ff908a83673
Robert Doucette <rice8204@gmail.com>
2018-10-13 12:28:29 +0200

    Fixes a small grammar error in public/index.php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
+| into the script here so that we don't have to worry about manually
 | loading any of our classes later on. It feels great to relax.
 |
 */
