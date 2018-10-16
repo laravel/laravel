cb793c26ddf194893147fa4527d4fe72410af4d0
Matthieu Haméon <1923261+mhameon@users.noreply.github.com>
2018-10-16 22:53:03 +0200

    Use localized strings
    Use localized strings similar to register.blade.php & login.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,12 +68,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">{{ __('Home') }}</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">{{ __('Login') }}</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">{{ __('Register') }}</a>
                         @endif
                     @endauth
                 </div>
