3c89b6a72e89cb5a8f8e404f236b6329ea82df15
GabMic <ruinsain@gmail.com>
2018-10-16 10:36:28 +0300

    Update ResetPasswordController.php

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
