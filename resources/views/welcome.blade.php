59bdc8cd4e1b998cd61e06f99d0c09917bf367ea
Boris Damevin <borisdamevin@gmail.com>
2018-10-16 23:45:14 +0200

    Fix bad font size render on link
    With Nunito, the 12px size with uppercase hase bad render (FF & Chrome).

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 12px;
+                font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
