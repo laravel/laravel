c96995fb61632c6da0d7fc13cf6fd16a57f6c5ec
Clemir Rondón <clemir@gmail.com>
2018-10-28 11:04:31 -0400

    Add message for UUID validation rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -112,6 +112,7 @@ return [
     'unique' => 'The :attribute has already been taken.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute format is invalid.',
+    'uuid' => 'The :attribute must be a valid UUID.',
 
     /*
     |--------------------------------------------------------------------------
