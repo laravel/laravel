4525f36cacc15cb82e8df4ef2e61f24423fcc639
Ben Sampson <bbashy@users.noreply.github.com>
2018-10-30 12:36:35 +0000

    use env value for redis queue name
    It's common to have a redis queue name when having multiple queues/applications on the same instance. Default value is the same.

--- config/queue.php
+++ config/queue.php
@@ -60,7 +60,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'default',
+            'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
             'block_for' => null,
         ],
