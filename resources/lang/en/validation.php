cff8c52824494899b0c79aed9ca137f8869951c3
Laurence Ioannou <github@theshiftexchange.com>
2018-10-30 18:39:37 +1100

    Update validation.php

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -136,9 +136,9 @@ return [
     | Custom Validation Attributes
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
     |
     */
 
