cfcb3ad05a449d494c5dfdb52a5a3d94681077e2
Farris Benjamin <benfarris40@gmail.com>
2018-10-31 14:48:44 +0100

    update welcome.blade.php
    add the version of laravel.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
+ @php  $laravel = app()->version(); @endphp
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel - {{$laravel}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -81,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - {{$laravel}
                 </div>
 
                 <div class="links">
