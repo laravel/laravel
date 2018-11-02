05acbad5b60ae507e80db33479848db27643581c
Taylor Otwell <taylor@laravel.com>
2018-11-02 10:11:21 -0500

    formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,16 +10,24 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/CreateUser.vue -> <create-user></create-user>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+
+// files.keys().map(key => {
+//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
+// })
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-const files = require.context('./', true, /\.vue$/i)
-files.keys().map(key => {
-    const name = _.last(key.split('/')).split('.')[0]
-    return Vue.component(name, files(key))
-})
 
 const app = new Vue({
     el: '#app'
