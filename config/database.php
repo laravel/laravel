f5d546b370c98e18ab3509671c55fc3ea2791e5d
Nathan Giesbrecht <nathan.giesbrecht@gmail.com>
2018-11-07 13:44:45 -0600

    Grammar fix in comments

--- config/database.php
+++ config/database.php
@@ -99,7 +99,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
+    | provides a richer set of commands than typical key-value systems
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
