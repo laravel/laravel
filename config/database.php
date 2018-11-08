c09519f547ae7a97eb26433f159cd81b8753e666
Taylor Otwell <taylor@laravel.com>
2018-11-07 19:05:23 -0500

    formatting

--- config/database.php
+++ config/database.php
@@ -103,7 +103,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
+    | provides a richer body of commands than a typical key-value system
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
