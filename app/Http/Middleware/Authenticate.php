6e031b62cc5dede7f39eebb42a1a10f0240cd991
ankurk91 <pro.ankurk1@gmail.com>
2018-11-09 18:15:20 +0530

    [5.8] Pass guards to redirectTo method in Authenticate middleware
    see https://github.com/laravel/framework/pull/26457

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,9 +10,10 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
      * @return string
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request, $guards)
     {
         return route('login');
     }
