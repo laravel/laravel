c9046b229115f59580a1eda7676a102b7d21e1dd
Anders Jürisoo <jurisoo@hotmail.com>
2018-11-27 07:52:25 +0800

    Fixed mixed up comment order

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,10 +17,10 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
