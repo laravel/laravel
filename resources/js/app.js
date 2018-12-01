997c3c0c7d903e5925d839e0880cccbf55f35362
Francis Morissette <Francismori7@users.noreply.github.com>
2018-12-01 14:32:44 -0500

    Prepare app.js for Mix 4.0 (and future)
    Since Mix 4.0 is still beta but is based on Babel7, it will now need to behave like `import`, which requires us to mention `default` to know what is `require`d.
    
    Not sure if this is required right now, but it will be required once the switch is made. AFAIK, this is a non-breaking change for people who don't upgrade.

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ window.Vue = require('vue');
  */
 
 // const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
