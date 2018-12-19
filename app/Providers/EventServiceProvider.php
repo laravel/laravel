326cc4bbbd56f473e4c711974f3ffb9c3a8543e6
jradtilbrook <jrad.tilbrook@gmail.com>
2018-12-19 14:48:14 +0800

    Add PHPCS and default rules
    Also change existing classes with violations

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,16 +19,4 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
         ],
     ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-
-        //
-    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,18 +17,6 @@ class RouteServiceProvider extends ServiceProvider
     protected $namespace = 'App\Http\Controllers';
 
     /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-
-        parent::boot();
-    }
-
-    /**
      * Define the routes for the application.
      *
      * @return void
