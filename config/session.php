1be5e29753d3592d0305db17d0bffcf312ef5625
Taylor Otwell <taylor@laravel.com>
2019-01-10 15:18:58 -0600

    add dynamo to stubs

--- config/session.php
+++ config/session.php
@@ -14,7 +14,7 @@ return [
     | you may specify any of the other wonderful drivers provided here.
     |
     | Supported: "file", "cookie", "database", "apc",
-    |            "memcached", "redis", "array"
+    |            "memcached", "redis", "dynamodb", "array"
     |
     */
 
