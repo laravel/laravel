915667a8d5fa31e7d35b617f64c47ab67a64a171
Taylor Otwell <taylor@laravel.com>
2019-01-11 08:48:42 -0600

    formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,10 +85,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
