fd1831f6104d4079f75cf5e6df484cbfd31941f1
Rodolfo Ruiz <rrpadilla@gmail.com>
2019-01-15 10:36:27 +0000

    remove Middleware TrustProxies. Disable Middleware Fideloper Trusproxies by default

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        // \Fideloper\Proxy\TrustProxies::class,
     ];
 
     /**
