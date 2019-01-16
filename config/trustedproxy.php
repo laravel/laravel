40f9d741fb1896d3b4fb9fc18dd97850bd355feb
Rodolfo Ruiz <rrpadilla@gmail.com>
2019-01-16 07:43:41 +0000

    fix Configuration comment formatting

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -3,48 +3,45 @@
 return [
 
     /*
-     * Set trusted proxy IP addresses.
-     *
-     * Both IPv4 and IPv6 addresses are
-     * supported, along with CIDR notation.
-     *
-     * The "*" character is syntactic sugar
-     * within TrustedProxy to trust any proxy
-     * that connects directly to your server,
-     * a requirement when you cannot know the address
-     * of your proxy (e.g. if using ELB or similar).
-     *
-     */
-    'proxies' => env('TRUSTEDPROXY_PROXIES', null), // [<ip addresses>,], '*', '<ip addresses>,'
+    |--------------------------------------------------------------------------
+    | Set trusted proxy IP addresses.
+    |--------------------------------------------------------------------------
+    |
+    | Both IPv4 and IPv6 addresses are supported, along with CIDR notation.
+    | The "*" character is syntactic sugar within TrustedProxy to trust any
+    | proxy that connects directly to your server, a requirement when you
+    | cannot know the address of your proxy (e.g. if using ELB or similar).
+    | To trust one or more specific proxies that connect directly to your
+    | server, use an array or a string separated by comma of IP addresses.
+    | Or, to trust all proxies that connect directly to your server, use a "*".
+    |
+    | Supported: "*", [<ip addresses>,], "<ip addresses>,"
+    |
+    */
+
+    'proxies' => env('TRUSTEDPROXY_PROXIES', null),
 
     /*
-     * To trust one or more specific proxies that connect
-     * directly to your server, use an array or a string separated by comma of IP addresses:
-     */
-    // 'proxies' => ['192.168.1.1'],
-    // 'proxies' => '192.168.1.1, 192.168.1.2',
+    |--------------------------------------------------------------------------
+    | Set Headers.
+    |--------------------------------------------------------------------------
+    |
+    | Which headers to use to detect proxy related data (For, Host, Proto, Port).
+    | @link https://symfony.com/doc/current/deployment/proxies.html
+    |
+    | Options include:
+    | - Use all x-forwarded-* headers to establish trust:
+    |   - \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
+    |   - 'HEADER_X_FORWARDED_ALL'
+    | - Use the FORWARDED header to establish trust:
+    |   - \Illuminate\Http\Request::HEADER_FORWARDED
+    |   - 'HEADER_FORWARDED'
+    | - If you are using AWS Elastic Load Balancer:
+    |   - \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+    |   - 'HEADER_X_FORWARDED_AWS_ELB'
+    |
+    */
 
-    /*
-     * Or, to trust all proxies that connect
-     * directly to your server, use a "*"
-     */
-    // 'proxies' => '*',
-
-    /*
-     * Which headers to use to detect proxy related data (For, Host, Proto, Port)
-     *
-     * Options include:
-     *
-     * - Illuminate\Http\Request::HEADER_X_FORWARDED_ALL (use all x-forwarded-* headers to establish trust)
-     * - Illuminate\Http\Request::HEADER_FORWARDED (use the FORWARDED header to establish trust)
-     * - Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB (If you are using AWS Elastic Load Balancer)
-     *
-     * - 'HEADER_X_FORWARDED_ALL' (use all x-forwarded-* headers to establish trust)
-     * - 'HEADER_FORWARDED' (use the FORWARDED header to establish trust)
-     * - 'HEADER_X_FORWARDED_AWS_ELB' (If you are using AWS Elastic Load Balancer)
-     *
-     * @link https://symfony.com/doc/current/deployment/proxies.html
-     */
     'headers' => env('TRUSTEDPROXY_HEADERS', 'HEADER_X_FORWARDED_ALL'),
 
 ];
