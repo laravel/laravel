f62a531bf546f685746e8924eb7df7ddb0f966e6
Stanislav Shupilkin <sshupilkin@gotrg.com>
2019-01-17 15:59:38 +0300

    Remove facade Event from section use in EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
