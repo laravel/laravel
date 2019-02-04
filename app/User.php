2f37ba38fa8f7cf6a1cda070984459405e90a37a
Eddie Palmans <epalmans@gmail.com>
2019-02-04 14:28:05 +0100

    Attribute casting for 'email_verified_at' on User model stub

--- app/User.php
+++ app/User.php
@@ -29,11 +29,11 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be mutated to dates.
+     * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $dates = [
-        'email_verified_at',
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
