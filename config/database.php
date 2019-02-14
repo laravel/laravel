df4ecb9c8352252e1bc239cbfa32a3a05e194162
Taylor Otwell <taylor@laravel.com>
2019-02-14 11:03:41 -0600

    change default redis configuration structure

--- config/database.php
+++ config/database.php
@@ -115,18 +115,28 @@ return [
 
         'client' => 'predis',
 
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'predis'),
         ],
 
-        'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => env('REDIS_DB', 0),
+                ],
+            ],
+
+            'cache' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => env('REDIS_CACHE_DB', 1),
+                ],
+            ],
         ],
 
     ],
