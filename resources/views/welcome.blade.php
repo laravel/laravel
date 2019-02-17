ffd670e96266c51f259818aea0cb2f84c2abc254
russx123 <45538389+russx123@users.noreply.github.com>
2019-02-17 13:24:02 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravels
                 </div>
 
                 <div class="links">
