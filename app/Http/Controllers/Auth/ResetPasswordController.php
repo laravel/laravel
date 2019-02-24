e0ebc8c0d889ee7b62a6a60716ed5b63129f3656
tomhorvat <tomhorvat@gmail.com>
2019-02-24 10:32:00 +0100

    Update ResetPasswordController.php
    After #4904 guest middleware should have guard parameter to work as intended

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -34,6 +34,6 @@ class ResetPasswordController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('guest:web');
     }
 }
