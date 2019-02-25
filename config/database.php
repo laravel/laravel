64b16c28526188e5577b86205cad5a5c953274ab
Taylor Otwell <taylor@laravel.com>
2019-02-25 14:32:49 -0600

    revert to old redis config

--- config/database.php
+++ config/database.php
@@ -119,24 +119,18 @@ return [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
         ],
 
-        'clusters' => [
-            'default' => [
-                [
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', 6379),
-                    'database' => env('REDIS_DB', 0),
-                ],
-            ],
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
 
-            'cache' => [
-                [
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', 6379),
-                    'database' => env('REDIS_CACHE_DB', 1),
-                ],
-            ],
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
     ],
