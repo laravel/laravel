66eeb3bca39b491110ef2143336b3e75eae5f396
Andrew Gorpenko <andrew.gorpenko@gmail.com>
2019-02-26 15:51:24 +0200

    Fix unterminated statements
    This tiny patch adds missing commas to the app.js file.

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,8 +17,8 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
