5dcec9a220914643d68c75acc204d732e9792ca0
Samuel Levy <sam@samuellevy.com>
2019-02-27 15:06:50 +1000

    Updated `.env.example` with setting used in new `config` files

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,7 +59,7 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => env('AWS_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
