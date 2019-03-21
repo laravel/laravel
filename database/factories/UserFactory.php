ae3aa682597ca092b0a14f25e4e7be470c148cd7
Narciso Nunez <narciso.arias21@gmail.com>
2019-03-21 11:26:32 -0400

    Change remember_token string length

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'remember_token' => Str::random(60),
     ];
 });
