2935285358d146e0536da82959726fd47cbd4500
Thomas Nadin <thomas.nadin@engageinteractive.co.uk>
2019-03-25 13:36:21 +0000

    Updates packages

--- config/sentry.php
+++ config/sentry.php
@@ -4,11 +4,12 @@ return array(
     'dsn' => env('SENTRY_LARAVEL_DSN'),
 
     // capture release as git sha
-    // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
-    // Capture bindings on SQL queries
-    'breadcrumbs.sql_bindings' => true,
+    'breadcrumbs' => [
 
-    // Capture default user context
-    'user_context' => false,
+        // Capture bindings on SQL queries logged in breadcrumbs
+        'sql_bindings' => true,
+
+    ],
 );
