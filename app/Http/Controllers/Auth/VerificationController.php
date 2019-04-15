be4ecafd8aa89612fbfab6effe8667a90d1a541b
Anas <bouzid.anas.1@gmail.com>
2019-04-15 22:25:26 +0100

    [5.8] throttle the verification request first
    With the current order, the `throttle` middleware is never executed

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -35,7 +35,7 @@ class VerificationController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+        $this->middleware('signed')->only('verify');
     }
 }
