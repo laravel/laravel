9b8829950115f2d2a060635137e0892b97a16d7b
Jason Carlson <jason.carlson@anrev.com>
2019-04-17 15:14:10 -0600

    allow for schema setting in .env for pgsql
    Laravel defaulted to the 'public' schema on pgsql connections and by default did not allow overriding it in the .env file. If a person uses different schemas for different environments, not allowing this in the .env file (where other environment data is defined) it made it more complicated than it needed to be. This also will make it easier for installers that only place database settings in the .env file

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'public',
+            'schema' => env('DB_SCHEMA', 'public'),
             'sslmode' => 'prefer',
         ],
 
