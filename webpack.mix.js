0e6042fba0ad8fd35f92a768c523b7a028184b7e
Dries Vints <dries.vints@gmail.com>
2019-04-22 14:08:06 +0000

    Apply fixes from StyleCI

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,4 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js').sass('resources/assets/sass/app.scss', 'public/css');
