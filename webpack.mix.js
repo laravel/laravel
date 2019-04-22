f574aa76e3e1b8b1d77469e37d2857e27da14379
Dries Vints <dries.vints@gmail.com>
2019-04-22 21:26:44 +0200

    Disable JS checks for now

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,4 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
