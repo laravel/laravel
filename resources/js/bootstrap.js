62250b4f3e71ba09e011cb2a30196ac2173a4057
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-04-23 09:24:22 +0100

    Apply fixes from StyleCI (#5004)

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,9 +34,7 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error(
-        'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token'
-    );
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
