6c36dbc5520b8213166b69bef61b7e66d3610649
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-04-23 09:17:29 +0100

    Apply fixes from StyleCI (#5001)

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent.vue').default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js').sass(
+    'resources/sass/app.scss',
+    'public/css'
+);
