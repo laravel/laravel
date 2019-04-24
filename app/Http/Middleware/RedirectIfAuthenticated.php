604a1fad13109ff54ca7dd15555d2f2db065b136
Karel Faille <karel@septime.net>
2019-04-24 14:53:30 +0200

    Remove unused /home URL

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/');
         }
 
         return $next($request);
