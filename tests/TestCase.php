1672212883c093f5a950cdfca0026c34bfe2f66c
Roshandelpoor Mohammad <roshandelpoor@outlook.com>
2019-05-04 11:24:52 +0430

    Add Method setUp and tearDown
    In most scenario we will be using the setUp() method more than the tearDown() method.
    This helps to run the test faster.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,29 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    /**
+     * that is called before every test.
+     * to initialise variables, open file connection etc
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        
+        // code
+    }
+    
+    /**
+     * that is called after every test.
+     * to unset variables, close file connection etc
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+        
+        // code
+    }
+    
 }
