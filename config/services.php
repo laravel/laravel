fe9f8497d98ec0fa6a7d43a2ae4127285b777065
Dries Vints <dries.vints@gmail.com>
2019-05-07 17:57:29 +0200

    Remove services deleted from core
    See https://github.com/laravel/framework/pull/28441 and https://github.com/laravel/framework/pull/28442

--- config/services.php
+++ config/services.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | as Stripe, Mailgun, Postmark and others. This file provides a sane
     | default location for this type of information, allowing packages
     | to have a conventional place to find your various credentials.
     |
@@ -28,10 +28,6 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
     ],
 
     'stripe' => [
