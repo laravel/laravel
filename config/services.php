c9bb74c7e01cffbb5e185990acecf3fcab7f5eda
Taylor Otwell <taylor@laravel.com>
2019-05-08 08:06:25 -0500

    formatting

--- config/services.php
+++ config/services.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, Postmark and others. This file provides a sane
+    | as Stripe, Mailgun, Postmark plus others. This file provides a sane
     | default location for this type of information, allowing packages
     | to have a conventional place to find your various credentials.
     |
