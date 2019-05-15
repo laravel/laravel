2718e1220fe93e71f4abe5c2ae03733f47defcb2
Dario Govergun <dpgover@gmail.com>
2019-05-15 11:53:17 -0300

    Digbang's way (Part 6): - sentry config update

--- config/sentry.php
+++ config/sentry.php
@@ -4,13 +4,9 @@ return [
     'enabled' => env('SENTRY_ENABLED', false),
 
     'dsn' => env('SENTRY_DSN'),
-    'dsn_public' => env('SENTRY_DSN_PUBLIC'),
 
     // capture release as git sha
-    'release' => @trim(@file_get_contents('.release')),
-
-    // Capture bindings on SQL queries
-    'breadcrumbs.sql_bindings' => true,
+    //'release' => @trim(@file_get_contents('.release')),
 
     // Capture default user context
     'user_context' => true,
