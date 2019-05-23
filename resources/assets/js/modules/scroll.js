071f01abb900fdab7074441cef780f4ccef1be1f
Simon Willans <simon@engageinteractive.co.uk>
2019-05-23 09:58:40 +0100

    Adds Tornis to replace older event listeners

--- resources/assets/js/modules/scroll.js
+++ resources/assets/js/modules/scroll.js
@@ -1,35 +1,21 @@
+import {
+	watchViewport,
+} from 'tornis';
+
 let x = 0,
-	y = 0;
+    y = 0;
 
-const listeners = {},
-	update = () => Object.keys(listeners).forEach(name => listeners[name](x, y)),
-	onScroll = () => {
-		const left = document.documentElement.scrollLeft;
-		const top = document.documentElement.scrollTop;
+const updateValues = ({
+    scroll,
+}) => {
+    if (scroll.changed) {
+		x = scroll.left;
+		y = scroll.top;
+    }
+};
 
-		x = left === 0 ? document.body.scrollLeft : left;
-		y = top === 0 ? document.body.scrollTop : top;
-
-		if (typeof window.requestAnimationFrame === 'function') {
-			window.requestAnimationFrame(update);
-		} else {
-			update();
-		}
-
-	};
-
-window.addEventListener('scroll', onScroll, { passive: true });
-
-onScroll();
+watchViewport(updateValues);
 
 export const getX = () => x;
 
 export const getY = () => y;
-
-export const addListener = (name, listener) => {
-	listeners[name] = listener;
-};
-
-export const removeListener = (name) => {
-	delete listeners[name];
-};
