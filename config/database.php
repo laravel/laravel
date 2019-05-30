bf60f7f74f9578902650c30887190f86515a1037
Antoni Siek <parrotcraft@gmail.com>
2019-05-30 18:22:45 +0200

    Added support for new redis URL property in config/database.php (#5037)
    Regarding laravel/framework#28612

--- config/database.php
+++ config/database.php
@@ -127,6 +127,7 @@ return [
         ],
 
         'default' => [
+            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
@@ -134,6 +135,7 @@ return [
         ],
 
         'cache' => [
+            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
