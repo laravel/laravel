803d296634837d4b75a29b71b38aff6a121bdb8a
Franck DAKIA <dakiafranck@gmail.com>
2019-06-02 18:12:08 +0000

    Update filesystems
    Change the env variable name

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
     |--------------------------------------------------------------------------
