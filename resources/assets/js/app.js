1c86dfdd5d49d16d9b40d6ead0237fc5a2404a35
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2019-06-14 16:01:23 +0100

    Update app js order

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,28 +4,33 @@ import Vue from 'vue';
 import svg4everybody from 'svg4everybody';
 
 import './bootstrap';
+import lang from './i18n';
 
-// components
 import ForgotPasswordForm from './components/accounts/forgot-password-form';
 import LoginForm from './components/accounts/login-form';
 import PasswordResetForm from './components/accounts/password-reset-form';
 import RegisterForm from './components/accounts/register-form';
 import ResendVerifyCodeForm from './components/accounts/resend-verify-code-form';
 
-// filters
-import lang from './i18n';
-
 Vue.filter('trans', (...args) => lang.get(...args));
+
+// Global
+// Vue.component('ExampleGlobalComponent', ExampleGlobalComponent);
 
 new Vue({
 	el: '#app',
 
+	// Local
 	components: {
+		// App
 		ForgotPasswordForm,
 		LoginForm,
 		PasswordResetForm,
 		RegisterForm,
 		ResendVerifyCodeForm,
+
+		// Styleguide
+		// ExampleStyleguideOnlyComponent,
 	},
 
 	mounted() {
