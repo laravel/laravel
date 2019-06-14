898ecf853d9dcee40d9a6191a6eb510fc2718b49
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2019-06-14 15:12:22 +0100

    Split out app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,25 @@
-<!doctype html>
-<html>
-<head>
-	@include('layouts/partials/meta', [
-		'stylesheet' => '/compiled/css/app.css'
-	])
+@extends('layouts/base')
+
+@section('head')
+	@parent
 
 	@include('layouts/partials/gtm', [
-		'placement' => 'head'
+		'placement' => 'head',
 	])
-</head>
-<body>
+@endsection
+
+@section('app:before')
 	@include('layouts/partials/gtm', [
-		'placement' => 'body'
+		'placement' => 'body',
 	])
 
 	@include('layouts/partials/outdated-browser')
+@endsection
 
-	<div id="app" class="site-wrapper">
-		@include('layouts/partials/site-header')
+@section('app')
+	@include('layouts/partials/site-header')
 
-		@yield('content')
+	@yield('content')
 
-		@include('layouts/partials/site-footer')
-	</div>
-
-	<script src="https://polyfill.io/v3/polyfill.min.js?features=Array.from%2CPromise%2CIntersectionObserver%2CElement.prototype.matches%2CElement.prototype.classList%2CArray.prototype.find"></script>
-	<script src="{{ mix('/compiled/js/app.js') }}" async></script>
-</body>
-</html>
+	@include('layouts/partials/site-footer')
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	@section('head')
+		@include('layouts/partials/meta', [
+			'stylesheet' => '/compiled/css/app.css'
+		])
+	@show
+</head>
+<body>
+	@yield('app:before')
+
+	<div id="app" class="site-wrapper">
+		@yield('app')
+	</div>
+
+	@section('app:after')
+		<script src="https://polyfill.io/v3/polyfill.min.js?features=Array.from%2CPromise%2CIntersectionObserver%2CElement.prototype.matches%2CElement.prototype.classList%2CArray.prototype.find"></script>
+		<script src="{{ mix('/compiled/js/app.js') }}" async></script>
+	@show
+</body>
+</html>
