717711ac7b4619de99f49e6c29d5ad36330dd978
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2019-06-17 12:38:13 +0100

    Fix styleguide section overlap

--- resources/views/layouts/styleguide.blade.php
+++ resources/views/layouts/styleguide.blade.php
@@ -93,9 +93,9 @@
 					</div>
 
 					@if ($section['blocks'])
-						<div class="relative mt-2">
+						<div class="relative mt-2 pointer-events-none">
 							<nav class="sticky left-0 top-0 w-full -mt-12 pt-12 z-10">
-								<div class="bg-white">
+								<div class="bg-white pointer-events-auto">
 									<div class="container mx-auto px-4">
 										<div class="flex items-center h-12 border-b">
 											<ul class="flex -ml-4 text-accent font-semibold">
@@ -113,38 +113,40 @@
 							<div class="container mx-auto px-4">
 								@foreach ($section['blocks'] as $blockSlug => $block)
 									<section class="-mt-24 pt-24" id="{{ $blockSlug }}">
-										<h3 class="mt-10 text-2xl text-gray-900 font-semibold">
-											<a href="#{{ $blockSlug }}">{{ $block['heading'] }}</a>
-										</h3>
+										<div class="mt-10 pointer-events-auto">
+											<h3 class="text-2xl text-gray-900 font-semibold">
+												<a href="#{{ $blockSlug }}">{{ $block['heading'] }}</a>
+											</h3>
 
-										@if ($block['copy'] ?? false)
-											<p v-if="$data.toggle.copy" class="max-w-3xl mt-4 text-lg">{{ $block['copy'] }}</p>
-										@endif
+											@if ($block['copy'] ?? false)
+												<p v-if="$data.toggle.copy" class="max-w-3xl mt-4 text-lg">{{ $block['copy'] }}</p>
+											@endif
 
-										@foreach ($block['previews'] as $previewSlug => $preview)
-											<block
-												class="block -mt-24 pt-24"
-												block="{{ $blockSlug }}"
-												id="{{ "$blockSlug-$previewSlug" }}"
-												preview="{{ $previewSlug }}"
-												section="{{ $slug }}"
-												:attributes='@json($preview['attributes'])'
-												:background-color='@json($preview['bg'] ?? null)'
-												:component='@json($preview['component'] ?? null)'
-											>
-												@if ($preview['heading'] ?? false)
-													<h4 class="mt-5 text-lg text-gray-900 font-semibold">
-														<a href="{{ "#$blockSlug-$previewSlug" }}">{{ $preview['heading'] }}</a>
-													</h4>
+											@foreach ($block['previews'] as $previewSlug => $preview)
+												<block
+													class="block -mt-24 pt-24"
+													block="{{ $blockSlug }}"
+													id="{{ "$blockSlug-$previewSlug" }}"
+													preview="{{ $previewSlug }}"
+													section="{{ $slug }}"
+													:attributes='@json($preview['attributes'])'
+													:background-color='@json($preview['bg'] ?? null)'
+													:component='@json($preview['component'] ?? null)'
+												>
+													@if ($preview['heading'] ?? false)
+														<h4 class="mt-5 text-lg text-gray-900 font-semibold">
+															<a href="{{ "#$blockSlug-$previewSlug" }}">{{ $preview['heading'] }}</a>
+														</h4>
 
-													@if ($preview['copy'] ?? false)
-														<p v-if="$data.toggle.copy" class="max-w-3xl mt-4">{{ $preview['copy'] }}</p>
+														@if ($preview['copy'] ?? false)
+															<p v-if="$data.toggle.copy" class="max-w-3xl mt-4">{{ $preview['copy'] }}</p>
+														@endif
 													@endif
-												@endif
 
-												<div v-if="false" class="mt-4 my-px" style="height: 200px;"></div>
-											</block>
-										@endforeach
+													<div v-if="false" class="mt-4 my-px" style="height: 200px;"></div>
+												</block>
+											@endforeach
+										</div>
 									</section>
 								@endforeach
 							</div>
