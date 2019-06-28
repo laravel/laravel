fc39b073f3f61a22f1b48329e294ebb881700dbe
Taylor Otwell <taylor@laravel.com>
2019-06-27 19:57:39 -0500

    remove ui scaffolding

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,19 +1,6 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
-} catch (e) {}
-
-/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
