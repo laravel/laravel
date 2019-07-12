7c9e5ea41293b63e051bd69f7929f8712893eea1
Gergő D. Nagy <dngege@gmail.com>
2019-07-12 15:59:55 +0200

    [5.9] Add ThrottleRequests to the priority array (#5057)
    * add ThrottleRequests to the priority array
    
    * Move ThrottleRequests under Authenticate middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,6 +73,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\Authenticate::class,
+        \Illuminate\Routing\Middleware\ThrottleRequests::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
