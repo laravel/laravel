34bf9d2fa81c4ec343f4bf3d642ccf6a919fac27
华晨 <chanjsq@gmail.com>
2019-07-16 11:02:04 +0800

    Fix cache, redis and session prefixes for non-English `APP_NAME`

--- config/cache.php
+++ config/cache.php
@@ -98,6 +98,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_CODE', env('APP_NAME', 'laravel')), '_').'_cache'),
 
 ];
--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix' => Str::slug(env('APP_CODE', env('APP_NAME', 'laravel')), '_').'_database_',
         ],
 
         'default' => [
