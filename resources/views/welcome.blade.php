af934eca93095a6fb01e259007631971c6f9ae5e
chuckrincon <chuckrincon@gmail.com>
2019-08-02 11:19:36 -0500

    [6.0] - Add vapor link on the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,6 +91,7 @@
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
