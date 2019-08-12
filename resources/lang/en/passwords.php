228d0a8b76b508df5a4c08ccdf8001fd95f90c3a
Dries Vints <dries.vints@gmail.com>
2019-08-12 11:57:50 +0200

    Remove deprecated language line

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,7 +13,6 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
