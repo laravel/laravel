bb433725483803a27f21d3b21317072610bc3e9c
Taylor Otwell <taylor@laravel.com>
2019-08-13 15:05:56 -0500

    formatting

--- config/services.php
+++ config/services.php
@@ -8,9 +8,9 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
+    | as Mailgun, SparkPost and others. This file provides a sane default
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
     |
     */
 
