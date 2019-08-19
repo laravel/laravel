ae02cb7df0fcc3eb0a400e6d6b7b6617c943bc9a
24Kdabaiyang <haoliang@outlook.jp>
2019-08-19 10:58:37 +0800

    Set the priority of Csrf middleware
    When the session expires, now prompted is 419 exception(csrf), should be performed the authentication first.
    Do you have any idea.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,6 +73,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\Authenticate::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
         \Illuminate\Routing\Middleware\ThrottleRequests::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
