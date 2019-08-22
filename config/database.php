4c554a32a8088dcbdcf3c75880df5acfc9f8194e
Taylor Otwell <taylor@laravel.com>
2019-08-22 08:22:02 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -122,7 +122,7 @@ return [
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'phpredis'),
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
