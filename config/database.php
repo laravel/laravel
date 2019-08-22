665dfc4328daeabaa496ac6a0743476348657585
Dries Vints <dries.vints@gmail.com>
2019-08-22 15:22:14 +0200

    [6.0] Use phpredis as default Redis client (#5085)
    * Use phpredis as default Redis client
    
    Follow up for https://github.com/laravel/framework/pull/29688
    
    It's best that we already start using `phpredis` as a default to discourage usage of Predis.
    
    * Update database.php

--- config/database.php
+++ config/database.php
@@ -119,10 +119,10 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
+        'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'predis'),
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
