01b677b0c2216aa8f7363724ca734a7f4741528f
Amir hossein Hossein Zadeh Karimi <AmirHosseinKarimi@users.noreply.github.com>
2019-08-29 21:12:30 +0430

    Change email max length to 320 characters
    The email address has a limitation of 320 characters.
    
    References:
    https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
    https://en.wikipedia.org/wiki/Email_address

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:320', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
