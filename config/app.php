da02aab604070d7bbcdc362216192569ca4c6c93
Jonathan Reinink <jonathan@reinink.ca>
2019-09-16 12:03:49 -0400

    Rename Redis alias to RedisManager

--- config/app.php
+++ config/app.php
@@ -214,7 +214,7 @@ return [
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
+        'RedisManager' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
