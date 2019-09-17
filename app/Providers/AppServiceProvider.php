cf66cc6357acc977b43740d76815660b0382b04a
vali.kh <vali.khodaparastt@gmail.com>
2019-09-18 01:37:14 +0430

    Data Wraping disable comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+//use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //Resource::withoutWrapping();
         //
     }
 }
