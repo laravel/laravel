2bb4345ae68b1b51414165aaff3d77a12d5367a8
mfilipoff <mfilipoff@digbang.com>
2019-10-02 11:13:42 -0300

    Frontend refactor to Laravel 6.0 default version.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
+/**
+ * First, we will load all of this project's Javascript utilities and other
+ * dependencies. Then, we will be ready to develop a robust and powerful
+ * application frontend using useful Laravel and JavaScript libraries.
+ */
+
 require('./bootstrap');
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,12 +11,26 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+// import Echo from 'laravel-echo'
 
 // window.Pusher = require('pusher-js');
 
