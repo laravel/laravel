940d4f42ef118f7483eb591b2db622491ae5895f
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-10-03 11:06:11 -0300

    Remove unsed

--- app/User.php
+++ app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
