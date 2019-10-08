3001962e602904d7a562a0afcf07a26455b03d58
Simon Willans <simon@engageinteractive.co.uk>
2019-10-08 11:28:47 +0100

    Updates /frontend template list to use tailwind

--- resources/views/app/frontend.blade.php
+++ resources/views/app/frontend.blade.php
@@ -1,13 +1,22 @@
-@extends('layouts/app')
+@extends('layouts/base')
 
-@section('content')
+@section('head')
+	@include('layouts/partials/meta', [
+		'stylesheet' => 'https://cdn.jsdelivr.net/npm/tailwindcss@1.0.4/dist/tailwind.min.css',
+		'mix_stylesheet' => false,
+	])
+@endsection
 
-	<h1>Templates</h1>
+@section('app')
+	<div class="container mx-auto py-12 px-5">
+		<h1 class="text-4xl xl:text-5xl tracking-tighter text-gray-900 font-bold">Templates</h1>
 
-	<ul>
-		@foreach ($templates as $template)
-			<li><a href="/{{ $template }}">{{ $template }}</a></li>
-		@endforeach
-	</ul>
-
+		<div class="mt-3">
+			<ul class="-mt-2">
+				@foreach ($templates as $template)
+					<li class="mt-2"><a href="/{{ $template }}">{{ $template }}</a></li>
+				@endforeach
+			</ul>
+		</div>
+	</div>
 @endsection
