c71d2c0f989215c9b5ecfcc0b3f652194b3d971d
Lloric Mayuga Garcia <lloricode@gmail.com>
2019-10-11 07:55:59 +0800

    Fix for reset unique in seeder unique columns
    https://github.com/fzaninotto/Faker/issues/1512#issuecomment-450109983

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $faker->unique(true)->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
