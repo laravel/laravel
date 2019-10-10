caf497589618fd9a7c00a0905baf9db199713a40
Simon Willans <simon@engageinteractive.co.uk>
2019-10-10 15:49:13 +0100

    Removes references to old input component

--- resources/assets/js/mixins/form.js
+++ resources/assets/js/mixins/form.js
@@ -1,4 +1,3 @@
-import EInput from '../components/common/input.vue';
 import ErrorText from '../components/common/error-text.vue';
 
 export default {
@@ -64,7 +63,6 @@ export default {
 	},
 
 	components: {
-		EInput,
 		ErrorText,
 	},
 };
