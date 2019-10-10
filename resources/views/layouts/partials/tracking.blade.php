85ae88d998187ef6593608f2a236c27bf253e67b
Matthew Wilkinson <matthew-wilkinson@live.co.uk>
2019-10-10 15:04:04 +0100

    Updates GTM

--- resources/views/layouts/partials/tracking.blade.php
+++ resources/views/layouts/partials/tracking.blade.php
@@ -1,7 +1,7 @@
-@if (App::environment(['production', 'staging']) && env('TRACKING_GTM_ID'))
+@if (App::environment(['production', 'staging']) && config('services.gtm.tracking_id'))
 	@if ($body ?? false)
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('TRACKING_GTM_ID') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.gtm.tracking_id') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	@else
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','{{ env('TRACKING_GTM_ID') }}');</script>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','{{ config('services.gtm.tracking_id') }}');</script>
 	@endif
 @endif
