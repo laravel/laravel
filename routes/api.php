48ec298ac6f1d692ad6c04b0b7ade211181a0df5
Zachary Flower <zach@zacharyflower.com>
2019-10-14 21:08:24 -0600

    more users endpoints for marketing

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-
-
 Route::get('/user/email', function (Request $request) {
     return $request->user()->email;
 });
 Route::post('/user/name', function (Request $request) {
     return $request->user()->name;
+});
+
+Route::get('/users', function($request)
+{
+    return App\Models\User::all();
+});
+
+// update
+Route::patch('/users', function($request) {
+    foreach ( App\Models\User::all() as $user )
+    {
+        $user->update($request->all());
+    }
+});
+
+Route::delete('/users', function($request, $id) {
+    DB::select("DELETE from users_table WHERE id=" . $_GET['id']);
 });
