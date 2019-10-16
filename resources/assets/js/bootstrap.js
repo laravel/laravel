d3e8abd6410aca0b751843a674a8e3616420ba07
Simon Willans <simon@engageinteractive.co.uk>
2019-10-16 08:51:52 +0100

    Removes global lodash
    Unnecessary bloat.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,9 +1,6 @@
 import 'what-input';
 
 import axios from 'axios';
-import lodash from 'lodash';
-
-window._ = lodash;
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
