e611e57a8e58e218b11af4f1ab37a79463594378
Daniel Bruno Gallinari <dgallinari@users.noreply.github.com>
2019-10-31 08:17:03 -0300

    Fix part 1 - MySQL too long key error
    When using MariaDB or MySQL below v. 5.7.7, there is an inconvenient error stating:
    [Illuminate\Database\QueryException]
    SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes (SQL: alter table users add unique users_email_unique(email))
    
    There are many great solutions for this issue, being one of them well-explained in:
    https://laravel-news.com/laravel-5-4-key-too-long-error
    
    However, I believe the best solution would be not having to deal with an error at all. Furthermore, you won't affect any other schema setting an undesired default length to them.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
