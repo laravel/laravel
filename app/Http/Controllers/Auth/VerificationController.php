80f626d388034803b1fb5d9c08c886a8455a09ea
Zachary Flower <zach@zacharyflower.com>
2019-11-05 10:42:12 -0700

    Update VerificationController.php

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -14,7 +14,7 @@ class VerificationController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-            $this->middleware('signed')->only('verify');
+              $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
