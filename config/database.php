7a6a4a179a9ea0f9af8af6c42beb9b58394526aa
Saman Zahedi <isamanzahedi@gmail.com>
2019-11-07 11:29:21 +0330

    Added sqlite:memory connection to use in phpunit

--- config/database.php
+++ config/database.php
@@ -43,6 +43,12 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'sqlite:memory' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
