4cba8ada90b360cdc04709d6fe854bd1659b1173
Jordan Walker <jordan@jayy.online>
2019-11-10 14:05:50 +0000

    Formatting

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -25,7 +27,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
