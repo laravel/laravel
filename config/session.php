5357863bef3b5c6d121db6f5aafc288b10b77640
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-11-23 01:00:02 +0000

    Use the null secure option for session cookies

--- config/session.php
+++ config/session.php
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
