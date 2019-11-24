8aaaf6af3da27deaf90fa2410dad440c038f4083
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-24 20:07:00 +0100

    Rename `encrypted` to `forceTLS`.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,5 +24,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
+//     forceTLS: true
 // });
