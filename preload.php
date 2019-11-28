a7bd10cc941d5c07894b22f6102b60e3f13d8d81
Panda <1017109588@qq.com>
2019-11-28 18:05:20 +0800

    [6.x] Laravel  Support  php7.4  preload
    [6.x] Laravel  Support  php7.4  preload

---
+++ preload.php
@@ -0,0 +1,119 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+class Preloader
+{
+    private array $ignores = [];
+
+    private static int $count = 0;
+
+    private array $paths;
+
+    private array $fileMap;
+
+    public function __construct(string ...$paths)
+    {
+        $this->paths = $paths;
+        $classMap = require __DIR__ . '/vendor/composer/autoload_classmap.php';
+        $this->fileMap = array_flip($classMap);
+    }
+
+    public function paths(string ...$paths): Preloader
+    {
+        $this->paths = array_merge(
+            $this->paths,
+            $paths
+        );
+
+        return $this;
+    }
+
+    public function ignore(string ...$names): Preloader
+    {
+        $this->ignores = array_merge(
+            $this->ignores,
+            $names
+        );
+
+        return $this;
+    }
+
+    public function load(): void
+    {
+        foreach ($this->paths as $path) {
+            $this->loadPath(rtrim($path, '/'));
+        }
+
+        $count = self::$count;
+
+        echo "[Preloader] Preloaded {$count} classes" . PHP_EOL;
+    }
+
+    private function loadPath(string $path): void
+    {
+        if (is_dir($path)) {
+            $this->loadDir($path);
+
+            return;
+        }
+
+        $this->loadFile($path);
+    }
+
+    private function loadDir(string $path): void
+    {
+        $handle = opendir($path);
+
+        while ($file = readdir($handle)) {
+            if (in_array($file, ['.', '..'])) {
+                continue;
+            }
+
+            $this->loadPath("{$path}/{$file}");
+        }
+
+        closedir($handle);
+    }
+
+    private function loadFile(string $path): void
+    {
+        $class = $this->fileMap[$path] ?? null;
+
+        if ($this->shouldIgnore($class)) {
+            return;
+        }
+
+        require_once($path);
+
+        self::$count++;
+
+        echo "[Preloader] Preloaded `{$class}`" . PHP_EOL;
+    }
+
+    private function shouldIgnore(?string $name): bool
+    {
+        if ($name === null) {
+            return true;
+        }
+
+        foreach ($this->ignores as $ignore) {
+            if (strpos($name, $ignore) === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+(new Preloader())
+    ->paths(__DIR__ . '/vendor/laravel')
+    ->ignore(
+        \Illuminate\Filesystem\Cache::class,
+        \Illuminate\Log\LogManager::class,
+        \Illuminate\Http\Testing\File::class,
+        \Illuminate\Http\UploadedFile::class,
+        \Illuminate\Support\Carbon::class,
+    )
+    ->load();
