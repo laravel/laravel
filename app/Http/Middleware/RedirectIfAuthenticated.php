5459b9f8930773f1600b6c41ab55a7d9f448288a
Debangshu Roy <42003389+iunteq@users.noreply.github.com>
2019-12-01 08:25:26 +0530

    Update RedirectIfAuthenticated.php in /app/Http/Middleware/
    This change is helpful for MULTI-GUARD-AUTHENTICATION. A dynamic approach!
     
    As it is SWITCH-CASE and default redirect link is defined so, anyone use predefined default single guard then nothing affect and if someone want to use MULTI-GUARD-AUTHENTICATION then it will be easy as well as simple to use; just need to replace their won guard name.
    
    ##If you want to use NAMED ROUTE  (https://laravel.com/docs/5.8/routing#named-routes) then you need to create route with proper name in this [routes/web.php] file.
    
    Thank you.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,35 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        switch ($guard) {
+            case 'admin':
+                $redirectTo = '/admin-home-page';
+                //OR can use route name
+                //$routeName = 'admin.home';
+                break;
+                
+            case 'guardName1':
+                $redirectTo = '/guardName1-home-page';
+                //OR can use route name
+                //$routeName = 'guardName1.home';
+                break;
+
+            case 'guardName2':
+                $redirectTo = '/guardName2-home-page';
+                //OR can use route name
+                //$routeName = 'guardName2.home';
+                break;    
+            
+            default:
+                $redirectTo = '/home';
+                //OR can use route name
+                //$routeName = 'home';
+                break;
+        }
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect($redirectTo);
+            // IF WE USE ROUTE NAME THEN
+            //return redirect(route($routeName));
         }
 
         return $next($request);
