b0851faed5efd22096f040ce36a3044f8520e87f
byjml <byjml@users.noreply.github.com>
2019-12-04 02:08:41 +0300

    Consistent order
    Keep the alphabetical order of the validation messages.

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -15,8 +15,8 @@ return [
 
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
-    'throttled' => 'Please wait before retrying.',
 
 ];
