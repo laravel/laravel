b235e1ec117d1948ffda111ccf43b4dfc87b66d7
Sergej <me@sergiz.com>
2019-12-04 12:17:07 +0100

    Do not check, if the request expects Json response

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return route('login');
     }
 }
