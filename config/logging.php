f48e2d500cb53cc4a09dfcb40beb0abafd79de4f
Taylor Otwell <taylorotwell@gmail.com>
2019-12-13 22:42:46 -0600

    change some default settings

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
