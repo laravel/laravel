4c7afd3c25ff6c2ede70c4e0cb23102066abf89f
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-12-18 22:57:13 +0000

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Http\Middleware\Authenticate;
-use Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
