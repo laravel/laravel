07ae4559d3a9a68c38c46954d751798f8f088228
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-12-18 22:58:09 +0000

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
