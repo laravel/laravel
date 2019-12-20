12c28822f402a948dac389bf037c550524845ba3
Taylor Otwell <taylorotwell@gmail.com>
2019-12-20 14:14:01 -0600

    one liner

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
