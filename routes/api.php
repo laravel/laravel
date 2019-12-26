e4683c6ecaff9a7dc779fb0dfe39f6336832fc9d
Barry vd. Heuvel <barryvdh@gmail.com>
2019-12-26 19:48:38 +0100

    Revert routes

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::any('/test', function (Request $request) {
-    $a++;
-    return $request->user();
-});
 
-
-Route::middleware('auth:api')->any('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
