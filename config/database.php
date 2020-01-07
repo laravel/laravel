42774e8baad7b9b17b15ee75f5dbef46bbb7abe8
kawax <kawaxbiz@gmail.com>
2020-01-07 17:38:27 +0900

    Comment out sqlite.url

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
+            //'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
