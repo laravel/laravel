91f12f4c715dfe5480c6acd254c4bbdd043e8736
Andrew Brown <browner12@gmail.com>
2020-01-28 23:48:18 -0600

    update Slack default level
    switch from minimum 'critical' level to 'debug'

--- config/logging.php
+++ config/logging.php
@@ -59,7 +59,7 @@ return [
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => 'critical',
+            'level' => 'debug',
         ],
 
         'papertrail' => [
