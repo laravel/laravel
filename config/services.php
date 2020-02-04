3b9cceb7b89c071305266d1c3d113257dd06017a
Salim Djerbouh <caddydz4@gmail.com>
2020-02-04 12:23:35 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -8,17 +8,11 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | as Postmark, AWS and more. This file provides the de facto
     | location for this type of information, allowing packages to have
     | a conventional file to locate the various service credentials.
     |
     */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-    ],
 
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
