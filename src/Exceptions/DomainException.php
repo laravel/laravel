f5f342d517bb041c274d96c4cb8b32b02a4e9273
Dario Govergun <dpgover@gmail.com>
2020-02-11 14:31:03 -0300

    Styling

--- src/Exceptions/DomainException.php
+++ src/Exceptions/DomainException.php
@@ -4,15 +4,16 @@ namespace ProjectName\Exceptions;
 
 use Throwable;
 
-abstract class DomainException extends \DomainException
+abstract class DomainException extends \RuntimeException
 {
     /** @var string */
     private $key;
 
     public function __construct(string $key, string $message = '', int $code = 0, Throwable $previous = null)
     {
-        $this->key = $key;
         parent::__construct($message, $code, $previous);
+
+        $this->key = $key;
     }
 
     public function getKey(): string
