050ceab1346384fa7261c4349d2351055ad6ec4c
Dries Vints <dries.vints@gmail.com>
2020-02-27 15:34:13 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -207,6 +207,7 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Http' => Illuminate\Support\Facades\Http::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
