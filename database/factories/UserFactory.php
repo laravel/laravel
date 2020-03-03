04c07eee423deecaacde3d9ad0c09da76bfae5f1
Steve McDougall <juststeveking@gmail.com>
2020-03-03 10:50:02 +0000

    Update UserFactory to use Hash facade

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,9 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
-use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('password'),
         'remember_token' => Str::random(10),
     ];
 });
