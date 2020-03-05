399586ebd77ee0210dd75f1f6bac2bc315405507
Honoré Hounwanou <mercuryseries@gmail.com>
2020-03-04 23:13:46 -0500

    Make use of fluent String
    Why not :) ?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ Str::of(app()->getLocale())->replace('_', '-') }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
