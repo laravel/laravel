29d02357dbf9ab094adc51ca321b69e4a4b40cb2
Eric Dowell <github@ericdowell.com>
2020-03-08 18:16:37 -0500

    Compare instead of cast app.debug

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', false) === true,
 
     /*
     |--------------------------------------------------------------------------
