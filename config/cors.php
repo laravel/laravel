efdd4dfd7d6e16786b0b4d6bc549d7ef89a0592b
Barry vd. Heuvel <barryvdh@gmail.com>
2020-03-11 17:10:10 +0100

    Update default CORS config

--- config/cors.php
+++ config/cors.php
@@ -25,9 +25,9 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => false,
+    'exposed_headers' => [],
 
-    'max_age' => false,
+    'max_age' => 0,
 
     'supports_credentials' => false,
 
