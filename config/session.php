f93f4ad8bf114116a7c6917e7da155b5656463af
Mathieu TUDISCO <oss@mathieutu.dev>
2020-03-13 20:53:56 +0100

    Fix session config changes (#5261)
    * Fix session config changes
    
    * Update session.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- config/session.php
+++ config/session.php
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', null),
+    'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
@@ -190,7 +190,7 @@ return [
     | take place, and can be used to mitigate CSRF attacks. By default, we
     | do not enable this as other CSRF protection services are in place.
     |
-    | Supported: "lax", "strict", "none"
+    | Supported: "lax", "strict", "none", null
     |
     */
 
