048ad18c6582b29d5af0dde5e40f80bd65035ed0
rodruiz <rodruiz@users.noreply.github.com>
2020-03-14 16:24:40 -0700

    Update RouteServiceProvider.php
    Class constant visibility can be used in PHP 7.1 only.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    const HOME = '/home';
 
     /**
      * Define your route model bindings, pattern filters, etc.
