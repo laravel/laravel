a164dab0be9b7aa82bbb861c8ed5c77d6160c83f
Taylor Otwell <taylor@laravel.com>
2020-03-15 11:25:43 -0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null, // In seconds
+            'timeout' => null,
         ],
 
         'ses' => [
