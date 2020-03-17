84d6976597dd5affc6a8e028d882d005905a42eb
Riadh KHEDHIRI <riadh.khedhiri@gmail.com>
2020-03-17 12:25:51 +0100

    Handle JSON requests correctly in RedirectIfAuthenticated middleware
    The redirect didn't satisfy the CORS policy as 'Access-Control-Allow-Origin' was missing.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            return $request->wantsJson()
+                ? new Response('', 204)
+                : redirect(RouteServiceProvider::HOME);
         }
 
         return $next($request);
