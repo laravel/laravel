427693f857f56807a9758f837c122747431a5fcd
JDdigital <62897769+JDdigital@users.noreply.github.com>
2020-04-01 14:42:48 +0100

    RouteServiceProvider::home to ENV Variable
    This variable should be as customisable as the "APP_NAME" and "APP_URL". Not sure whether this should also be added to config/app.php too thinking about it?

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = env('HOME_PATH', '/home');
 
     /**
      * Define your route model bindings, pattern filters, etc.
