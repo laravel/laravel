e8eb2fc2a65b53e42cc92159490d8dc21d3d37cb
Propaganistas <Propaganistas@users.noreply.github.com>
2020-04-06 18:11:00 +0200

    Update startup.sh

--- .azure/startup.sh
+++ .azure/startup.sh
@@ -3,4 +3,4 @@ apt-get update -qq && apt-get install cron -yqq
 service cron start
 
 (crontab -l 2>/dev/null; echo "* * * * * /home/site/wwwroot/.azure/test.sh")|crontab
-(crontab -l 2>/dev/null; echo "* * * * * /home/site/wwwroot/.azure/test2.sh")|crontab
+(crontab -l 2>/dev/null; echo "* * * * * cd /home/site/wwwroot && php artisan schedule:run")|crontab
