a6b32ace11e2edbf40f868311e5b243af8cf1bff
Propaganistas <Propaganistas@users.noreply.github.com>
2020-04-06 15:55:54 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
