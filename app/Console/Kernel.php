60a4fbce70179dc693f3522a88f59512d1aad662
Propaganistas <Propaganistas@users.noreply.github.com>
2020-04-06 20:17:36 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {        
         $schedule->call(function () {
-            info(date('Y-m-D_H-i-s'));
+            info('test! ' .date('Y-m-D_H-i-s'));
         })->everyMinute();
     }
 
