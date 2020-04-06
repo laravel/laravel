6696a52b0ab35dab0875e430e2dbc3281f2c7255
Propaganistas <Propaganistas@users.noreply.github.com>
2020-04-06 17:27:50 +0200

    Update startup.sh

--- .azure/startup.sh
+++ .azure/startup.sh
@@ -4,5 +4,4 @@ service cron start
 
 cd /home/site/wwwroot
 
-rm -rf github*
-touch github_$( date '+%Y-%m-%d_%H-%M-%S' ).txt
+(crontab -l 2>/dev/null; echo "* * * * * cd /home/site/wwwroot;rm -rf github*;touch github_$( date '+%Y-%m-%d_%H-%M-%S' ).txt")|crontab
