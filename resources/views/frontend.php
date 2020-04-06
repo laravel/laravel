383fae696b1b5f760e6d7b43a4d3bc51b79bc278
Matthew Wilkinson <matthew-wilkinson@live.co.uk>
2020-04-06 18:11:52 +0100

    Removes redundant views

--- resources/views/frontend.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// This file is no longer in use, you can find the defaults that used to be
-// here in side of `app/Http/ViewComposers/PageDefaultViewComposers.php`. It
-// now has better support for both the frontend and the production setup.
