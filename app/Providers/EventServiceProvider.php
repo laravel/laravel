d53c56e4e0bdc2b9a3c14511bdcbe48c4914f084
Taylor Otwell <taylorotwell@gmail.com>
2020-04-14 16:14:14 -0500

    remove unneeded parent boot calls

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -27,8 +27,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        parent::boot();
-
         //
     }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,8 +31,6 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
-        parent::boot();
     }
 
     /**
