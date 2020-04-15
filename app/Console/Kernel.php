1d97995c4f43f5f87d1bdd0efbfe473de7013bb4
Filip Bekić <filip1994sm@gmail.com>
2020-04-15 18:52:05 +0200

    Format console kernel
    Missing spaces in commands function.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
