0e01834bedb0a360608e4eec6c71418454004ce6
Taylor Otwell <taylorotwell@gmail.com>
2020-04-17 10:36:39 -0500

    add sponsor link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,8 +104,22 @@
                     </div>
                 </div>
 
-                <div class="mt-4 text-center text-sm text-gray-500 sm:text-right">
-                    Build v{{ Illuminate\Foundation\Application::VERSION }}
+                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                    <div class="text-center text-sm text-gray-500 sm:text-left">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="-mt-px w-5 h-5 text-gray-400">
+                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                            </svg>
+
+                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
+                                Sponsor
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Build v{{ Illuminate\Foundation\Application::VERSION }}
+                    </div>
                 </div>
             </div>
         </div>
