b9af2b229455efd68c806ff0f8f3d7e1411c9bbf
Taylor Otwell <taylorotwell@gmail.com>
2020-04-21 10:12:39 -0500

    Use new factories
    Use the new factories provided in 8.x

--- app/User.php
+++ app/User.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
