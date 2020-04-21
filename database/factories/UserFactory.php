5f6f5c929b25ab3ba83fccf3a7b01c4bd13d60f4
Taylor Otwell <taylorotwell@gmail.com>
2020-04-21 12:41:27 -0500

    add model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,19 @@
 
 namespace Database\Factories;
 
+use App\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
