3e8e76a48a4c5e133a287b73ce58abd7f3d71b18
Chuck Rincón <chuckrincon@gmail.com>
2020-04-22 15:56:11 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline">Documentation</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" target="_blank" class="underline">Documentation</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -66,7 +66,7 @@
                         <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline">Laracasts</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" target="_blank" class="underline">Laracasts</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -79,7 +79,7 @@
                         <div class="p-6 border-t border-gray-200">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline">Laravel News</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" target="_blank" class="underline">Laravel News</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -97,7 +97,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 text-sm">
-                                    Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
+                                    Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" target="_blank" class="underline">Forge</a>, <a href="https://vapor.laravel.com" target="_blank" class="underline">Vapor</a>, <a href="https://nova.laravel.com" target="_blank" class="underline">Nova</a>, and <a href="https://envoyer.io" target="_blank" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" target="_blank" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" target="_blank" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" target="_blank" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" target="_blank" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" target="_blank" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" target="_blank" class="underline">Telescope</a>, and more.
                                 </div>
                             </div>
                         </div>
@@ -111,7 +111,7 @@
                                 <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                             </svg>
 
-                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
+                            <a href="https://github.com/sponsors/taylorotwell" target="_blank" class="ml-1 underline">
                                 Sponsor
                             </a>
                         </div>
