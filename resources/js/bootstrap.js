2e7df9029b79ef6c5fcbce8495e0f106d1dd53c3
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-05-02 08:13:37 +1000

    Remove forceTLS option

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,5 +24,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
 // });
