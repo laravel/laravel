7d62f500a789416738a7181d5217d33ca096db04
feek <5747667+mr-feek@users.noreply.github.com>
2020-05-04 06:31:39 -0700

    chore: update typehint to be nullable (#5296)
    By default, this property is null. Therefore, it should be marked as nullable

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,7 +10,7 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string
+     * @var array|string|null
      */
     protected $proxies;
 
