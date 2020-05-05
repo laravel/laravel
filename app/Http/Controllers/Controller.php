acd0c4dd43db244c50bceed369eef67c094a2250
lappq <64824132+lappq@users.noreply.github.com>
2020-05-05 13:06:44 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+        use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
