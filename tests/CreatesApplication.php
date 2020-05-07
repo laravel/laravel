e2756def9f9616234086c25f6bb73c369297c59e
Igor Finagin <Igor@Finag.in>
2020-05-07 18:39:10 +0300

    Autogenerate key for tests
    nunomaduro/collision:4.1.3  and later no longer generates `app.key`

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Artisan;
 
 trait CreatesApplication
 {
@@ -16,6 +17,8 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+        
+        Artisan::call('key:generate');
 
         return $app;
     }
