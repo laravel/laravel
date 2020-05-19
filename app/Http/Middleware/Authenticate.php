d7e3aad1a66ef9c7a430b0e8f3d5c3b33c1ecd02
Mark Walet <mark.walet@gmail.com>
2020-05-19 14:43:30 +0200

    Fix inconsistent return points

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,7 +10,7 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @return string|void
      */
     protected function redirectTo($request)
     {
