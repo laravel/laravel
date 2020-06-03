6aa12f1b06fd06f9ece32f191329f4260ac31e88
Taylor Otwell <taylorotwell@gmail.com>
2020-06-03 14:51:22 -0500

    use route service provider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url(App\Providers\RouteServiceProvider::HOME) }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
