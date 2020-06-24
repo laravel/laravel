c58b40b9bc8288cda1f551dbe0a4d6547b37b2c9
Taylor Otwell <taylorotwell@gmail.com>
2020-06-24 13:12:04 -0500

    Remove extra line

--- public/index.php
+++ public/index.php
@@ -25,7 +25,6 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-
 /*
 |--------------------------------------------------------------------------
 | Run The Application
