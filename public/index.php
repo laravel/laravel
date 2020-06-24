6ca2ecacea0f6f0ff8a4c2f0d39c1ddda4c17f3e
Taylor Otwell <taylorotwell@gmail.com>
2020-06-24 13:13:32 -0500

    remove unneeded block

--- public/index.php
+++ public/index.php
@@ -3,13 +3,6 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
 define('LARAVEL_START', microtime(true));
 
 /*
