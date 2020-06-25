0ec89e9df4e2765ab74712d01abd86e044dd9c35
marianofilipoff <mfilipoff@digbang.com>
2020-06-25 12:19:48 -0300

    Integración con Laravel Mix (#14)

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,11 @@
+window._ = require('lodash')
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios')
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
