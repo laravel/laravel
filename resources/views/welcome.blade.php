db0a0cf57601a1e88c33192faa63188ebe0bfc99
masal9pse <returnymgstokh@icloud.com>
2020-06-26 02:56:46 +0900

    test pullreq

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,105 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+ <title>Laravel</title>
 
-            .full-height {
-                height: 100vh;
-            }
+ <!-- Fonts -->
+ <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+ <!-- Styles -->
+ <style>
+  html,
+  body {
+   background-color: #fff;
+   color: #636b6f;
+   font-family: 'Nunito', sans-serif;
+   font-weight: 200;
+   height: 100vh;
+   margin: 0;
+  }
 
-            .position-ref {
-                position: relative;
-            }
+  .full-height {
+   height: 100vh;
+  }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  .flex-center {
+   align-items: center;
+   display: flex;
+   justify-content: center;
+  }
 
-            .content {
-                text-align: center;
-            }
+  .position-ref {
+   position: relative;
+  }
 
-            .title {
-                font-size: 84px;
-            }
+  .top-right {
+   position: absolute;
+   right: 10px;
+   top: 18px;
+  }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+  .content {
+   text-align: center;
+  }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+  .title {
+   font-size: 84px;
+  }
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+  .links>a {
+   color: #636b6f;
+   padding: 0 25px;
+   font-size: 13px;
+   font-weight: 600;
+   letter-spacing: .1rem;
+   text-decoration: none;
+   text-transform: uppercase;
+  }
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+  .m-b-md {
+   margin-bottom: 30px;
+  }
+ </style>
+</head>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<body>
+ <div class="flex-center position-ref full-height">
+  @if (Route::has('login'))
+  <div class="top-right links">
+   @auth
+   <a href="{{ url('/home') }}">Home</a>
+   @else
+   <a href="{{ route('login') }}">Login</a>
+
+   @if (Route::has('register'))
+   <a href="{{ route('register') }}">Register</a>
+   @endif
+   @endauth
+  </div>
+  @endif
+
+  <div class="content">
+   <div class="title m-b-md">
+    Laravel
+   </div>
+
+   <div class="links">
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laracasts.com">Laracasts</a>
+    <a href="https://laravel-news.com">News</a>
+    <a href="https://blog.laravel.com">Blog</a>
+    <a href="https://nova.laravel.com">Nova</a>
+    <a href="https://forge.laravel.com">Forge</a>
+    <a href="https://vapor.laravel.com">Vapor</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+   </div>
+  </div>
+ </div>
+</body>
+
 </html>
