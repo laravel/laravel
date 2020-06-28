e64c496b875063aa0b0f377a003acbfd45fe9e8b
Ernesto Noe Isordia Cuevas <eisordia@itexico.com>
2020-06-27 20:12:16 -0500

    Fix migration error.
    Length of characters on user need be increased to let migrate database.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(255);
     }
 }
