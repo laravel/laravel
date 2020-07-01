9c9995f02fcef694104eff4e102094e55e5a920a
bluesoul <atlassian@nags.me>
2020-07-01 16:31:31 -0600

    Remove unused and undocumented asset_url key.

--- config/app.php
+++ config/app.php
@@ -54,8 +54,6 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
-
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
