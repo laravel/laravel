6f1f40bc940fac0fcfe66420bee4962c4e6a98c3
Taylor Otwell <taylorotwell@gmail.com>
2020-07-14 14:39:13 -0500

    update link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,7 +108,7 @@
                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                             </svg>
 
-                            <a href="https://gumroad.com/laravel" class="ml-1 underline">
+                            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
                                 Shop
                             </a>
 
