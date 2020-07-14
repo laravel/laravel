ce3e006d61299bdd3bd2b99ec5489577e55a296a
Taylor Otwell <taylorotwell@gmail.com>
2020-07-14 14:07:25 -0500

    remove unneeded property. can use ignore method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,15 +7,6 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
