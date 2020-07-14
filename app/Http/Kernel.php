a14a1208adc2e94e85d1afb0c36c5250a5668b4d
Taylor Otwell <taylorotwell@gmail.com>
2020-07-14 15:48:22 -0500

    remove alias that isnt needed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,7 +55,6 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
