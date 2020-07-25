336f268c09f19ddf6ebf8004f3e3fa76f222ef76
Samson Endale <4sam21@gmail.com>
2020-07-25 23:00:40 +0300

    Customizable welcome page through RouteServiceProvider
    According to [laravel docs](https://laravel.com/docs/7.x/authentication#included-authenticating),this path should be customizable by configuring the RouteServiceProvider::HOME constant, right?
    
    The developer can change that by him self, but the docs set an expectation.
    I just edited it on GitHub. Feel free to shot it down ¯\_(ツ)_/¯

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url(App\Providers\RouteServiceProvider::HOME) }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
