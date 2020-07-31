a2249d2795bfa9873eabe68264ea11cce04ea344
Nguyễn Nam Long <j2teamnnl@gmail.com>
2020-07-31 14:57:12 +0700

    Update User.php

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
