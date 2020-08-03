9877edf41fd7515a065824efe82241de9c64d891
Nuno Maduro <enunomaduro@gmail.com>
2020-08-03 09:12:32 +0100

    Apply fixes from StyleCI

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,7 +5,7 @@
 | Bind Traits And Classes
 |--------------------------------------------------------------------------
 |
-| The `uses` function lets you recursively bind traits and classes to 
+| The `uses` function lets you recursively bind traits and classes to
 | your test files. By default, test files are already bound to the
 | PHPUnit test case class. Of course, feel free to modify this.
 |
