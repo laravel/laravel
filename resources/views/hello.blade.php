d89ff899be53a8e1d66ef58b3c960c11240534d3
amana1221 <62594393+amana1221@users.noreply.github.com>
2020-08-06 07:08:03 +0900

    Create hello.blade.php

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <h2>Hello World from blade!!</h2>
+  <p>{{date("Y/m/d H:i:s")}}</p>
+</body>
+</html>
