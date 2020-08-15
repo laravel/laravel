f702a7a842c4bb1176b82b184f1dbe2a20276645
Bernard <gicehajunior76@gmail.com>
2020-08-16 02:38:17 +0300

    added token expiration at and updated at

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -16,7 +16,9 @@ class CreatePasswordResetsTable extends Migration
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
+            $table->timestamp('token_expires_at')->nullable();
             $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
     }
 
