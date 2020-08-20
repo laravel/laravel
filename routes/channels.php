fd8a872b3b3c7e56886b781514fb5f27914627cf
Taylor Otwell <taylorotwell@gmail.com>
2020-08-20 15:32:30 -0500

    update channel

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
