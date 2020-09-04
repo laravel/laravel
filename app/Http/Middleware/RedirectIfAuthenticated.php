0cd0ca290683271cf1d741330c4ffe722363c355
Bram Ceulemans <bramceulemans@me.com>
2020-09-04 17:35:43 +0200

    Remove usage of non-strict empty() function
    https://github.com/phpstan/phpstan-strict-rules/blob/master/src/Rules/DisallowedConstructs/DisallowedEmptyRule.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,13 +12,13 @@ class RedirectIfAuthenticated
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  Closure  $next
+     * @param  string[] $guards
      * @return mixed
      */
     public function handle($request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        $guards = count($guards) === 0 ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
