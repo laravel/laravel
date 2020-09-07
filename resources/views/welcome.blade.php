d59a9206e604c35fda1cb82b6b89a1cf60c13b1f
Youri Wijnands <youri@wijnands.me>
2020-09-07 15:50:31 +0200

    Use the new Google Fonts API
    https://developers.google.com/fonts/docs/css2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
