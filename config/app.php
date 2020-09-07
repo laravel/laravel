cae05b5fb13361c559bea739b2e140c51429fd3e
hpawe01 <friedrich.pawelka@googlemail.com>
2020-09-07 10:08:15 +0200

    Add information about valid timezone values

--- config/app.php
+++ config/app.php
@@ -64,6 +64,7 @@ return [
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
+    | See https://www.php.net/manual/timezones.php for valid timezones.
     |
     */
 
