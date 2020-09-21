f1a51f7c622783d98bf3916fc56c86755feb5426
Alex Mayer <amayer5125@gmail.com>
2020-09-20 21:35:48 -0400

    [8.x] Update User Model to Match Jetstream Formatting (#5422)
    * Update User Model to Match Jetstream Formatting
    
    * Update User.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -26,7 +28,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
