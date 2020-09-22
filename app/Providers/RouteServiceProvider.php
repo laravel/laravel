67a373ebe155d165279a315f70dfe1c977bbba35
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-09-22 11:15:51 -0300

    Fix route when uncomment $namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,9 +38,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
