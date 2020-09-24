a60381478d7b7774d8ea817688f8ce377393e97d
IggsGrey <41928390+IggsGrey@users.noreply.github.com>
2020-09-24 13:11:40 +0100

    Final fix for "Target class [Controller] does not exist."

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
