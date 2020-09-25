48935a3945fae0a61afc84e23bef56a7c0237ba5
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-09-25 15:28:47 +0200

    Add absent validation rule translation

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -13,6 +13,7 @@ return [
     |
     */
 
+    'absent' => 'The :attribute must be absent.',
     'accepted' => 'The :attribute must be accepted.',
     'active_url' => 'The :attribute is not a valid URL.',
     'after' => 'The :attribute must be a date after :date.',
