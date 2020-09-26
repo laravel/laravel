18312aa73f109404e646c9505b92477c026c18e5
gaodeng <gaodengming@gmail.com>
2020-09-26 11:17:36 +0800

    Do not Redirect if client wants json

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
+            if (Auth::guard($guard)->check() && !$request->wantsJson()) {
                 return redirect(RouteServiceProvider::HOME);
             }
         }
