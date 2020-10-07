8442409fe439b15ae8e24c2c80a307499fd8ba51
Raihannajib <najibraihan3@gmail.com>
2020-10-07 08:08:31 -0700

    myfirst-contribution

--- app/User.php
+++ app/User.php
@@ -15,18 +15,14 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password',];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token',];
 
     /**
      * The attributes that should be cast to native types.
