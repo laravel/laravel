5396be6ef26aebe99c1c5ac6ec944c349d13f371
Taylor Otwell <taylorotwell@gmail.com>
2020-10-27 15:15:00 -0500

    update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,7 @@
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Build v{{ Illuminate\Foundation\Application::VERSION }}
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
