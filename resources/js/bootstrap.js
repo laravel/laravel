521303af4b65ad955ec8549cb4c13b4dbc422039
Hristo <hdimitrov@talkative.uk>
2020-11-09 08:11:06 +0000

    Remove lodash

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,3 @@
-window._ = require('lodash');
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
