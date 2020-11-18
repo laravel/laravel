391e998d602e6de52c0ef38f232a624f87661e35
Simon Willans <simon@engageinteractive.co.uk>
2020-11-18 13:45:22 +0000

    Removes account and unused common components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,46 +10,23 @@ import lang from './i18n';
 
 // Common
 import EButton from './components/common/Button';
-import ETable from './components/common/Table';
-import Breadcrumb from './components/common/Breadcrumb';
 import Icon from './components/common/Icon';
 import IconText from './components/common/IconText';
-import Pagination from './components/common/Pagination';
 import Placeholder from './components/common/Placeholder';
-
-// Accounts
-// import ForgotPasswordForm from './components/accounts/ForgotPasswordForm';
-// import LoginForm from './components/accounts/LoginForm';
-// import PasswordResetForm from './components/accounts/PasswordResetForm';
-// import RegisterForm from './components/accounts/RegisterForm';
-// import ResendVerifyCodeForm from './components/accounts/ResendVerifyCodeForm';
 
 Vue.filter('trans', (...args) => lang.get(...args));
 
 // Global
 Vue.component('EButton', EButton);
-Vue.component('ETable', ETable);
-Vue.component('Breadcrumb', Breadcrumb);
 Vue.component('Icon', Icon);
 Vue.component('IconText', IconText);
-Vue.component('Pagination', Pagination);
 Vue.component('Placeholder', Placeholder);
 
 new Vue({
 	el: '#app',
 
 	// Local
-	components: {
-		// App
-		// ForgotPasswordForm,
-		// LoginForm,
-		// PasswordResetForm,
-		// RegisterForm,
-		// ResendVerifyCodeForm,
-
-		// Styleguide
-		// ExampleStyleguideOnlyComponent,
-	},
+	components: {},
 
 	mounted() {
 		svg4everybody();
