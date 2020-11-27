a94362ba2e89281b22857d9ebad1f01ffa23b8be
hyper-vlados <hypervlados@gmail.com>
2020-11-27 13:09:41 +0200

    Requires

--- routes/channels.php
+++ routes/channels.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
 });
