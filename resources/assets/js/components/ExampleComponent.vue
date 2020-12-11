d19b68cce03e8e39c624923a457b008b194f603c
zhaiduting <zhaiduting@163.com>
2020-12-11 19:18:10 +0800

    The first example of using TypeScript with Vue

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -3,21 +3,31 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Example of using Vue.extend(...)</div>
 
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
+                    <div class="panel-body" v-html="description"></div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+<script lang="ts">
+    export default Vue.extend({
+        data() {
+            let description = [
+                'This is a Vue component written in TypeScript.',
+                'To let TypeScript properly infer types inside Vue component options, ',
+                'you need to define components with <code>Vue.component(...)</code> or <code>Vue.extend(...)</code>',
+                '',
+                '<i><a href="https://vuejs.org/v2/guide/typescript.html">Click Detail</a> for more information</i>'
+            ];
+            return {
+                description
+            }
+        },
+        mounted(): void {
+            this.description = _.join(this.description, '<br>');        // Test global lodash
         }
-    }
+    })
 </script>
