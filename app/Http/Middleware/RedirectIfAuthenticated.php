14578a96ed4c65e7d80155a41a43c12a88d91a19
Musa <40173603+voyula@users.noreply.github.com>
2020-12-20 06:27:40 +0300

    [8.x] Fix redirection on session regenerates

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect()->intended(RouteServiceProvider::HOME);
             }
         }
 
