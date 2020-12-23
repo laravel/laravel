68fdee9de577cf57f3d9bae24f7152f507cd50ce
Thomas Lété <bistory@gmail.com>
2020-12-23 14:10:37 +0100

    Void can't be used in a union type

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,12 +10,14 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|void
+     * @return string|null
      */
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
+        
+        return null;
     }
 }
