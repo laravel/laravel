611e0898c7c86a8184320111fe77b97993526d20
middlegrey <studio@middlegrey.com>
2021-02-08 21:32:32 +0100

    Update 2019_08_19_000000_create_failed_jobs_table.php

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -15,7 +15,7 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
+            $table->uuid('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
