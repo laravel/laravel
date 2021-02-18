63b3a447f602de855c31bad6c4646af5d3dd7464
Isaac Mayne <5263829-IsaacMP@users.noreply.gitlab.com>
2021-02-18 16:39:20 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,20 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
+const tailwindcss = require('tailwindcss')
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('tailwind.config.js') ],
+    });
+
+if(mix.inProduction()){
+    mix.version()
+}
