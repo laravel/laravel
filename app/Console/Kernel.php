0cbf7cbb2a33c691d1cb2beca9086c597375e3bb
Sam Raskul <73840675+samraskul@users.noreply.github.com>
2021-02-18 11:31:43 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,9 +20,8 @@ class Kernel extends ConsoleKernel
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule):void
     {
         // $schedule->command('inspire')->hourly();
     }
@@ -30,9 +29,8 @@ class Kernel extends ConsoleKernel
     /**
      * Register the commands for the application.
      *
-     * @return void
      */
-    protected function commands()
+    protected function commands():void
     {
         $this->load(__DIR__.'/Commands');
 
