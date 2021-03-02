f66ecc47ea1cd0e5c86b50ae49d1b57aaa44f25b
Karel Faille <shaffe.fr@gmail.com>
2021-03-02 19:44:30 +0100

    Use same default queue name for all drivers

--- config/queue.php
+++ config/queue.php
@@ -54,7 +54,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
-            'queue' => env('SQS_QUEUE', 'your-queue-name'),
+            'queue' => env('SQS_QUEUE', 'default'),
             'suffix' => env('SQS_SUFFIX'),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
         ],
