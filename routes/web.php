ef1a93c88f4e7b840052d980c4cc642408c88bdd
Mouad ZIANI <mouad.ziani1997@gmail.com>
2021-03-11 11:42:48 +0100

    used short view helper to render welcome view

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
