6a92fb464590ab8bbd717cccad8521856cd5fbb3
Philo Hermans <me@philohermans.com>
2021-03-19 15:30:19 +0100

    Add prohibited validation rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -104,6 +104,7 @@ return [
     'required_with_all' => 'The :attribute field is required when :values are present.',
     'required_without' => 'The :attribute field is required when :values is not present.',
     'required_without_all' => 'The :attribute field is required when none of :values are present.',
+    'prohibited' => 'The :attribute field is prohibited.',
     'prohibited_if' => 'The :attribute field is prohibited when :other is :value.',
     'prohibited_unless' => 'The :attribute field is prohibited unless :other is in :values.',
     'same' => 'The :attribute and :other must match.',
