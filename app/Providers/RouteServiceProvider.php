1e1c024b832f0fc25e7a330c313920fc848979ae
Nguyen You <tunt081295@gmail.com>
2021-03-24 15:42:30 +0700

    Remove unnecessary visibility declaration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    const HOME = '/home';
 
     /**
      * The controller namespace for the application.
