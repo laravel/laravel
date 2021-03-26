76f6c2edf8f0f544fcf8ea487a7b99b8bae9536a
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2021-03-26 13:34:27 -0700

    Change the way of committing In bootstrap.js file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,13 +16,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+/*
+    import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+    window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: process.env.MIX_PUSHER_APP_KEY,
+        cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+        forceTLS: true
+    });
+*/
