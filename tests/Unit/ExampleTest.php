5808129a1f702f973c7c31203d16db2066bd9030
Dries Vints <dries@vints.io>
2021-03-26 15:47:26 +0100

    Rename test methods (#5574)

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_example()
     {
         $this->assertTrue(true);
     }
