8a88fec95fac539513041174ef9d81a270432dd4
ThunderbirdsX3 <THUNDER_BIRDS_No.XIII@live.com>
2021-03-31 11:48:43 +0700

    Add verify option to smtp
    Add verify option to smtp for suggestion develop can change it false

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'verify' => true,
         ],
 
         'ses' => [
