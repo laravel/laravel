e73553c6bf7ede5f4ad294907f04177d546a1355
Hassan Tariq <devflix@outlook.com>
2021-04-03 11:38:43 +0500

    Fix a minor typo

--- config/app.php
+++ config/app.php
@@ -113,7 +113,7 @@ return [
     | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | This key is used by the Illuminate encrypter service and should be set
+    | This key is used by the Illuminate encryptor service and should be set
     | to a random, 32 character string, otherwise these encrypted strings
     | will not be safe. Please do this before deploying an application!
     |
