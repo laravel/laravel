d7e73e664682e5d1aec2f6d867933e8472a636c1
MostafaRabia <mostafarabia64@gmail.com>
2021-04-06 12:56:56 +0200

    Update validation.php
    The boolean validation don't accept true or false from front, but accept 1 or 0.
    For this confuse for the front, I made this pull request.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -29,7 +29,7 @@ return [
         'string' => 'The :attribute must be between :min and :max characters.',
         'array' => 'The :attribute must have between :min and :max items.',
     ],
-    'boolean' => 'The :attribute field must be true or false.',
+    'boolean' => 'The :attribute field must be 1 or 0.',
     'confirmed' => 'The :attribute confirmation does not match.',
     'date' => 'The :attribute is not a valid date.',
     'date_equals' => 'The :attribute must be a date equal to :date.',
