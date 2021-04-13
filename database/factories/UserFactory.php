0e8e9a0727afce37d6954a9763a86dde82eb99ee
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2021-04-13 07:28:01 -0500

    [8.x] Using faker method instead of properties (#5583)
    After Faker PHP 1.14 using properties is deprecated and is recommended to use methods instead.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,8 +23,8 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
