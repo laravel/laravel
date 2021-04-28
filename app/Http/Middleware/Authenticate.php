e9ade190689e502ef40bdbed1e9b1b94754df6f7
Fatih Aytekin <fatih@aytekin.me>
2021-04-28 15:21:45 +0300

    Update PHPDoc

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,7 +10,8 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @return string|void
+     * @noinspection PhpReturnDocTypeMismatchInspection
      */
     protected function redirectTo($request)
     {
