4ce641d5713738d58032fa3fb407b4c37115ebde
Dries Vints <dries@vints.io>
2021-05-07 12:54:25 +0200

    Apply fixes from StyleCI (#5607)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
