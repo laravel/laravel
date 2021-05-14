637c85d624bf19355025b68aaa90e6cadf8a2881
Taylor Otwell <taylorotwell@gmail.com>
2021-05-14 10:09:46 -0500

    wip

--- config/cache.php
+++ config/cache.php
@@ -27,7 +27,7 @@ return [
     | same cache driver to group types of items stored in your caches.
     |
     | Supported drivers: "apc", "array", "database", "file",
-    |            "memcached", "redis", "dynamodb", "null"
+    |         "memcached", "redis", "dynamodb", "octane", "null"
     |
     */
 
@@ -35,10 +35,6 @@ return [
 
         'apc' => [
             'driver' => 'apc',
-        ],
-
-        'octane' => [
-            'driver' => 'octane',
         ],
 
         'array' => [
@@ -90,6 +86,10 @@ return [
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
             'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
             'endpoint' => env('DYNAMODB_ENDPOINT'),
+        ],
+
+        'octane' => [
+            'driver' => 'octane',
         ],
 
     ],
