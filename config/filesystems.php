c5d38d469a447d6831c3cf56d193be7941d6586f
Taylor Otwell <taylorotwell@gmail.com>
2021-05-18 17:01:26 -0500

    update skeleton for filesystem tweaks to make sail usage easier

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,6 +50,7 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
     ],
