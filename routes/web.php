14a28bc555a2559c164ddf9279e6de2cab016734
zhouzhishen <947917490@qq.com>
2021-05-27 11:50:23 +0800

    路由第一次修改

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/list/{$id}' ,function (int $id){
+    return 'id 的值：'.$id;
+});
