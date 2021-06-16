4ce600407a4fa1af3bf93a744e6bc46f0e59651b
Ajay Patel <ajaypatel@atyantik.com>
2021-06-16 20:38:38 +0530

    Reformat indentation and spaces.

--- config/queue.php
+++ config/queue.php
@@ -89,16 +89,16 @@ return [
         'database' => env('DB_CONNECTION', 'mysql'),
         'table' => 'failed_jobs',
     ],
-    
+
     /*
     |----------------------------------------------------------------------------
     | Batch Configuration
-    |---------------------------------------------------------------------------- 
-    | This option will allow us to change `job_batches` table name and DB connection 
-    | used for the same.
+    |----------------------------------------------------------------------------
+    | This option will allow us to change `job_batches` table name and DB connection
+    | used for for storing batches table.
     |
     */
-    'batching'=>[
+    'batching' => [
         'database' => env('DB_CONNECTION', 'mysql'),
         'table' => 'job_batches'
     ]
