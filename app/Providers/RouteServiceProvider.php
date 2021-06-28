5faa23168e62d87fc03a287fc39b9dd8cbf9acab
Amir Abouei <abouei.me@gmail.com>
2021-06-29 01:58:11 +0430

    Update RouteServiceProvider.php
    Fix routes problem, during using route commands in command line with php artisan

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    protected $namespace = 'App\Http\Controllers';
     /**
      * The path to the "home" route for your application.
      *
