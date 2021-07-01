c5f6f6187c24e160309df372b98d20380b786784
Josh Salway <josh.salway@gmail.com>
2021-07-01 23:37:19 +1000

    Update validation.php (#5637)

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -117,7 +117,7 @@ return [
     ],
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
-    'timezone' => 'The :attribute must be a valid zone.',
+    'timezone' => 'The :attribute must be a valid timezone.',
     'unique' => 'The :attribute has already been taken.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute format is invalid.',
