a3f22e73b2801951bc5354e808231fd347bf457b
Tekin Aydoğdu <tekinaydogdu01@gmail.com>
2021-07-03 22:37:59 +0300

    Update database.php - code quality improvement

--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
