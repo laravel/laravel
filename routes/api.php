3c887263ec252ec88875a0f598ef31cefb157ad8
Marco Aurélio Deleu <deleugyn@gmail.com>
2021-07-04 18:36:30 +0200

    Add sample routing for API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return response(['data' => 'Hello from Laravel!']);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
