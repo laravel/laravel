a340d36f583e52450023882267e79a0492826bc7
iAm[i]nE <tihouati.amine@gmail.com>
2021-08-02 16:19:33 +0100

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,9 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (! $request->expectsJson()) {
+                    return redirect(RouteServiceProvider::HOME);
+                }
             }
         }
 
