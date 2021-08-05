e5962266d79ab4aa3b7e49d1d5cddb7f85c057e6
Cristiano Fromagio <16294559+cristianofromagio@users.noreply.github.com>
2021-08-05 10:45:48 -0300

    [8.x] Add accepted_if validation rule (#5658)
    * Add accepted_if validation rule
    
    * Update validation.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -14,6 +14,7 @@ return [
     */
 
     'accepted' => 'The :attribute must be accepted.',
+    'accepted_if' => 'The :attribute must be accepted when :other is :value.',
     'active_url' => 'The :attribute is not a valid URL.',
     'after' => 'The :attribute must be a date after :date.',
     'after_or_equal' => 'The :attribute must be a date after or equal to :date.',
