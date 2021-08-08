9eb8fe775d8b154311a0401bc646ef94ded0f6eb
Bruce Ingalls <bruce.ingalls@gmail.com>
2021-08-08 17:00:15 -0400

    support sqlite cfg for memory unit tests & relative proj path

--- config/database.php
+++ config/database.php
@@ -38,7 +38,8 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE') === ':memory:' ? ':memory:'
+                : database_path(env('DB_DATABASE', 'database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
