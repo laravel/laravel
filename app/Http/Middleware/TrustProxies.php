001e33dd2cd8a8b902fc8656a8858d7c18b30c96
Dries Vints <dries@vints.io>
2021-08-10 09:34:05 +0200

    Use new TrustProxies middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
