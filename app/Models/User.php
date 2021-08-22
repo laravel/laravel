e332622979caaf7ee782e57424bb60b60dab6a7c
Maarten Buis <buismaarten@outlook.com>
2021-08-22 22:50:31 +0200

    Use phpdoc comments from Model class

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for serialization.
      *
      * @var array
      */
