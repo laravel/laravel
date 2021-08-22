be5aef65d3e25f3e22aba966cb80394cd5d788d1
Maarten Buis <buismaarten@outlook.com>
2021-08-22 22:56:54 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = [
         'name',
@@ -34,7 +34,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be cast.
      *
      * @var array
      */
