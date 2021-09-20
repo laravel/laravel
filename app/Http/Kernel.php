20cd618a45032d9eb25fa01cc43f7116f57d44d9
Alexander Dmitrjuk <a.dmitrjuk@robo.finance>
2021-09-20 11:09:16 +0700

    Fix typo

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends HttpKernel
     /**
      * The application's global HTTP middleware stack.
      *
-     * These middleware are run during every request to your application.
+     * These middlewares are run during every request to your application.
      *
      * @var array
      */
@@ -49,7 +49,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware.
      *
-     * These middleware may be assigned to groups or used individually.
+     * These middlewares may be assigned to groups or used individually.
      *
      * @var array
      */
