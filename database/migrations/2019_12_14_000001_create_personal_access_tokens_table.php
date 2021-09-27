7445dd72607e4e6900ac34a45767167ef75fb359
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2021-09-27 18:32:30 -0500

    Keeping access tokens migration id consistent
    This is just a tiny "fix," using id(); to be consistent with the other migrations already included in the framework.

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,7 +14,7 @@ class CreatePersonalAccessTokensTable extends Migration
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
