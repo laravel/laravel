2c644455dabb133ad305f367379857094631f35f
Markus Machatschek <mmachatschek@users.noreply.github.com>
2021-09-29 16:44:21 +0200

    Use anonymous migration for personal_access_tokens table (#5698)
    * Use new migration class name style for personal_access_tokens table
    
    * Update 2019_12_14_000001_create_personal_access_tokens_table.php

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -33,4 +33,4 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::dropIfExists('personal_access_tokens');
     }
-}
+};
