281a033928ac856f6a28db3a78b061140d52ad15
Markus Machatschek <mmachatschek@users.noreply.github.com>
2021-09-29 16:41:33 +0200

    Use new migration class name style for personal_access_tokens table

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
