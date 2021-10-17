a23c4f03f8cdb80ab7d38701e55d51f885c16b8b
Iman <imanghafoori1@gmail.com>
2021-10-18 02:53:39 +0330

    Update docblocks of 'failed'

--- config/queue.php
+++ config/queue.php
@@ -80,7 +80,9 @@ return [
     |
     | These options configure the behavior of failed queue job logging so you
     | can control which database and table are used to store the jobs that
-    | have failed. You may change them to any database / table you wish.
+    | have failed. Set the table and the driver to null to avoid saving.
+    |
+    | Drivers: "database-uuids", "dynamodb", "null"
     |
     */
 
