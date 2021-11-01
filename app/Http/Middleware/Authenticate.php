fa838405b74115c7de0d8ba068acf75e8f315f46
Viktor Szépe <viktor@szepe.net>
2021-11-01 14:31:57 +0100

    Return explicit values in Authenticate::redirectTo

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if ($request->expectsJson()) {
+            return null;
         }
+
+        return route('login');
     }
 }
