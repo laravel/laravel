399d435c4f0b41a5b6d3e14894195f9196d36bb8
Taylor Otwell <taylor@laravel.com>
2021-11-09 09:56:23 -0600

    add facade

--- config/app.php
+++ config/app.php
@@ -209,6 +209,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Http' => Illuminate\Support\Facades\Http::class,
+        'Js' => Illuminate\Support\Js::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
