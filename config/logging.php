656eb9894f15314d98055eaa9fa9bd45eff607a3
Bram <bramceulemans@me.com>
2021-12-02 19:57:07 +0100

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -37,9 +37,9 @@ return [
     | Log Channels
     |--------------------------------------------------------------------------
     |
-    | Here you may configure the log channels for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you with a variety of powerful log handlers / formatters to utilize.
+    | Here you may configure the log channels for your application. Out of the
+    | box, Laravel uses the Monolog PHP logging library. This gives you
+    | with a variety of powerful log handlers / formatters to utilize.
     |
     | Available Drivers: "single", "daily", "slack", "syslog",
     |                    "errorlog", "monolog",
