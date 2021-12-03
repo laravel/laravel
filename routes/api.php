a40bd595a18b3d2035cf89f3356b045db40421b4
Isaac Hsu <16hsuisaac@gmail.com>
2021-12-02 16:24:50 -0800

    removed default route in laravel file for /user

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
