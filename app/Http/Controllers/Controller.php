9e38ab0933bb0cf66d0a63d0d46c79b3e819638c
Isaac Hsu <16hsuisaac@gmail.com>
2021-12-02 21:42:25 -0800

    finished function for updating users

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,4 +24,21 @@ class UserAPIController extends Controller
 
         return response($user, 201);
     }
+
+    public function updateUser(Request $request, $id) {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required'
+        ]);
+        if (User::where('id', $id)->exists()) {
+            $user = User::where('id', $id);
+            $user->update($request->all());
+            return response($user, 200);
+        } else {
+            return response()->json([
+                "message" => 'user does not exist'
+            ], 404);
+        }
+    }
 }
