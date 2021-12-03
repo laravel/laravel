3629b5c7b4d83e5b59fa63f7f3679cfc4fd13570
Isaac Hsu <16hsuisaac@gmail.com>
2021-12-02 16:24:32 -0800

    created branch and reverted files

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
