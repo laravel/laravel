676a26e846315d5a43c5a9ec47cd0f0bff5f6bd5
Mohammad Hosein Abedini <mohammadhoseinabedini@gmail.com>
2021-12-07 09:39:26 +0330

    Remove redundant default value for asset_url in app.php config file
    The default value for env function is null, so null value in asset_url is just simply redundant

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
