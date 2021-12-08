eb94f29edf7b438ab95969a6ffc596bdf74880fc
rennokki <alex@renoki.org>
2021-12-08 15:54:43 +0200

    Add client_options to Pusher driver

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,6 +39,10 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
             ],
+            'client_options' => [
+                // Pass custom Guzzle options: https://docs.guzzlephp.org/en/stable/request-options.html
+                // 'verify' => true,
+            ],
         ],
 
         'ably' => [
