437e0a989529270b2bd5e29ee23dfb857297d022
Dries Vints <dries@vints.io>
2021-12-17 12:19:23 +0100

    Update validation.php

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -45,6 +45,7 @@ return [
     'distinct' => 'The :attribute field has a duplicate value.',
     'email' => 'The :attribute must be a valid email address.',
     'ends_with' => 'The :attribute must end with one of the following: :values.',
+    'enum' => 'The selected :attribute is invalid.',
     'exists' => 'The selected :attribute is invalid.',
     'file' => 'The :attribute must be a file.',
     'filled' => 'The :attribute field must have a value.',
