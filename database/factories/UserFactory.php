4c94086856e5a876dd8d0ae933bf96d14964ba70
Craig Morris <craig.michael.morris@gmail.com>
2022-01-21 12:21:38 +0000

    switch to extends

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @implements Factory<User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
