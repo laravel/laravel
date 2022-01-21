7bff7c6c61aa5865966cdd6cea93409771f97413
Craig Morris <craig.michael.morris@gmail.com>
2022-01-21 12:03:57 +0000

    add type to UserFactory for generic parent class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+/**
+ * @implements Factory<User>
+ */
 class UserFactory extends Factory
 {
     /**
