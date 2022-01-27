aed682e0deeac10ba6465a398ef4cb4856a6d35e
Beau Simensen <beau@dflydev.com>
2022-01-27 12:00:18 -0600

    [9.x] Make it easier to support Papertrail on Vapor out of the box (#5780)
    * Make it easier to support Papertrail on Vapor
    
    * Fixed style

--- config/logging.php
+++ config/logging.php
@@ -78,10 +78,11 @@ return [
         'papertrail' => [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => SyslogUdpHandler::class,
+            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
         ],
 
