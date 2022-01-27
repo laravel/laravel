d4abd6baddfed3b4f98b449836539304f5742049
Beau Simensen <beau@dflydev.com>
2022-01-26 20:25:39 -0600

    Fixed style

--- config/logging.php
+++ config/logging.php
@@ -82,7 +82,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
         ],
 
