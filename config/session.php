a43611e4554f74dfdc49b8a3c1e59fc608315206
Said Bakr <said.fox@gmail.com>
2022-02-01 11:55:24 +0200

    Update domain entry
    Replacing `null` with `$_SERVER['HTTP_HOST']` in `domain` entry. To allow valid domain for multiple domains apps.

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', $_SERVER['HTTP_HOST']),
 
     /*
     |--------------------------------------------------------------------------
