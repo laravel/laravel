a0b3dea4f09887060a92be176117879e11016184
Kostya <75450127+krvvko@users.noreply.github.com>
2022-02-01 07:48:08 -0500

    Fixing the length of the description block
    I know that every next comment description block should be exactly 3 symbols shorter, than on the previous line. I found the bug and fixed it!

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create something great
 |
 */
 
