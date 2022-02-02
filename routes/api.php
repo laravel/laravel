480f00b90368c8062dc8f3a3c002cc8bf243988e
Anjorin Damilare <damilareanjorin1@gmail.com>
2022-02-02 04:18:01 +0100

    [9.x]: change route default callback to arrow function

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
