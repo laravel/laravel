1a78125e9b158036ba3fca8e0341f8825e871ef0
Anjorin Damilare <damilareanjorin1@gmail.com>
2022-02-02 04:22:19 +0100

    [9.x]: change default broadcast channel callback to arrow func

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+Broadcast::channel('App.Models.User.{id}', fn ($user, $id) => (int) $user->id === (int) $id);
