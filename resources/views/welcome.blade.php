3e836560511fd77434040870c4d9599a97a227da
Dzaki Ahnaf Z <61301953+dzaki236@users.noreply.github.com>
2022-02-07 13:19:38 +0700

    remove "/" on home url()
    remove "/" use home only. .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
