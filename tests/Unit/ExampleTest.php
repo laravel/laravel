aaddbc7dac612e25713fcc6f40888ab34c893df2
Roger Vilà <rogervila@me.com>
2022-02-09 09:14:00 +0100

    Update ExampleTest.php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -8,10 +8,8 @@ class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
-     * @return void
      */
-    public function test_that_true_is_true()
+    public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
     }
