e077976680bdb2644698fb8965a1e2a8710b5d4b
Felix Dorn <github@felixdorn.fr>
2022-02-11 15:22:45 +0100

    [9.x] Remove redundant `null`s (#5811)
    * follow up of #5791
    
    * Update app.php
    
    Co-authored-by: Dries Vints <dries@vints.io>

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
