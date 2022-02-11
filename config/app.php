b9c7b0e3c26bb008083602d124490535558e4b54
Dries Vints <dries@vints.io>
2022-02-11 12:31:13 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -57,6 +57,7 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
