dceec150949f645ca4cfbe3581d556adbcfb7123
m4tlch <m4tlch@users.noreply.github.com>
2022-02-15 17:04:48 +0200

    Add underscore to prefix in database cache key
    For Redis caching prefix with underscore : 
    'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
    
    but when cache stored in database, then the key is created "merged" with prefix, by this line:  'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'), for example, if "key" is a key for cache, then the result is: "laravel_cachekey", not a preferable "laravel_cache_key"

--- config/cache.php
+++ config/cache.php
@@ -105,6 +105,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
 
 ];
