871ff9e65fea5588b1018d8770db6fc6ad37069d
suyar <296399959@qq.com>
2022-02-17 23:16:40 +0800

    [9.x] Update sanctum config file (#5820)
    * Update sanctum config file
    
    * Update composer.json
    
    Co-authored-by: suyaqi <suyaqi@wy.net>
    Co-authored-by: Dries Vints <dries@vints.io>

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravel\Sanctum\Sanctum;
+
 return [
 
     /*
@@ -16,7 +18,7 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
