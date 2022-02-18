a54da82e5ad1ef12a738cf06a7feba8a7bd140c8
Sagheer Hussain Zardari <sagheerhzardari@gmail.com>
2022-02-18 13:18:45 +0500

    Changed Login & Register Button
    Login and Register button were looking just text so I made changes so they can look like buttons.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,9 +25,9 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 bg-green-500 rounded-md px-12 shadow-md dark:text-gray-500 underline">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 bg-green-500 rounded-md px-12 shadow-md dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
