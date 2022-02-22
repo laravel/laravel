969ff64e02b5ba316ebfe58aba76ca9ceac34542
Roy Shay <tooshay@users.noreply.github.com>
2022-02-22 21:05:53 +0000

    Small typo fix in filesystems.php (#5827)
    * Update filesystems.php
    
    * Update filesystems.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- config/filesystems.php
+++ config/filesystems.php
@@ -22,7 +22,7 @@ return [
     |
     | Here you may configure as many filesystem "disks" as you wish, and you
     | may even configure multiple disks of the same driver. Defaults have
-    | been setup for each driver as an example of the required options.
+    | been set up for each driver as an example of the required values.
     |
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
