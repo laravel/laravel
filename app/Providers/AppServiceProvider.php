6d7c024417efb35fc3dc5ea8da31c754c2e04f1a
Rhaima Mohamed <68730147+Rhaima96@users.noreply.github.com>
2022-02-28 14:23:21 +0100

    Update AppServiceProvider.php
    Fix laravel migration error by adding "Schema::defaultStringLength(191);" in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
