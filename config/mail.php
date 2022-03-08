95fec9a3e8a98bc58f6a932875f84d62a1693308
Dries Vints <dries@vints.io>
2022-03-08 15:38:39 +0100

    Update mail.php (#5836)

--- config/mail.php
+++ config/mail.php
@@ -58,7 +58,7 @@ return [
 
         'sendmail' => [
             'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -t -i'),
+            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
         ],
 
         'log' => [
