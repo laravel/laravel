51007981ceee00eba2183ba8da6948aca94fddb3
Taylor Otwell <taylor@laravel.com>
2022-03-16 16:00:42 -0500

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,7 +55,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth_session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
