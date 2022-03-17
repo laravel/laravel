2df87e49beacb93b3682f58b14530875fded4a20
Matt Slater <matthew.slater@engageinteractive.co.uk>
2022-03-17 08:47:29 +0000

    Updates viewport function style to Arrow Functions

--- resources/assets/js/composables/useViewport.js
+++ resources/assets/js/composables/useViewport.js
@@ -12,14 +12,14 @@ import {
 
 import variables from 'assets/variables.json';
 
-function useResize(callback, delay = 300) {
+const useResize = (callback, delay = 300) => {
 	const onDebouncedResize = useDebounceFn(callback, delay);
 
 	onMounted(() => window.addEventListener('resize', onDebouncedResize));
 	onUnmounted(() => window.removeEventListener('resize', onDebouncedResize));
 }
 
-function useMq() {
+const useMq = () => {
 	const breakpoints = reactive(useBreakpoints(variables.breakpoints));
 
 	return {
