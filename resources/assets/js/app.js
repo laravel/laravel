2e9b80185a687d307f7a989fb19cdf0f75a5bae8
Matt Slater <matthew.slater@engageinteractive.co.uk>
2022-03-17 08:46:35 +0000

    Updates use of @ alias | Re-order's imports alphabetically

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,17 +1,17 @@
 import { createApp } from 'vue';
 
-import '@/js/etc/validators';
+import './etc/validators';
 
-import Form from './components/common/form';
 import Button from './components/common/Button';
+import Form from './components/common/form';
 import Icon from './components/common/Icon';
 import Placeholder from './components/common/Placeholder';
 
 const app = createApp({});
 
-app.component('EForm', Form);
 app.component('EButton', Button);
-app.component('EPlaceholder', Placeholder);
+app.component('EForm', Form);
 app.component('EIcon', Icon);
+app.component('EPlaceholder', Placeholder);
 
 app.mount('#app');
