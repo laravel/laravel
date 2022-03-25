8594815f5e85a8e9d12519c5df1bbda6efb1ae95
neoteknic <neoteknic@gmail.com>
2022-03-25 16:38:42 +0100

    Add username in config to use with phpredis + ACL (#5851)
    Linked to https://github.com/laravel/framework/pull/41683/commits

--- config/database.php
+++ config/database.php
@@ -129,6 +129,7 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
@@ -137,6 +138,7 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
