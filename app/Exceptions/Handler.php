a8cefc2dd15a7af467b17bd2c058054a76e39902
Taylor Otwell <taylor@laravel.com>
2022-04-12 09:31:23 -0500

    update wording

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,7 +8,7 @@ use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exceptions with their corresponding custom log levels.
+     * A list of exception types with their corresponding custom log levels.
      *
      * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
      */
