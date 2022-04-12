a507e1424339633ce423729ec0ac49b99f0e57d7
Taylor Otwell <taylor@laravel.com>
2022-04-12 09:30:44 -0500

    add levels to handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,15 @@ use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
+     * A list of exceptions with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
      * A list of the exception types that are not reported.
      *
      * @var array<int, class-string<Throwable>>
