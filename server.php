352a81eac916c31c921baa63e9b173eace79a3dd
Dries Vints <dries@vints.io>
2022-04-12 08:56:38 +0200

    Update server.php

--- server.php
+++ server.php
@@ -8,7 +8,7 @@
  */
 
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
