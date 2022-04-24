2f7c242bb130a1e2f8fcac8a41354092f03417cd
Nicolas Hedger <nicolas@hedger.ch>
2022-04-24 17:46:00 +0200

    Add return types

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -22,20 +22,16 @@ class EventServiceProvider extends ServiceProvider
 
     /**
      * Register any events for your application.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         //
     }
 
     /**
      * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
      */
-    public function shouldDiscoverEvents()
+    public function shouldDiscoverEvents(): bool
     {
         return false;
     }
