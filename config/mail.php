4bbac7fa1e9aaa520a495bf13f3c2552f2ed9ee3
Bram in 't Zandt <bram@bigspark.com>
2022-05-04 13:28:27 +0200

    Update mail.php
    Add `local_domain` as an option to the smtp configuration. This can be used to change the domain that is used to send the `EHLO` command during the SMTP handshake.
    
    `null` is a sensible default since Symfony/Mailer will use it's own default (`127.0.0.1`) to send the mail.

--- config/mail.php
+++ config/mail.php
@@ -42,6 +42,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
