5c5a300bfd83cd3432eb342fdabf2b85bfaee4bd
Dries Vints <dries@vints.io>
2022-05-05 15:53:49 +0200

    [9.x] Add specific test user in seeder (#5879)
    * Update DatabaseSeeder.php
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
