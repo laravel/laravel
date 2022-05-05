12cbd193679a5bc1119e385e53134a3445cb7beb
Mojtaba Moosavi <mojtaba06@yahoo.com>
2022-05-05 16:43:57 +0430

    change id type of personal_access_tokens table
    on sanctum, the id of the personal_access_tokens table is bigIncrements type,
    but on the personal_access_tokens table that creates as default, the id has id type.
    this merge request change id to the bigIncrements on the personal_access_tokens table.

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements();
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
