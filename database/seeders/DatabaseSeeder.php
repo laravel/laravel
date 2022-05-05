aacf594fc547bcffcb43af8db0cb31d460ba94f0
Dries Vints <dries@vints.io>
2022-05-05 13:47:30 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // \App\Models\User::factory()->create(['name' => 'John Doe', 'email' => 'john@example.com']);
+
         // \App\Models\User::factory(10)->create();
     }
 }
