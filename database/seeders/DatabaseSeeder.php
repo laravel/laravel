82cf9c6558cad7d9f50b8e96b80bc99c0d6c48bf
Taylor Otwell <taylor@laravel.com>
2022-05-05 08:52:48 -0500

    formatting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory()->create(['name' => 'John Doe', 'email' => 'john@example.com']);
-
         // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
