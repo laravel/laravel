8d6d5eaf954efe073e56741c2691d5c3593abdc7
Dominique FERET <dominique.feret@ingenidoc.fr>
2022-05-10 15:58:17 +0200

    Add visibility parameter on S3 config
    This prevents a write error if you write to a private bucket.

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
+            'visibility' => env('AWS_VISIBILITY', 'private'),
         ],
 
     ],
