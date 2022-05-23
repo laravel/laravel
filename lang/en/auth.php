a050645371b079a2df621e7430bc0251336bbe91
jaxramus <104056541+jaxramus@users.noreply.github.com>
2022-05-23 19:04:04 -0400

    Add verification-link-sent to auth lang
    Add verification-link-sent to auth lang which is used in official starter kits.

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,6 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'verification-link-sent' => 'A new verification link has been sent to your registered email address.',
 
 ];
