98ad5b1bb3ad8e346f68e9e7019885598a1dbe97
Muhammed Sari <muhammed@dive.be>
2022-05-25 17:34:01 +0200

    Use shared application instance instead of helper

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', $app->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
