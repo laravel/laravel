fa8284b4eb4d041ce96b901ca32462ef51ac79be
Sivadas Rajan <50444047+sivadasrajan@users.noreply.github.com>
2022-06-07 22:54:03 +0530

    Update database.php
    Fixed Foreign key not generated during migration on wamp- MySQL version: 5.5.5-10.3.14-MariaDB by adding a default value.

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
