a43f8e2ac0e3da3dba7393509c9117f5e2ab5cef
rennokki <alex@renoki.org>
2022-06-07 20:06:16 +0300

    Added support for easy development configuration in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
+//     wsHost: process.env.MIX_PUSHER_HOST ?? `ws-${process.env.MIX_PUSHER_CLUSTER}.pusher.com`,
+//     wsPort: process.env.MIX_PUSHER_PORT ?? 80,
+//     wssPort: process.env.MIX_PUSHER_PORT ?? 443,
+//     forceTLS: (proces.env.MIX_PUSHER_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
 // });
