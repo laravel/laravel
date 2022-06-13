6b43cfa228e053f971ade13fd10d4ef268b6277a
Jess Archer <jess@jessarcher.com>
2022-06-13 10:32:50 +1000

    Use CSS entry points

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,1 @@
 import './bootstrap';
-import '../css/app.css';
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
-        laravel(),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 })
