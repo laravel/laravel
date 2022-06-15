c6eabb66232ae3ce1139cf19171f7f2a97804646
Jess Archer <jess@jessarcher.com>
2022-06-15 14:27:38 +1000

    Linting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -8,4 +8,4 @@ export default defineConfig({
             'resources/js/app.js',
         ]),
     ],
-})
+});
