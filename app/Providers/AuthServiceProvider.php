ff44ab02e32dd77be74efdd85583390ac084e84b
MathiasReker <mathias@reker.dk>
2022-06-16 20:25:13 +0200

    No unused imports
    Unused use statements must be removed.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
