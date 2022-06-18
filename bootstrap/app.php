10c390899ee8a91a43abce21425b8e95cc065821
bbrist <brettbrist@gmail.com>
2022-06-18 16:47:35 -0600

    Add ConsoleOutput to Service Container
    Register ConsoleOutput as singleton so it can be injected elsewhere

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,11 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->singleton(
+    Symfony\Component\Console\Output\ConsoleOutput::class,
+    Symfony\Component\Console\Output\ConsoleOutput::class
+);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
