86b4b1b65659a7b0f4b3dad1e753aa1efa5a7642
Jess Archer <jess@jessarcher.com>
2022-06-23 04:07:47 +1000

    [9.x] Vite (#5904)
    * Use Vite
    
    * Gitignore Vite build directory
    
    * Use CSS entry points
    
    * Update plugin
    
    * Linting
    
    * Update plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,7 +25,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+    ],
+});
