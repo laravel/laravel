d694bc06ccbf66f4f8ce432c39b71deea6766135
rennokki <alex@renoki.org>
2022-06-24 21:35:29 +0300

    [9.x] Added support for easy development configuration in bootstrap.js (#5900)
    * Added support for easy development configuration in bootstrap.js
    
    * Added extra variables for existing configuration in broadcasting
    
    * Update bootstrap.js
    
    * Setting default for empty variable
    
    * Update .env.example
    
    * Update .env.example
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     forceTLS: true
+//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_CLUSTER}.pusher.com`,
+//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
 // });
