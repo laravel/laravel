a091aab1340bec65937bc803d5d9f7eead887fbb
Mohammad Amin Dehghani <m.a.dehghani609@gmail.com>
2022-06-28 18:11:02 +0430

    [9.x] Force https scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
