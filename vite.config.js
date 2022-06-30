82f2e2ff5b9d04b3bc1b89739e37dee842de36ca
Alexander Wulf <alex.f.wulf@gmail.com>
2022-06-30 17:04:02 +0200

    Hot reload added

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
 });
