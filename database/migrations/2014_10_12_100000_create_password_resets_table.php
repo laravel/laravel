46559c26ac7239caa59911f51224d51f03c97110
Sam Lambert <sam@planetscale.com>
2022-06-30 17:20:50 -0700

    Add autoinc id column to password_resets table
    It is generally good hygiene to have auto-incrementing id columns explicitly defined on all MySQL tables.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->id();
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
