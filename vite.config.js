a4372d1f7ed79c68511c0601c385c6925d53bc7e
Tim MacDonald <hello@timacdonald.me>
2022-07-01 14:03:12 +1000

    add default reloading to skeleton

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
 });
