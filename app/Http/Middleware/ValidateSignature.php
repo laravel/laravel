d08853a5060e7462d06cd9cdcdfeb73757a17af3
Stephen Rees-Carter <stephen@rees-carter.net>
2022-07-13 18:59:20 +1000

    Remove leading slash

--- app/Http/Middleware/ValidateSignature.php
+++ app/Http/Middleware/ValidateSignature.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use \Illuminate\Routing\Middleware\ValidateSignature as Middleware;
+use Illuminate\Routing\Middleware\ValidateSignature as Middleware;
 
 class ValidateSignature extends Middleware
 {
