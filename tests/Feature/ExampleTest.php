fa5e54a2ab6b1de8a4cef69b59bcfdecd07ab0cb
Nuno Maduro <enunomaduro@gmail.com>
2022-07-15 14:38:49 +0100

    [9.x] Uses `laravel/pint` for styling (#5945)
    * Uses `laravel/pint` for styling
    
    * Makes `.styleci.yml` ignored on export
    
    * Update composer.json
    
    Co-authored-by: Dries Vints <dries@vints.io>

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
