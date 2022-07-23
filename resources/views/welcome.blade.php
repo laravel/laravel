47f381ab2881097e521737d26791af7bab5e2be8
Abenet Tamiru <me@abenet.email>
2022-07-23 08:13:39 +0200

    Update font delivery
    Seeing the non compliance of Google Fonts to GDPR I thought to update the CDN.
    My non-designer eyes could see no difference on the site.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
