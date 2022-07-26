59e58bfb310fcfea6096f28a034b8e2efaf5696c
manuglopez <darkwebside@gmail.com>
2022-07-26 14:16:55 +0200

    Add missing favicon to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
         <title>Laravel</title>
 
         <!-- Fonts -->
