b37c970a3a6b8ebb9d415ec55405b958230b7025
Ghost Walker <100309290+GhostWalkers@users.noreply.github.com>
2022-08-11 20:36:07 +0300

    Remove extra way
    The path is already written in use

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
