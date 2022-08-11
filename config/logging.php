a666dc0c21937668bfac6a2e00a4dee0a56106ce
OGDAMS <62488009+Ogdams@users.noreply.github.com>
2022-08-11 10:16:37 +0100

    separate logs (daily)

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
