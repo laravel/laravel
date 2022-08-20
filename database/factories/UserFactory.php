7b17f5f32623c2ee75f2bff57a42bb8f180ac779
Taylor Otwell <taylor@laravel.com>
2022-08-20 12:22:43 -0500

    use short closure

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,10 +33,8 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
