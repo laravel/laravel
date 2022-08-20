e2e25f607a894427d6545f611ad3c8d94d022e9d
Taylor Otwell <taylor@laravel.com>
2022-08-20 12:46:21 -0500

    use except

--- app/Http/Middleware/ValidateSignature.php
+++ app/Http/Middleware/ValidateSignature.php
@@ -7,11 +7,11 @@ use Illuminate\Routing\Middleware\ValidateSignature as Middleware;
 class ValidateSignature extends Middleware
 {
     /**
-     * The names of the parameters that should be ignored.
+     * The names of the query string parameters that should be ignored.
      *
      * @var array<int, string>
      */
-    protected $ignore = [
+    protected $except = [
         // 'fbclid',
         // 'utm_campaign',
         // 'utm_content',
