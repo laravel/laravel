bdd7d6ce887e2eb686c34f0e449f106c0969bed0
sacOO7 <sachinshinde7676@gmail.com>
2022-09-02 11:54:28 +0530

    Updated ably specific broadcasting env. variables

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -49,10 +49,10 @@ return [
 
         'ably' => [
             'driver' => 'ably',
+            'native_client' => env('ABLY_NATIVE_CLIENT', false),
             'key' => env('ABLY_KEY'),
             'token_expiry' => env('ABLY_TOKEN_EXPIRY', 3600),
             'disable_public_channels' => env('ABLY_DISABLE_PUBLIC_CHANNELS', false),
-            'pusher_adapter' => env('ABLY_PUSHER_ADAPTER', false),
         ],
 
         'redis' => [
