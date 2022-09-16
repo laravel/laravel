191a5d3113b67822c4077bd532f6ca8c97ae5f67
cmx_star <316785038@qq.com>
2022-09-16 15:56:38 +0800

    测试贡献代码

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,7 +7,7 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Get the path the user should be redirected to when they are not authenticated.1
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
