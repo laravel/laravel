c8e52d60cbd209131761a44304610bb92e878f84
Madalin Ignisca <madalin.ignisca@outlook.com>
2022-09-27 10:50:47 +0200

    Allow overriding LOG path and LOG daily keep limit

--- config/logging.php
+++ config/logging.php
@@ -59,15 +59,15 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => env('LOG_PATH', storage_path('logs/laravel.log')),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => env('LOG_PATH', storage_path('logs/laravel.log')),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => env('LOG_DAILY_KEEP', 14),
         ],
 
         'slack' => [
