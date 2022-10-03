48cc1f392bdf032ae604a67165d41bbabbf55799
Dries Vints <dries@vints.io>
2022-10-03 09:10:03 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+        // \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
     ];
 
     /**
