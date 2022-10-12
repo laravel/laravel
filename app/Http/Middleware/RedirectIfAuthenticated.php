4bdbd21ba94f1fb941c0dfd22efe2ca8996854f1
Nuno Maduro <enunomaduro@gmail.com>
2022-10-12 11:33:45 +0100

    Fixes `RedirectIfAuthenticated`

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,19 +4,18 @@ namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
 use Closure;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string|null ...$guards): Response|RedirectResponse
+    public function handle(Request $request, Closure $next, string|null ...$guards): Response
     {
         $guards = empty($guards) ? [null] : $guards;
 
