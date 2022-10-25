272416b9e7231a83cfd4006ae2ad0e1d30854ae1
David Bomba <turbo124@gmail.com>
2022-10-25 12:56:15 +1100

    Update filesystems configuration to include stub for Cloudflare R2

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,18 @@ return [
             'throw' => false,
         ],
 
+        'r2' => [
+            'driver' => 's3',
+            'key' => env('R2_ACCESS_KEY_ID'),
+            'secret' => env('R2_SECRET_ACCESS_KEY'),
+            'region' => env('R2_DEFAULT_REGION'),
+            'bucket' => env('R2_BUCKET'),
+            'url' => env('R2_URL'),
+            'visibility' => 'private',
+            'endpoint' => env('R2_ENDPOINT'),
+            'use_path_style_endpoint' => env('R2_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
     ],
 
     /*
