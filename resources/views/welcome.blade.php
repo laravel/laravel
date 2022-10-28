3e43536d752ebfcaec60fd7ae1525f87c4f353ff
Shahghasi Adil <shaghasY555@gmail.com>
2022-10-28 21:32:11 +0430

    better way of showing laravel current version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,7 @@
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ app()->version() }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
