b0c0522c515ad09b4d3c889cc1409f83b8e25dd7
Kenny <kenny@hietbr.ink>
2022-11-17 15:18:02 +0100

    Fix typo in Session.php
    'a' should be 'an' in this context  🙂

--- config/session.php
+++ config/session.php
@@ -163,7 +163,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | By setting this option to true, session cookies will only be sent back
-    | to the server if the browser has a HTTPS connection. This will keep
+    | to the server if the browser has an HTTPS connection. This will keep
     | the cookie from being sent to you when it can't be done securely.
     |
     */
