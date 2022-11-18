d28b1cc248bdd39f961d1cfa7be8d80994dd75cb
Tendai Karuma <55027177+protendai@users.noreply.github.com>
2022-11-18 08:43:00 +0200

    Removed ?  after user() which was causing errors
    Changed line 49  from
    return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
    to
    return Limit::perMinute(60)->by($request->user()->id ?: $request->ip());
    
    The  ?  after user() was causing errors. Noticed it yesterday after creating a new project

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(60)->by($request->user()->id ?: $request->ip());
         });
     }
 }
