d91cfdb20b8f1647da76fd4017298b0006e2ff80
hafijur rahman <bmsakib09.hr@gmail.com>
2022-11-30 16:01:07 +0600

    Remove Disabled demonstration command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        //
     }
 
     /**
