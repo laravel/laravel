39f4830e92a7467b2a7fe6bc23d0ec14bc3b46a6
Taylor Otwell <taylor@laravel.com>
2022-12-19 11:35:07 -0600

    add decimal translation

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -37,6 +37,7 @@ return [
     'date' => 'The :attribute is not a valid date.',
     'date_equals' => 'The :attribute must be a date equal to :date.',
     'date_format' => 'The :attribute does not match the format :format.',
+    'decimal' => 'The :attribute must have :decimal decimal places.',
     'declined' => 'The :attribute must be declined.',
     'declined_if' => 'The :attribute must be declined when :other is :value.',
     'different' => 'The :attribute and :other must be different.',
