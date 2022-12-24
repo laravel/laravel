daa72336db3af440f32baa81d0969a1c69bef552
Tushar Juneja <tusharjuneja204@gmail.com>
2022-12-24 20:37:26 +0530

    Fix(typo): grammar error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@
 |
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
+| the IoC container for the system binding of all the various parts.
 |
 */
 
