ca01443b96e02ca79ef138d615d5a36b7484a03c
Vytautas M <balu-lt@users.noreply.github.com>
2022-12-27 18:39:26 +0200

    [9.x] Support pusher-js v8.0 (#6059)
    * Specify cluster for Pusher
    
    * Update bootstrap.js
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
 //     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
 //     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
 //     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
