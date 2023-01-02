05c87c2d0619758a60ee61ff773277399b0a7e3a
Andrew <browner12@gmail.com>
2023-01-02 01:05:42 -0600

    switch email to a primary key
    switching from a normal index here to a primary index works the same except for adding a `UNIQUE` constraint.
    
    The `DatabaseTokenRepository` deletes existing records with an email first, before creating a new one, so this additional constraint will be okay.
    
    https://github.com/laravel/framework/blob/9.x/src/Illuminate/Auth/Passwords/DatabaseTokenRepository.php#L88

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
