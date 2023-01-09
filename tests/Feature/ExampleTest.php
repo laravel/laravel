d53b4096e72a2e7ee2f2e1fa939a9321e0875bcc
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-01-09 16:40:02 +0000

    [9.x] Updated Example Feature Test to use fluent syntax

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/')
+            ->assertOk();
     }
 }
