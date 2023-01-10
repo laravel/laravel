568a4ad40ec2200eb795c0d6fee85e0640d2d2e2
andiLeong <alwayssmile20150724@gmail.com>
2023-01-10 10:30:36 +0800

    add default secure option in seesion.php config

--- config/session.php
+++ config/session.php
@@ -168,7 +168,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
