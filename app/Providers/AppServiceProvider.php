cc811dced0556442856e11810b062b8d08ee9b1c
Caner Kılıçoğlu <gralrummy@gmail.com>
2023-01-24 10:26:23 +0300

    function required for migration adjustments after every installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(config('database.connections.mysql.defaultstringlength') ? config('database.connections.mysql.defaultstringlength') : 191);
     }
 }
