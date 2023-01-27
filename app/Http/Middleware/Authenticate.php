32513db2a1d0660cd5f09a4819e348cae1e7c1cf
Jason McCreary <jason@pureconcepts.net>
2023-01-27 06:44:57 -0500

    Use nullable typing

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,7 +10,7 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): string|null
+    protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
     }
