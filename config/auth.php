a28ad2966df4488c873f793c6e5b03cbea547d1e
Taylor Otwell <taylor@laravel.com>
2023-01-30 16:53:14 -0600

    rename password reset tokens table in skeleton

--- config/auth.php
+++ config/auth.php
@@ -89,7 +89,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_resets',
+            'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
         ],
--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_resets', function (Blueprint $table) {
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
