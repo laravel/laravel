112201a34a8cd466423800feef6d2bbaa4523726
Taylor Otwell <taylor@laravel.com>
2023-02-02 20:25:38 -0600

    first pass at refined skeleton

--- public/index.php
+++ public/index.php
@@ -44,7 +44,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../.laravel/app.php';
 
 $kernel = $app->make(Kernel::class);
 
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -12,7 +12,7 @@ trait CreatesApplication
      */
     public function createApplication(): Application
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__.'/../.laravel/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
