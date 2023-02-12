5b60b604c4167b30286a6e033723a6b274e8452d
Arne_ <a.perschke@hctec.net>
2023-02-12 21:06:25 +0100

    [9.x] Adds clarification to throttle auth setting (#6096)
    * adds clarification to throttle auth setting
    
    * Update auth.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- config/auth.php
+++ config/auth.php
@@ -84,6 +84,10 @@ return [
     | considered valid. This security feature keeps tokens short-lived so
     | they have less time to be guessed. You may change this as needed.
     |
+    | The throttle setting is the number of seconds a user must wait before
+    | generating more password reset tokens. This prevents the user from
+    | quickly generating a very large amount of password reset tokens.
+    |
     */
 
     'passwords' => [
