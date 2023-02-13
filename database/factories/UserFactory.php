0ffc40264bd47af6e5f9b7e92eddde873599d907
zobay <aszubayr@gmail.com>
2023-02-13 19:24:58 +0600

    Fix style ci issue

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
