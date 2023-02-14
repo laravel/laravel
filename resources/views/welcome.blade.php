6285eea340ae2756213f2d879f30fcc9cc8110bb
miladev-ent <milwad.dev@gmail.com>
2023-02-14 20:09:25 +0330

    add favicon to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>Laravel</title>
 
+        <!-- Icons -->
+        <link rel="shortcut icon" href="favicon.ico">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
