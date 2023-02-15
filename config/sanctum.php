e203e18b662717143ba3c997970edcbfe869eedf
Eimantas Likas <eimantas.likas@gmail.com>
2023-02-15 18:08:53 +0200

    Add missing trailing comma to config

--- config/sanctum.php
+++ config/sanctum.php
@@ -18,7 +18,7 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        Sanctum::currentApplicationUrlWithPort(),
     ))),
 
     /*
