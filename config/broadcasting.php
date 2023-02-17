11ea9727bbae6bcfd703b572b4946ce2626e1f13
Azvya Erstevan I <erstevn@gmail.com>
2023-02-17 15:40:12 +0700

    Missing cluster option in broadcasting.php
    Cluster option now is mandatory in pusher-js ^8.0 pusher/pusher-js#655

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,6 +41,7 @@ return [
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'cluster' => env('PUSHER_APP_CLUSTER', '')
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
