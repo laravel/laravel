11c26bb6984ab043af7eaa0509b5f1367aa1aebe
nejcc <nejc.cotic@gmail.com>
2023-02-18 14:18:05 +0100

    improving the performance and maintainability of the codebase
    reduces the likelihood of errors related to duplicate declarations and avoids unnecessary file inclusions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,6 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        require_once base_path('routes/console.php');
     }
 }
