4805bd166836418f2fe58fa002226ac14b6ab1aa
nejcc <nejc.cotic@gmail.com>
2023-02-18 17:58:55 +0100

    update test required with once

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -12,7 +12,7 @@ trait CreatesApplication
      */
     public function createApplication(): Application
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require_once __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
