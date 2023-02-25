efeec4ceb4eb8d4ede0aa6f8745bb3038abd4913
Ismael Seck <68471659+mrseck@users.noreply.github.com>
2023-02-25 19:25:22 +0100

    laravel new project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Test Simplon</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
