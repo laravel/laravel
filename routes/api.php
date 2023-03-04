919bbf3482ae9c48fa9df71bddefa9799c1a4ffe
dhtmdgkr123 <osh12201@gmail.com>
2023-03-04 17:15:01 +0900

    Fix import sequence

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
