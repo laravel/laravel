9615601ca2737c1c36af7035c0199686055a1a72
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2023-04-12 18:56:56 +0530

    Remove commented namespace

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
