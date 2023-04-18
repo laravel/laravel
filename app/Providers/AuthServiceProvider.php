c9ce5c8350ae20492944f87039025d9d9c4d1729
Taylor Otwell <taylor@laravel.com>
2023-04-18 15:56:39 -0500

    policies property is not needed with auto discovery

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,15 +8,6 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
-
-    /**
      * Register any authentication / authorization services.
      */
     public function boot(): void
