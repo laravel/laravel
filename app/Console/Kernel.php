9010aeb6e0a86358536b38bdd24467a447d6a4a1
Taylor Otwell <taylor@laravel.com>
2023-04-21 15:09:18 -0500

    no longer necessary to load commands manually

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,8 +20,6 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
