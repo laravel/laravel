34ab5558dbf2c422043b21743afebb42f9c26601
Zep Fietje <hey@zepfietje.com>
2023-04-28 13:03:14 +0200

    Clean up user model traits

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
