1f19b653f0052292ea34375d57dd9a43da31cbee
Jesper Noordsij <jesper@sslleiden.nl>
2023-05-01 16:26:52 +0200

    Use static middleware methods in api routes file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(Authenticate::using('sanctum'))->get('/user', function (Request $request) {
     return $request->user();
 });
