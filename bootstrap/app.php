04fb0190928b04d99d42a2b3580375881312592d
Taylor Otwell <taylor@laravel.com>
2023-05-05 16:06:39 -0500

    fix comment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Application;
 |
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
+| the service container that can resolve all classes and components.
 |
 */
 
