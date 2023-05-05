150e379ce2f2af2205ce87839565acb8ac6ace2e
Saya <379924+chu121su12@users.noreply.github.com>
2023-05-06 01:42:51 +0800

    Update mail.php (#6170)

--- config/mail.php
+++ config/mail.php
@@ -36,6 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
+            'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
