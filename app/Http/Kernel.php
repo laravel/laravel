e13a4d09d572e312441317aaa4c735fcf0dd4b16
Taylor Otwell <taylor@laravel.com>
2023-05-10 16:39:56 -0500

    remove csrf middleware from skeleton

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\ValidateCsrfToken::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
