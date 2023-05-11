4c85d2215dca232287d76fc5aa4db1194219ae0c
Taylor Otwell <taylor@laravel.com>
2023-05-11 15:00:39 -0500

    use database by default for failed jobs - ship combined job migration

--- config/queue.php
+++ config/queue.php
@@ -103,7 +103,7 @@ return [
     */
 
     'failed' => [
-        'driver' => env('QUEUE_FAILED_DRIVER', 'file'),
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'database' => env('DB_CONNECTION', 'mysql'),
         'table' => 'failed_jobs',
     ],
