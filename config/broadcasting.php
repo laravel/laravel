1892bf156fc18b66c2f48bcb3ef7b30e9615d05b
Jesse Leite <jesseleite@gmail.com>
2023-05-12 13:46:10 -0400

    Bring back cluster config option, as required by pusher-js v8.0.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,6 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
