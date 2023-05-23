a6bfbc7f90e33fd6cae3cb23f106c9689858c3b5
Taylor Otwell <taylor@laravel.com>
2023-05-23 16:45:40 -0500

    add lock path

--- config/cache.php
+++ config/cache.php
@@ -52,6 +52,7 @@ return [
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
+            'lock_path' => storage_path('framework/cache/data'),
         ],
 
         'memcached' => [
