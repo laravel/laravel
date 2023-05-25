7b016a9edb5eda776bdfaf5b7f0dec8d0c5a9d73
Taylor Otwell <taylor@laravel.com>
2023-05-25 15:39:45 -0500

    mail config comments

--- config/mail.php
+++ config/mail.php
@@ -93,7 +93,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | You may wish for all emails sent by your application to be sent from
-    | the same address. Here, you may specify a name and address that is
+    | the same address. Here you may specify a name and address that is
     | used globally for all emails that are sent by your application.
     |
     */
