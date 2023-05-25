a597f64ccbdef56e6f116db2e21ebcadd7f77ea6
Taylor Otwell <taylor@laravel.com>
2023-05-25 15:35:09 -0500

    filesystem comments

--- config/filesystems.php
+++ config/filesystems.php
@@ -9,7 +9,7 @@ return [
     |
     | Here you may specify the default filesystem disk that should be used
     | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application. Just store away!
+    | based disks are available to your application for file storage.
     |
     */
 
