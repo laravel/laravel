3e2fb9f74e947332609f3edbde65243b07f747b8
Taylor Otwell <taylor@laravel.com>
2023-05-25 15:34:32 -0500

    database file comments

--- config/database.php
+++ config/database.php
@@ -11,7 +11,7 @@ return [
     |
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
+    | you may use many connections at once throughout the application.
     |
     */
 
@@ -24,12 +24,7 @@ return [
     |
     | Here are each of the database connections setup for your application.
     | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
+    | supported by Laravel is shown below to assist your development.
     |
     */
 
