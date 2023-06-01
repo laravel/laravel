bccf155c177e2cd2420b196e0eb3c4db30f85d61
Viktor Szépe <viktor@szepe.net>
2023-06-01 10:48:58 +0200

    Fix HTML encoding in welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&amp;display=swap" rel="stylesheet" />
 
         <!-- Styles -->
         <style>
