0e9e7401dbfc49ab7479bc8f1327bba9fdbf30ba
Miguel Piedrafita <github@miguel.build>
2023-06-02 02:16:17 +0200

    fix typo in app.php

--- config/app.php
+++ config/app.php
@@ -116,7 +116,7 @@ return [
     | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | This key is utilized by Laravel's encryption services and shold be set
+    | This key is utilized by Laravel's encryption services and should be set
     | to a random, 32 character string, otherwise these encrypted strings
     | will not be safe. Please do this before deploying an application.
     |
