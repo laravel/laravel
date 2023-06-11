456a77e5eba9f2e391654654749a62c4ef46f547
Taylor Otwell <taylor@laravel.com>
2023-06-11 17:48:19 -0500

    fix env var names for cache and broadcast

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => env('CACHE_STORE', 'file'),
 
     /*
     |--------------------------------------------------------------------------
