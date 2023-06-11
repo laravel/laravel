fd5ffff3f95c061ba54c8d9f48da885eb2d54a06
Taylor Otwell <taylor@laravel.com>
2023-06-11 17:50:34 -0500

    let default queue driver be database

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'database'),
 
     /*
     |--------------------------------------------------------------------------
