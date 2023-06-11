f1d667aaa0c0429606cef5795e750370325cb699
Taylor Otwell <taylor@laravel.com>
2023-06-11 17:48:19 -0500

    fix env var names for cache and broadcast

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_CONNECTION', 'null'),
 
     /*
     |--------------------------------------------------------------------------
