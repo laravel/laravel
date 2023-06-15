2e6477b518c3e4cff604ad80429c774ba33c9f77
Steve McDougall <JustSteveKing@users.noreply.github.com>
2023-06-15 18:45:47 +0100

    Update Controller.php
    Making the default controller `abstract` as it is only ever extended not used directly

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 }
