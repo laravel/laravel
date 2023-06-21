626e1e636c901381694f3419103f2119c9f8e360
Taylor Otwell <taylor@laravel.com>
2023-06-21 14:34:58 +0200

    slim providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 */
 
 return Application::configure()
+    ->withProviders([
+        AppServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
