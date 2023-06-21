a28b176809f2ce39bf7d474affc5e98aa1483a74
Taylor Otwell <taylor@laravel.com>
2023-06-21 14:34:58 +0200

    slim providers

--- config/app.php
+++ config/app.php
@@ -1,27 +1,5 @@
 <?php
 
-use Illuminate\Support\ServiceProvider;
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on any
-    | requests to your application. You may add your own services to the
-    | arrays below to provide additional features to this application.
-    |
-    */
-
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        // Package Service Providers...
-    ])->merge([
-        // Application Service Providers...
-        App\Providers\AppServiceProvider::class,
-    ])->merge([
-        // Added Service Providers (Do not remove this line)...
-    ])->toArray(),
-
+    //
 ];
