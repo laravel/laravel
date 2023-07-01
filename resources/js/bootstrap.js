591eb856396af44f824e92dcd3e64ee1caff0952
Taylor Otwell <taylor@laravel.com>
2023-06-30 20:12:27 -0500

    adjust comment

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,6 @@
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
+ * to our Laravel back end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
@@ -12,7 +12,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
+ * allow your team to quickly build robust real-time web applications.
  */
 
 // import './echo';
