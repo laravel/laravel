8e5a2eeaf92d63d9dec6d4f0185826950a0322dd
Taylor Otwell <taylor@laravel.com>
2023-06-30 22:02:02 -0500

    remove has api tokens trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
