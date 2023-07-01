db8478fd63db47995d720edf31add53bbcbda0be
Taylor Otwell <taylor@laravel.com>
2023-06-30 19:18:47 -0500

    remove api routes file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure()
     ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
     )
