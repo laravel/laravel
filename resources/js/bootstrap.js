16f723c91ee35baaf2199132f43bdf9862da8295
Taylor Otwell <taylor@laravel.com>
2023-07-07 17:12:30 +0200

    echo file is not needed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,11 +8,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
-// import './echo';
