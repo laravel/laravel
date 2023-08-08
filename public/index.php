bd39eacfef4ee020ca23a4064e69306eb035af48
José Orlando Felicio Parreira <69628135+joseorlandofp@users.noreply.github.com>
2023-08-08 10:28:07 -0300

    Fix: typo on comment

--- public/index.php
+++ public/index.php
@@ -40,7 +40,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 | Once we have the application, we can handle the incoming request using
 | the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| to the client's browser, allowing them to enjoy our application.
 |
 */
 
