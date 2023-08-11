ce6010f30228c563165930c5c528b61d9dc001df
Mirmuxsin Khamroev <58307677+Mirmuxsin@users.noreply.github.com>
2023-08-11 10:21:01 +0500

    Update channels.php
    Fixed grammar:
    
    Consider using all the.
    Incorrect:  
    She is going to all of the parks. 
    Correct:  
    She is going to all the parks. 

--- routes/channels.php
+++ routes/channels.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
-| Here you may register all of the event broadcasting channels that your
+| Here you may register all the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
