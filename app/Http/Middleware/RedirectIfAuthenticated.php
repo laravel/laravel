b815bb6c4c26f5329ffd54575aad8bbab31cd913
Alberto Favaro <alberto.favaro.vr@gmail.com>
2023-08-22 09:10:04 +0200

    Update RedirectIfAuthenticated middleware for API support
    Added check on the response expectation of the request in RedirectIfAuthenticated middleware for work correctly in the context of a Laravel Breeze application scaffolded with api option  or in general in the context of a client-driven authentication where Laravel act as a pure Backend.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,6 +21,9 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
+                if ($request->expectsJson()) {
+                    return response()->json(['message' => 'Already authenticated.']);
+                }
                 return redirect(RouteServiceProvider::HOME);
             }
         }
