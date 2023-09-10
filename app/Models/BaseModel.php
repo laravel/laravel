4364f9de9da69df823bef7a8cc543b1e896256a4
Ahmed Almory <40957053+ahmedalmory@users.noreply.github.com>
2023-09-10 15:15:53 +0300

    Create BaseModel.php for common functionality

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    // Common functionality or attributes can be added here
+    use HasFactory;
+}
