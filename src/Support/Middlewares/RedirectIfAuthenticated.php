d9f6cdf2ba77a776c6ce87842a267dd72b14e155
Sergio Ojeda <soj3da@gmail.com>
2023-09-13 14:24:20 -0300

    style: fix formatting in RedirectIfAuthenticated middleware

--- src/Support/Middlewares/RedirectIfAuthenticated.php
+++ src/Support/Middlewares/RedirectIfAuthenticated.php
@@ -15,7 +15,7 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
