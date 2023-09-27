ffbbeeb65c5f93a1f9111f6efd932e0d0b702116
Pranay Aryal <drpranayaryal@gmail.com>
2023-09-26 22:06:47 -0400

    cors.php add 'login', 'logout' to paths

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
 
     'allowed_methods' => ['*'],
 
