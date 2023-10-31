40080dcc101d25a70bcdcbe3aee4df44446b2322
Adaibekov Darkhan <78083548+dak0k@users.noreply.github.com>
2023-10-31 16:59:32 +0600

    Added the type of the $policies property to array

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array<class-string, class-string>
      */
-    protected $policies = [
+    protected array $policies = [
         //
     ];
 
