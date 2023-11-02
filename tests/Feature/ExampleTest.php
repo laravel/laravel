d593734c4e9ce4c3b5251a5c270c65924edfba83
Steven Wichers <me@stevenwichers.com>
2023-11-01 22:26:19 -0700

    Add test for users table
    Use Schema to check for table

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
@@ -15,5 +17,9 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_database():void {
+        $this->assertTrue(Schema::hasTable('users'));
     }
 }
