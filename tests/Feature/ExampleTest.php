3d46ce37f839497f757081e8f16b4df54eff321a
Sergio Ojeda <soj3da@gmail.com>
2023-11-03 21:53:32 -0300

    fix: test_the_application_returns_a_successful_response

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        $this->withoutVite();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
