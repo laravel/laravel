1998cf19955faf3015ab6efb3fea45ed39d4affe
nejcc <nejc.cotic@gmail.com>
2023-11-26 10:59:57 +0100

    update the AppServiceProvider boot for older version of mysql defaultStringLength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+//use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
