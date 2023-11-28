93496b7edb71f8cf7eec9fe90267bd5c518ed7ac
Sérgio Nunes <ssn@fe.up.pt>
2023-11-28 10:02:00 +0000

    add support for DB_SCHEMA environment variable for PostgreSQL databases

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => env('DB_SCHEMA', 'public'),
             'sslmode' => 'prefer',
         ],
 
