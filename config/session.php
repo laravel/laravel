e27c0c1f8479db7f212c02aeeae924ec85aacdb7
Taylor Otwell <taylor@laravel.com>
2023-11-30 16:35:17 -0600

    formatting

--- config/session.php
+++ config/session.php
@@ -203,11 +203,12 @@ return [
     | Partitioned Cookies
     |--------------------------------------------------------------------------
     |
-    | Setting this value to true will tie the cookie to the top-level site in
+    | Setting this value to true will tie the cookie to the top-level site for
     | a cross-site context. Partitioned cookies are accepted by the browser
     | when flagged "secure" and the Same-Site attribute is set to "none".
     |
     */
 
     'partitioned' => false,
+
 ];
