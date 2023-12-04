5490827743d4234ffcfef4e89309791069adcb0d
Hugo Henrique <mxhugoxm@gmail.com>
2023-12-04 15:37:25 -0300

    feat: Set as Default InnoDB Storage Engine

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
+            'prefix' => 'InnoDB',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
