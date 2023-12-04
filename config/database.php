f839a22e61751470b6d56132011f3588ac7d98c0
Hugo Henrique <mxhugoxm@gmail.com>
2023-12-04 15:39:59 -0300

    feat: Set as Default InnoDB Storage Engine

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => 'InnoDB',
+            'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
