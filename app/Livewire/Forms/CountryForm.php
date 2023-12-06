fe7f7b3ad0fcfaca4c26f8ef8b1d2142ca13a9ee
prakash <prakash.paramanandam@arkinfotec.com>
2023-12-06 10:40:20 +0530

    Livewire initial commit

--- app/Livewire/Forms/CountryForm.php
+++ app/Livewire/Forms/CountryForm.php
@@ -2,13 +2,21 @@
 
 namespace App\Livewire\Forms;
 
+use App\Models\Country;
 use Livewire\Attributes\Validate;
+use Illuminate\Validation\Rule as ValidationRule;
 
 class CountryForm extends BaseFormComponent
 {
-    #[Validate('required|max:3')]
     public string|null $name;
 
-    #[Validate('required')]
     public string|null $short_code;
+
+    public function rules()
+    {
+        return [
+            'name' => "required|max:100|string|unique:countries,name,{$this->model->id},id",
+            'short_code' => 'required'
+        ];
+    }
 }
