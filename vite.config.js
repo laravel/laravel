8ce9714ccf672ee40024fc3a47434e760acb40bd
prakash <prakash.paramanandam@arkinfotec.com>
2023-12-07 16:34:23 +0530

    Pulse integration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/scss/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
