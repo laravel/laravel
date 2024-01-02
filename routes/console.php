12932d7594a39960d9afe5e6fe245a68c88badea
Nuno Maduro <enunomaduro@gmail.com>
2024-01-02 12:24:23 +0000

    Simplifies console routes

--- routes/console.php
+++ routes/console.php
@@ -17,17 +17,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-/*
-|--------------------------------------------------------------------------
-| Console Schedule
-|--------------------------------------------------------------------------
-|
-| Below you may define your scheduled tasks, including console commands
-| or system commands. These tasks will be run automatically when due
-| using Laravel's built-in "schedule:run" Artisan console command.
-|
-*/
-
-Schedule::command('inspire')->hourly();
+})->purpose('Display an inspiring quote')->hourly();
