dd60315e9ad725c99d883a668f35cfaa6a1aa8e3
Nuno Maduro <enunomaduro@gmail.com>
2024-01-11 20:32:05 +0000

    [11.x] Simplify PHP comments (#6316)
    * Simplifies comments
    
    * Apply fixes from StyleCI
    
    * Removes non used line
    
    * remove some comments
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded within the "web" middleware group which includes
-| sessions, cookie encryption, and more. Go build something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
