e4c6d4da3dbe5a2854815adf67b8a251d1602508
Nuno Maduro <enunomaduro@gmail.com>
2024-01-11 17:28:00 +0000

    Simplifies comments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the brain for all of the Laravel components. We will
-| also use the application to configure core, foundational behavior.
-|
-*/
+/**
+ * The first thing we will do is create a new Laravel application instance
+ * which serves as the brain for all of the Laravel components. We will
+ * also use the application to configure core, foundational behavior.
+ */
 
 return Application::configure()
     ->withProviders()
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded within the "web" middleware group which includes
-| sessions, cookie encryption, and more. Go build something great!
-|
-*/
+/**
+ * Here is where you can register web routes for your application. These
+ * routes are loaded within the "web" middleware group which includes
+ * sessions, cookie encryption, and more. Go build something great!
+ */
 
 Route::get('/', function () {
     return view('welcome');
