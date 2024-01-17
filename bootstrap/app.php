b1502f3c0732a5e30da71c0ee73e769138eb6a8a
Taylor Otwell <taylor@laravel.com>
2024-01-17 16:44:19 -0600

    slim comments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-/**
- * The first thing we will do is create a new Laravel application instance
- * which serves as the brain for all of the Laravel components. We will
- * also use the application to configure core, foundational behavior.
- */
-
 return Application::configure()
     ->withProviders()
     ->withRouting(
