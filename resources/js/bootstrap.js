86c7c863d6b6e7c4de1365b84e178e08e4ead86e
Taylor Otwell <taylor@laravel.com>
2024-01-24 12:57:48 -0600

    slim bootstrap file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,3 @@
-/**
- * The axios HTTP library is used by a variety of first-party Laravel packages
- * like Inertia in order to make requests to the Laravel backend. This will
- * automatically handle sending the CSRF token via a header based on the
- * value of the "XSRF" token cookie sent with previous HTTP responses.
- */
-
 import axios from 'axios';
 window.axios = axios;
 
