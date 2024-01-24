7df47a2604543788e2aeb8d6be3c05dbd8e26a0e
Taylor Otwell <taylor@laravel.com>
2024-01-24 15:15:09 -0600

    simple health route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         // channels: __DIR__.'/../routes/channels.php',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
