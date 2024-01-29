513cfa4cc1a59cdac9edcee69e3492f9062b8c50
Algo-E Tech Company <136216514+algoetech@users.noreply.github.com>
2024-01-29 00:22:24 -1200

    my first contribution to Gee
    returning user name from controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function user() {
+        return Auth::user();
+    }
 }
