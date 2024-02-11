d9ef2483eb25913e8dc8a77148adc95c9879c676
Jamshed Javed <jamshady@gmail.com>
2024-02-11 19:41:56 +0000

    Vite config uses APP_URL (via .env file) to define hostname for HMR
    This allows proper operation within containers, etc

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    // Load env file based on `mode` (dev, serve or build) in the current working directory.
+    // Set the 3rd parameter to '' to load all env variables regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '')
+    const host = env.APP_URL.match(/\/\/([\w.]+)/)[1]
+
+    return {
+        server: {
+            hmr: {
+                host,
+            },
+        },
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    }
 });
