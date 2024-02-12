ba35cdff939fd730a3e0493ab7a749eaabfc99cd
Patrick Hesselberg <patrick.hesselberg@hotmail.com>
2024-02-12 13:40:50 +0100

    Use list instead of array<int, string>

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -25,7 +25,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
