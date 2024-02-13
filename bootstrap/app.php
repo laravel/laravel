6a2b373d26fe5d4b7ab95d728924a025721283af
Nuno Maduro <enunomaduro@gmail.com>
2024-02-13 17:10:41 +0000

    Moves `withProviders` to configure internally

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
