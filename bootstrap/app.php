151eac5c1aaa37071c152fcb1f72ed3c38b6db15
Taylor Otwell <taylor@laravel.com>
2024-02-14 11:46:45 -0600

    remove comments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
