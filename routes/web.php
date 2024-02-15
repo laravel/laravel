276e7913ecd666d33fe61a51783f66403369392a
Jason Beggs <jason@roasted.dev>
2024-02-15 13:45:28 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+})->name('login');
+Route::get('/register', function () {
+})->name('register');
