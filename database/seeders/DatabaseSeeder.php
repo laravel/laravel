08fa336739d215b49d56a825df934e4bebab58f3
ArielMejiaDev <arielmejiadev@gmail.com>
2024-02-16 23:44:39 -0600

    propose database seeder change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // $this->call([
+            //
+        //]);
     }
 }
