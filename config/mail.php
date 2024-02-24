edd96e295299b5447d45ef36dd532edb0bb01ffa
Nuno Maduro <enunomaduro@gmail.com>
2024-02-24 16:41:17 +0000

    Uses `env` on postmark email commented code as docs

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 
         'postmark' => [
             'transport' => 'postmark',
-            // 'message_stream_id' => null,
+            // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
             // 'client' => [
             //     'timeout' => 5,
             // ],
