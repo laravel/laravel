a3184e265d306ceafea30890f7f4f7b4651e9649
Jason Beggs <jason@roasted.dev>
2024-02-27 13:35:58 -0500

    Update Home to Dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,10 +27,10 @@
                         <nav class="-mx-3 flex flex-1 justify-end">
                             @auth
                                 <a
-                                    href="{{ url('/home') }}"
+                                    href="{{ url('/dashboard') }}"
                                     class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                 >
-                                    Home
+                                    Dashboard
                                 </a>
                             @else
                                 <a
