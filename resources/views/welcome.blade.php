f0174f18a65592545bd1b0eccbe43974a65a2014
Thiago Leite <49279938+t-leite@users.noreply.github.com>
2024-03-20 16:11:28 -0300

    test new branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel - ABS</title>
+        <title>Laravel - ABS LATAM</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
