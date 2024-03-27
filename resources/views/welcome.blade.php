18694572c895d61af140ab8bf9447ea44e9ed2b1
MHO <yannick@chillpills.io>
2024-03-27 14:37:17 +0100

    Add Vite Blade Directive in Welcome Blade view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
