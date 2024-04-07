ce4cfb4ff3b58e3d12c381ee3aa3c01ef66255f6
devman1917 <92359289+devman1917@users.noreply.github.com>
2024-04-07 17:02:02 +0330

    Update UserFactory.php
    improve syntax

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,11 +12,6 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -27,7 +22,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' =>  once(fn() => Hash::make('password')),
             'remember_token' => Str::random(10),
         ];
     }
