4b1588713d05830f7cdf88159fa3739831d167cb
Taylor Otwell <taylor@laravel.com>
2024-05-03 12:16:26 -0500

    add resend

--- config/services.php
+++ config/services.php
@@ -24,6 +24,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'resend' => [
+        'key' => env('RESEND_KEY'),
+    ],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
