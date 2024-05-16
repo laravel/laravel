b651fb109c3e3a58e695cc0c0f332f370e648306
Ricardo Čerljenko <ricardo@lloyds-digital.com>
2024-05-16 23:36:21 +0200

    updated mail config (#6402)

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
         'ses' => [
