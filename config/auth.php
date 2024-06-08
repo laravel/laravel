e347e88ddcb3f171fcdace9934727780a622b493
Mahmuod-Eldeep <159500091+Mahmuod-Eldeep@users.noreply.github.com>
2024-06-08 18:31:48 +0300

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -7,7 +7,7 @@ return [
     | Authentication Defaults
     |--------------------------------------------------------------------------
     |
-    | This option defines the default authentication "guard" and password
+    |  This option defines the default authentication "guard" and password
     | reset "broker" for your application. You may change these values
     | as required, but they're a perfect start for most applications.
     |
