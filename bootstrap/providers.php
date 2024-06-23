29e6671ed2a248af2451f44d6a48e5a35a643d18
Nguyen Dac Hai <nguyendmim@gmail.com>
2024-06-23 10:58:28 +0700

    Added explanatory comments to providers.php and console.php for clarity

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ *  This configuration file returns an array of service providers to be registered
+ */
 return [
     App\Providers\AppServiceProvider::class,
 ];
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+// Use statements to include necessary classes and facades.
+// 'Inspiring' provides a collection of inspiring quotes.
+// 'Artisan' is the facade for the Artisan command-line interface.
+
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Define a new Artisan command using the Artisan::command method.
+// This command is named 'inspire' and when executed, it will display
+// an inspiring quote to the console.
+
 Artisan::command('inspire', function () {
+    // Output an inspiring quote to the console.
+    // The $this->comment method is used to display the quote in the console.
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
