186378de683f6fb8a7189b9a863df4475460c22f
Ketan Modi <codewithbhaveshdev@gmail.com>
2024-09-03 12:10:11 +0530

    Adding some comments for service provider
    - Adding return type in docblock
    - Added comments which can guide how to bind a service to the container and how to register routes or event listeners

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,17 +8,25 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     * @return void
      */
     public function register(): void
     {
-        //
+        // Example of binding a service to the container
+        // $this->app->bind(SomeInterface::class, SomeConcreteClass::class);
+        
+        // Register additional services here
     }
 
     /**
      * Bootstrap any application services.
+     * @return void
      */
     public function boot(): void
     {
-        //
+        // Example of registering routes or event listeners
+        // $this->app->router->group([], function ($router) {
+        //     require base_path('routes/web.php');
+        // });
     }
 }
