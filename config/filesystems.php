bab16982dd64e3f58bf2242d5ad0585f9a331e01
Taylor Otwell <taylor@laravel.com>
2024-09-11 15:12:40 -0500

    private files (#6450)

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,8 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/private'),
+            'serve' => true,
             'throw' => false,
         ],
 
