164aa7d6ea750dcda882c197cd499512cc11da1e
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-18 20:35:26 +0530

    Refactor User model to use HasFactory trait and add type hint for UserFactory

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
