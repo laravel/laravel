49bceac41ff34dd6df12760041ead73f8888dc6c
Dominik Koch <dominik@koch-bautechnik.de>
2024-10-08 15:34:41 +0200

    Update welcome.blade.php to add missing alt tag (#6462)
    Every image should have an alt tag for accessibility reasons, but this one did not, so this commit aims to add the missing one.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
+            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
