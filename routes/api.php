bd3f7472f5618e2859fe7bf3df581a52f1282b9a
bforath <brett.forath@proton.me>
2024-10-09 10:43:48 -0700

    Add api routing with versioning.

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('v1')->group(base_path('routes/api_v1.php'));
+
---
+++ routes/api_v1.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\V1\BookController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('nyt/best-sellers/',[
+    BookController::class, 'search'
+]);
