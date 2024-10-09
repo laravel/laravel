d1ef37a8b4f639e63522bff87a0545c032fa09d9
Taylor Otwell <taylor@laravel.com>
2024-10-09 15:46:01 -0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
