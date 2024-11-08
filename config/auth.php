9141f9e6d569ef807118120efd7fc7d42cb93790
Andrew Brown <browner12@gmail.com>
2024-11-08 10:22:55 -0600

    make password resets use the "cache driver" by default
    - drop the `password_reset_tokens` migration.
    - update the auth config to use the 'cache' driver by default

--- config/auth.php
+++ config/auth.php
@@ -92,8 +92,8 @@ return [
 
     'passwords' => [
         'users' => [
+            'driver' => 'cache',
             'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,12 +21,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -43,7 +37,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
