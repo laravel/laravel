7564259a0d932c2fe90618564dafdeb47e44816f
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2024-11-16 19:15:17 +0530

    Enhance database configuration with additional connections for better compatibility
    Enhance database configuration with additional connections for better compatibility

--- config/database.php
+++ config/database.php
@@ -42,7 +42,28 @@ return [
             'synchronous' => null,
         ],
 
-        'mysql' => [
+      'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+
+    'mysql-old' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
