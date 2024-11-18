a92bff14e303c7f145102a3a2a1cfe534a34eeff
Andrew Brown <browner12@gmail.com>
2024-11-17 21:51:53 -0600

    match `HidesAttributes` docblocks
    the docblock in `HidesAttributes` was updated in #42512, so this child class should be using the same.
    
    otherwise PHPStan throws a "PHPDoc type array<int, string> of property App\Models\User::$hidden is not covariant with PHPDoc type list<string> of  property Illuminate\Database\Eloquent\Model::$hidden" error

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array<string>
      */
     protected $hidden = [
         'password',
