2eacb3d0f08a401ff70107c7a617e27478fb576d
Taylor Otwell <taylor@laravel.com>
2024-11-18 08:18:44 -0600

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
