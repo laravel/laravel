5003810eddfc3a69a0c951beda50378f9fb5e355
Pavel <DvDty@users.noreply.github.com>
2024-11-21 11:50:58 +0200

    Narrow down array types

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -26,7 +26,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
