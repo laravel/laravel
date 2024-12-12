72a3c775aaf29f30387599bd4be6843a3fe3a224
Chetan Patel <87816669+ChetanTechnource@users.noreply.github.com>
2024-12-12 09:39:41 +0530

    [11.x] Storage:link source and target directory statement updated
    Updated key values source and target directory statement for symlinks array

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,8 +66,8 @@ return [
     |
     | Here you may configure the symbolic links that will be created when the
     | `storage:link` Artisan command is executed. The array keys should be
-    | the locations of the links and the values should be their targets.
-    |
+    | the target directories of the symbolic links and the values should be 
+    | the source directories.
     */
 
     'links' => [
