eb8085cf77bc5165d1af0b90bd9cdfb406d65299
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-13 21:57:40 +0800

    [11.x] Update `config/mail.php` with supported configuration (#6506)
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/mail.php
+++ config/mail.php
@@ -39,10 +39,10 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
+            'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
