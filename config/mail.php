9d149ffedb2437cc1648ae5b98f727ef6f4993be
Khaled Huthaily <khaledenglish@yahoo.com>
2024-12-16 16:59:20 -0700

    set default scheme for smtp mailer
    This PR addresses an issue where the smtp mailer fails with the message `The "" scheme is not supported; supported schemes for mailer "smtp" are: "smtp", "smtps".`

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,7 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => env('MAIL_SCHEME', 'smtp'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
