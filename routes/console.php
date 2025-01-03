a825c7799ec9f86cc3fa450e045145c8afb2c20d
Peter Temi <info@diadal.com.ng>
2025-01-03 15:57:32 +0100

    Update console.php
    Undefined variable: $this by `PHPStan`

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,6 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+Artisan::command('inspire', function () use ($command) {
+    $command->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
