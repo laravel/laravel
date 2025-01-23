8a104a53147de18afcc91feffc6462a0ca905f70
Ngo Quoc Dat <datlechin@gmail.com>
2025-01-23 09:18:37 +0700

    Upgrade to Tailwind CSS v4.0

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
