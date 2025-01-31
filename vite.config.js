c6dcaf788c63387f6086cae6cba974f2b89e43a9
Ngô Quốc Đạt <datlechin@gmail.com>
2025-02-01 00:34:00 +0700

    Upgrade to Tailwind CSS v4.0 (#6523)
    * Upgrade to Tailwind CSS v4.0
    
    * Upgrade to Tailwind CSS 4
    
    * Upgrade to Tailwind CSS 4
    
    * Upgrade to Tailwind CSS 4
    
    * Upgrade to Tailwind CSS 4
    
    * Upgrade to Tailwind CSS v4.0
    
    * remove compatible styles
    
    Co-authored-by: Julius Kiekbusch <contact@julius-kiekbusch.de>
    
    * wip
    
    ---------
    
    Co-authored-by: Julius Kiekbusch <contact@julius-kiekbusch.de>

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
