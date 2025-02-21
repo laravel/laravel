cb8e7fdde5f50b4b7937bce4b2d412f8d5752886
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2025-02-21 19:44:09 +0000

    use base_path to reference route files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use function base_path;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: base_path('routes/web.php'),
+        commands: base_path('routes/console.php'),
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
