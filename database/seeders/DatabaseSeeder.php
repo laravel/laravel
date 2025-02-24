742ca8b2486181e72c1f25538bae84be4363d9cd
magadanuhak <32567230+magadanuhak@users.noreply.github.com>
2025-02-24 16:56:06 +0200

    Removed unnecessary comment DatabaseSeeder.php
    Removed unnecessary comment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
