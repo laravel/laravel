1eb3a2e3ec29e7042fca008987b25059b8e3c036
Anders Jenbo <anders@jenbo.dk>
2025-02-26 23:55:05 +0100

    feat: add type hints for $this in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+/** @var Illuminate\Foundation\Console\Kernel $this */
 Artisan::command('inspire', function () {
+    /** @var Illuminate\Foundation\Console\ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
