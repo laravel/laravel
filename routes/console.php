27348b7c9030388a2db7be57c3775fc9fd2a1ced
Taylor Otwell <taylor@laravel.com>
2025-03-04 16:07:15 -0600

    remove change for now

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
