139067230229d16805cf5bd865acb8a9b59ab6b9
Anders Jenbo <anders@jenbo.dk>
2025-03-04 16:48:56 +0100

    feat: add type hints for $this in routes/console.php (#6559)
    * feat: add type hints for $this in routes/console.php
    
    * Update console.php
    
    * Update console.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
+    /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
