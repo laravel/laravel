3f68c1c846fa39fd96db3f39ab47e05056022ac4
Anders Jenbo <anders@jenbo.dk>
2025-03-04 17:29:53 +0100

    Hint outer scope in console routes

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Foundation\Console\ClosureCommand;
+use Illuminate\Foundation\Console\Kernel;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+/** @var Kernel $this */
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
