72396caa1e333faa93d04008b319dc99868a2386
Taylor Otwell <taylor@laravel.com>
2025-03-04 09:44:47 -0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/** @var Illuminate\Foundation\Console\Kernel $this */
 Artisan::command('inspire', function () {
     /** @var Illuminate\Foundation\Console\ClosureCommand $this */
     $this->comment(Inspiring::quote());
