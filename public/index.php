4dfd37a15f8b07e265fcaf1751ef66f04c7e9ff7
Ahmed Alaa <ahmed.alaa4611@gmail.com>
2025-03-15 11:36:50 +0200

    Clear and concise implementation by removing unnecessary type annotation that do not provide significant benefits

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -14,7 +13,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
