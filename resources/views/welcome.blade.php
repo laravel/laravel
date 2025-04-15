64672cf2e79f95b28997ecb719b15b24b95806f0
kjsysadmin <kjhnstn23@gmail.com>
2025-04-15 16:27:12 -0500

    Updated title - should be to branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel - ARK-RX.com </title>
+        <title>Laravel - KJ</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
