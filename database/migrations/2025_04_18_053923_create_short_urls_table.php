892ce7a32514ae1abbb1834d2948a72e0eefce42
codeit88 <sami44007@gmail.com>
2025-04-18 10:45:43 +0500

    first commit

---
+++ database/migrations/2025_04_18_053923_create_short_urls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('short_urls', function (Blueprint $table) {
+            $table->id();
+            $table->text('url');
+            $table->string('short_code')->unique();
+            $table->unsignedBigInteger('access_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('short_urls');
+    }
+};
