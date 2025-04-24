4ad3b2cd07ee1949ab7d6bbdcf7f22a5d9e2a98f
Jack <jack@fusionspim.com>
2025-04-24 12:32:12 +0100

    Update cache.php

--- config/cache.php
+++ config/cache.php
@@ -43,7 +43,7 @@ return [
             'connection' => env('DB_CACHE_CONNECTION'),
             'table' => env('DB_CACHE_TABLE', 'cache'),
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
-            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
+            'lock_table' => env('DB_CACHE_LOCK_TABLE','cache_locks'),
         ],
 
         'file' => [
