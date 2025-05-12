2d8f562092a376cf80d07e3e4583bb8463bb51bf
Taylor Otwell <taylor@laravel.com>
2025-05-11 20:55:57 -0500

    remove apc

--- config/session.php
+++ config/session.php
@@ -13,8 +13,8 @@ return [
     | incoming requests. Laravel supports a variety of storage options to
     | persist session data. Database storage is a great default choice.
     |
-    | Supported: "file", "cookie", "database", "apc",
-    |            "memcached", "redis", "dynamodb", "array"
+    | Supported: "file", "cookie", "database", "memcached",
+    |            "redis", "dynamodb", "array"
     |
     */
 
