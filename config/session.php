4fe6e7bb2bfa65b4d984981937c53f99c0afe6f4
Ahmed Alaa <ahmed.alaa4611@gmail.com>
2025-05-15 09:50:11 +0300

    remove apc

--- config/session.php
+++ config/session.php
@@ -97,7 +97,7 @@ return [
     | define the cache store which should be used to store the session data
     | between requests. This must match one of your defined cache stores.
     |
-    | Affects: "apc", "dynamodb", "memcached", "redis"
+    | Affects: "dynamodb", "memcached", "redis"
     |
     */
 
