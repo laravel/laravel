9ec7e8a8bc6afea693cd89d4d30a961dc4d4e02e
Wogan May <225550+woganmay@users.noreply.github.com>
2025-05-19 18:21:08 +0200

    Minor language update
    PhpStorm's Natural Language module recommends this change to a countable noun, which happens to be exactly 6 characters long, so as to not affect the overall length of the line, while still making it ever-so-slightly more parseable.

--- config/auth.php
+++ config/auth.php
@@ -104,7 +104,7 @@ return [
     | Password Confirmation Timeout
     |--------------------------------------------------------------------------
     |
-    | Here you may define the amount of seconds before a password confirmation
+    | Here you may define the number of seconds before a password confirmation
     | window expires and users are asked to re-enter their password via the
     | confirmation screen. By default, the timeout lasts for three hours.
     |
