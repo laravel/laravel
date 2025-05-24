3593f90cff1e3cb98ad9c76796853574cdd74eb6
RzlAm <112256482+RzlAm@users.noreply.github.com>
2025-05-24 14:39:39 +0700

    style: reformat helper function

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,11 +1,11 @@
 <?php
-if (!function_exists('jdd')) {
+if (! function_exists('jdd')) {
 	/**
 	 * JSON Dump and Die.
 	 * Return a JSON response and immediately stops program execution.
 	 *
-	 * @param mixed $data The data to convert to JSON and send as response.
-	 * @param int|null $status HTTP status code (default is 200).
+	 * @param  mixed  $data  The data to convert to JSON and send as response.
+	 * @param  int|null  $status  HTTP status code (default is 200).
 	 * @return void
 	 */
 	function jdd($data, $status = 200): void
