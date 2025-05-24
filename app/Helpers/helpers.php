01dd583d27aefd7c9f896721df1cf7d6f9d19f14
RzlAm <112256482+RzlAm@users.noreply.github.com>
2025-05-24 13:41:52 +0700

    feat: add jdd helper

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,19 @@
+<?php
+if (!function_exists('jdd')) {
+	/**
+	 * JSON Dump and Die.
+	 * Return a JSON response and immediately stops program execution.
+	 *
+	 * @param mixed $data The data to convert to JSON and send as response.
+	 * @param int|null $status HTTP status code (default is 200).
+	 * @return void
+	 */
+	function jdd($data, $status = 200): void
+	{
+		while (ob_get_level()) {
+			ob_end_clean();
+		}
+		response()->json($data, $status)->send();
+		exit;
+	}
+}
