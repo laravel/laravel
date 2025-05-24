4cedec245c5c2318e02b982d86bfab1f71299c63
RzlAm <112256482+RzlAm@users.noreply.github.com>
2025-05-24 13:42:27 +0700

    feat: add jdd helper for blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('jdd', function ($expression) {
+            return "<?php jdd(...[$expression]); ?>";
+        });
     }
 }
