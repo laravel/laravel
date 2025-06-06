cd854afd59581e1a40b1023646467371b4bab98a
Khaled Huthaily <khaledenglish@yahoo.com>
2025-06-06 09:09:38 -0600

    fix alphabetical order (#6627)
    move `resend` before `ses`

--- config/services.php
+++ config/services.php
@@ -18,14 +18,14 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'resend' => [
+        'key' => env('RESEND_KEY'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
-    'resend' => [
-        'key' => env('RESEND_KEY'),
     ],
 
     'slack' => [
