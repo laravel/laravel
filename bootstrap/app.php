5afdc614afe3de017c1ace8a4cefad4c865dab88
Aluisio Pires <aluisiopireseng@gmail.com>
2025-06-06 17:02:16 -0300

    [12.x] Fix: Add void return type to withMiddleware and withExceptions to satisfy Rector analysis

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
