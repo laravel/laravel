b491017516cdfdf02ed48989fc835c1bf215ecab
Jonathan Goode <u01jmg3@gmail.com>
2025-06-25 12:05:30 +0100

    Use `Str::snake` as the separator in use is an underscore

--- config/database.php
+++ config/database.php
@@ -147,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::snake(env('APP_NAME', 'laravel')).'_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
