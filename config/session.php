dd473eaddc824aeada037f0dc702a3b68a51946c
Taylor Otwell <taylor@laravel.com>
2025-07-02 18:42:49 -0400

    fix

--- config/session.php
+++ config/session.php
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel')).'-session'
+        Str::snake((string) env('APP_NAME', 'laravel')).'_session'
     ),
 
     /*
