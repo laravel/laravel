4d1266f9ca63356241397d5567079adecc4857b4
Ahmed Alaa <92916738+AhmedAlaa4611@users.noreply.github.com>
2025-08-15 12:21:46 +0300

    Use application name from configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
