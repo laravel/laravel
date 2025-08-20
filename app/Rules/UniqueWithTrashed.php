85fc8e8d2b32fa030bed86ff756d468f3ad757ca
yaminlam <yamin@appinionbd.com>
2025-08-20 15:13:31 +0600

    Add UniqueWithTrashed validation rule

--- app/Rules/UniqueWithTrashed.php
+++ app/Rules/UniqueWithTrashed.php
@@ -11,11 +11,7 @@ class UniqueWithTrashed implements ValidationRule
     protected string $column;
     protected ?int $ignoreId;
 
-    /**
-     * @param string $modelClass Eloquent model class
-     * @param string $column Column name to check uniqueness
-     * @param int|null $ignoreId ID to ignore (useful for updates)
-     */
+
     public function __construct(string $modelClass, string $column = 'name', ?int $ignoreId = null)
     {
         $this->modelClass = $modelClass;
@@ -23,13 +19,6 @@ class UniqueWithTrashed implements ValidationRule
         $this->ignoreId = $ignoreId;
     }
 
-    /**
-     * Run the validation rule.
-     *
-     * @param  string  $attribute
-     * @param  mixed   $value
-     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $query = $this->modelClass::withTrashed()
