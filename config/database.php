5b585a22783d9d528bf6d0c7160bb7ad11fbfba7
Taylor Otwell <taylor@laravel.com>
2025-08-26 10:10:43 -0500

    deferred option

--- config/database.php
+++ config/database.php
@@ -40,6 +40,7 @@ return [
             'busy_timeout' => null,
             'journal_mode' => null,
             'synchronous' => null,
+            'transaction_mode' => 'DEFERRED',
         ],
 
         'mysql' => [
