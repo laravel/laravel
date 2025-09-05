454251cffb16117b47c6793020960c3fd3addebf
Ahmed Alaa <92916738+AhmedAlaa4611@users.noreply.github.com>
2025-09-05 21:33:25 +0300

    Fix type casting for environment variables in config files

--- config/session.php
+++ config/session.php
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel')).'-session'
+        Str::slug((string) env('APP_NAME', 'laravel')).'-session'
     ),
 
     /*
