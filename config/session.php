7aa71dd9cdb3d58bce0505fd1d987f0517e45142
mdhasanurdev <mdhasanurdev@gmail.com>
2025-09-09 02:11:55 +0600

    fix(config): cast expire_on_close to boolean

--- config/session.php
+++ config/session.php
@@ -34,7 +34,7 @@ return [
 
     'lifetime' => (int) env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
+    'expire_on_close' => (bool) env('SESSION_EXPIRE_ON_CLOSE', false),
 
     /*
     |--------------------------------------------------------------------------
