e9c6b1960a5de6286871ce06d741a2a1a6d5ceef
Joel Stein <joel@mediatrix.digital>
2025-09-10 23:21:07 -0500

    Set MySQL time zone to UTC in database configuration.

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone="+00:00"',
             ]) : [],
         ],
 
