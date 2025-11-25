176315197ae55855f0b2e3577252f17266de18c9
MUHAMMAD QISTI AMALUDDIN BIN MOHD ROZAINI <119896181+QistiAmal1212@users.noreply.github.com>
2025-11-25 08:57:02 +0800

    Ignore Laravel compiled views for Vite
    Before testing, run php artisan cache:clear to clear old compiled views.
    
    This issue only happens during local development when running npm run dev.
    Laravel continuously recompiles Blade into PHP inside storage/framework/views (especially after Livewire re-renders), and these files are changed by Laravel itself, not by us.
    
    Because of that, Vite thinks we modified a file and triggers a full reload—even though the change is purely internal framework logic. This makes the reload behavior unacceptable and noisy during development.
    
    Since these compiled views belong to Laravel’s backend logic and not frontend assets, Vite does not need to watch them.
    
    Ignoring storage/framework/views keeps Vite stable and prevents reloads caused by Laravel doing its own internal work.

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,11 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        watch: {
+            ignored: [
+                '**/storage/framework/views/**',    
+              ],
+        },
+    },
 });
