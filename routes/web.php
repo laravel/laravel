2abe3056e0d9fc747c39351d6e2c33e7103d3405
David Torrell <david@ciber.cat>
2026-01-02 21:57:28 +0100

    fix changes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ciber', function () {
+    return view('ciber');
+});
+
+Route::get('/dd', function () {
+    dd('dd');
+});
+
+
