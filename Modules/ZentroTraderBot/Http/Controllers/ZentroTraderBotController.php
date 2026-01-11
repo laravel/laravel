9d5c398e9100073ab3f0bf75cac2a95f6939bf52
Donel Vazquez Zambrano <dvzambrano@gmail.com>
2026-01-11 11:28:08 -0500

    I

--- Modules/ZentroTraderBot/Http/Controllers/ZentroTraderBotController.php
+++ Modules/ZentroTraderBot/Http/Controllers/ZentroTraderBotController.php
@@ -327,7 +327,7 @@ class ZentroTraderBotController extends JsonsController
             $wallet = $suscriptor->data["wallet"];
         $description = "";
         if (isset($wallet["address"]))
-            $description = "    _Esta es tu wallet personal en este bot:_\n🫆 `" . $wallet["address"] . "`\n\n";
+            $description = "_Esta es tu wallet personal en este bot:_\n🫆 `" . $wallet["address"] . "`\n\n";
 
         return $this->getMainMenu(
             $suscriptor,
