b1ede0c1a9b31f79acfb2f8d421bb3bd5ead2173
Jack <jjbayliss@icloud.com>
2026-01-12 23:13:20 +0000

    Update 0001_01_01_000002_create_jobs_table.php

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('jobs', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('queue')->index();
             $table->longText('payload');
             $table->unsignedTinyInteger('attempts');
