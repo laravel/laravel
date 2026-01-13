bfdbb95d7a3616643a0b15317fa849e4c4011856
Taylor Otwell <taylor@laravel.com>
2026-01-13 08:43:09 -0600

    Update 0001_01_01_000002_create_jobs_table.php

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('jobs', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('queue')->index();
             $table->longText('payload');
             $table->unsignedTinyInteger('attempts');
