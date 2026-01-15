e713de24b883712cbb7fbc12d9b3aa1460e28cb7
Jack Bayliss <jjbayliss@icloud.com>
2026-01-15 14:49:36 +0000

    Revert "add index to failed jobs" (#6739)
    This reverts commit 36281b285a83c762446a9fe260ae95f76dfd35a6.

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -42,8 +42,6 @@ return new class extends Migration
             $table->longText('payload');
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
-
-            $table->index(['connection', 'queue', 'failed_at']);
         });
     }
 
