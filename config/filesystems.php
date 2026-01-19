31607246bd4debc01ad3e6d26c395c07dfa21755
KentarouTakeda <4785040+KentarouTakeda@users.noreply.github.com>
2026-01-20 00:16:42 +0900

    [12.x] Add `APP_URL` fallback in filesystems config (#6742)

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL'), '/').'/storage',
+            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
