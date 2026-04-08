bee4c98cefb825123c7451dd2c4788c16e28f1ed
Jonny Nott <jonnott@gmail.com>
2026-04-08 21:27:37 +0100

    Update comment in config/session.php

--- config/session.php
+++ config/session.php
@@ -219,10 +219,10 @@ return [
     | Session Serialization
     |--------------------------------------------------------------------------
     |
-    | This value controls the serialization strategy for session data, which
-    | is JSON by default. Setting this to "php" allows the storage of PHP
-    | objects in the session but can make an application vulnerable to
-    | "gadget chain" serialization attacks if the APP_KEY is leaked.
+    | This value controls the serialization strategy for session data. Setting
+    | this to "php" allows the storage of PHP objects in the session but can
+    | make an application vulnerable to "gadget chain" serialization attacks
+    | if the APP_KEY is leaked.
     |
     | Supported: "json", "php"
     |
