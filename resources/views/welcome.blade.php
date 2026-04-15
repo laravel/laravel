25680a3c41ac32660308a436595e6f052eb97457
Samuel <ratto_trash@hotmail.it>
2026-04-15 16:31:14 +0200

    Update changelog link to Laravel framework repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,7 +120,7 @@
 
                     <p class="mt-6 lg:mt-10 text-[#706f6c] dark:text-[#A1A09A]">
                         v{{ app()->version() }}
-                        <a href="https://github.com/laravel/laravel/blob/13.x/CHANGELOG.md" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                        <a href="https://github.com/laravel/framework/blob/13.x/CHANGELOG.md" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
                             <span>View changelog</span>
                             <svg
                                 width="10"
