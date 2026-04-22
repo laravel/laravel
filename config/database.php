187d333f7dcad4a1e0285211e2eb3250952a619a
Juan Lago <juan@leadsupply.com>
2026-04-22 15:36:21 +0200

    Added SSL_VERIFY_SERVER_CERT option to database configuration for MySQL.

--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? Arr::whereNotNull([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => false,
             ]) : [],
         ],
 
@@ -80,8 +81,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? whereNotNull([
+            'options' => extension_loaded('pdo_mysql') ? Arr::whereNotNull([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => false,
             ]) : [],
         ],
 
