c026cec9ffd22b2eb391dc2d8b4e3cf010c91379
Bradley Johnson <bradleyjohnson0009@gmail.com>
2026-05-15 00:16:20 -0400

    refactor: cache and jobs migration drop order

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -29,7 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
         Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('cache');
     }
 };
--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -52,8 +52,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
-        Schema::dropIfExists('job_batches');
         Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('job_batches');
+        Schema::dropIfExists('jobs');
     }
 };
