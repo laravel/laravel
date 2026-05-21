0d9dcb120c8099e937f88bef916aad2b673e3c5e
LucasCavalheri <lucas.dev.carvalho@gmail.com>
2026-05-21 11:35:38 -0300

    Configure JSON exception rendering for API routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(
+            fn(Request $request) => $request->is('api/*') || $request->expectsJson(),
+        );
     })->create();
