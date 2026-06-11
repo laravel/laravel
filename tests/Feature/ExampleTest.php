479370b7de7bbe34c00569ca1a37023609b230dd
congkv <congkv@gmail.com>
2026-06-11 21:54:12 +0700

    Use PHPUnit Test attribute in example tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,7 +11,8 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    #[Test]
+    public function the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -9,7 +10,8 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    #[Test]
+    public function true_is_true(): void
     {
         $this->assertTrue(true);
     }
