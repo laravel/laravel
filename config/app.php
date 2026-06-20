945b3241b422c2967b1dbb3301768016c96f2841
Jack Bayliss <jjbayliss@icloud.com>
2026-06-20 17:03:09 +0100

    Add 'array' as a supported maintenance mode driver
    Follows what we did here: https://github.com/laravel/framework/pull/60490 
    
    Cus its the skeleton, feels kwl to do

--- config/app.php
+++ config/app.php
@@ -114,7 +114,7 @@ return [
     | manage Laravel's "maintenance mode" status. The "cache" driver will
     | allow maintenance mode to be controlled across multiple machines.
     |
-    | Supported drivers: "file", "cache"
+    | Supported drivers: "file", "cache", "array"
     |
     */
 
