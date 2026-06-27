ce94c632dc22487ed8323ec4f53e61580da4fe96
AraZhar <49127959+AraZharr@users.noreply.github.com>
2026-06-28 02:13:48 +0800

    Create EditProduct.php

---
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use App\Filament\Resources\ProductResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditProduct extends EditRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
