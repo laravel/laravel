13b33b58faada18e47657f844dcb1bf4763458f2
AraZhar <49127959+AraZharr@users.noreply.github.com>
2026-06-28 01:44:17 +0800

    Create Category.php

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'icon', 'status'];
+
+    protected $casts = [
+        'status' => 'boolean',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
