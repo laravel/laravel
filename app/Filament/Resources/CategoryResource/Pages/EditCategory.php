adc2de58b925971b8eb69820d8c4f7ab29ef9ae5
AraZhar <49127959+AraZharr@users.noreply.github.com>
2026-06-28 02:09:02 +0800

    Create EditCategory.php

---
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\Pages;
+
+use App\Filament\Resources\CategoryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCategory extends EditRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
