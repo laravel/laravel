d55535e02a82ab540f4e0f914518fbd2bf0ac1f9
AraZhar <49127959+AraZharr@users.noreply.github.com>
2026-06-28 01:49:02 +0800

    Create Transaction.php

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id', 'reference', 'payment_method', 
+        'amount', 'status', 'paid_at'
+    ];
+
+    protected $casts = [
+        'amount' => 'integer',
+        'paid_at' => 'datetime',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
