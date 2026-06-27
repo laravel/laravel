23b4b677911f9ee118c7962fa60135b2ffc11c3a
AraZhar <49127959+AraZharr@users.noreply.github.com>
2026-06-28 01:35:35 +0800

    Create 2024_06_27_000002_create_products_table.php

---
+++ database/migrations/2024_06_27_000002_create_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('supplier_code');
+            $table->integer('base_price');
+            $table->integer('sell_price');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
