26e39bbe95aaf1aad707e7d7880addd829248c8d
fadrian06 <franyeradriansanchez@gmail.com>
2026-08-30 08:52:46 -0400

    Refactor Application::configure call in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
